ebb5559e9d183546b4b26e97293aad5e70a67972
Piero Proietti <piero.proietti@gmail.com>
2017-10-23 16:34:50 +0200

    network-manager

--- scripts/pve-install.sh
+++ scripts/pve-install.sh
@@ -4,7 +4,7 @@
 #
 echo "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-5.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-5.x.gpg
-apt remove --purge os-prober resolvconf network-manager gnome-network-manager
+apt-get remove os-prober resolvconf network-manager --purge
 apt-get update
 apt-get dist-upgrade
-apt install proxmox-ve postfix open-iscsi
+apt-get install proxmox-ve postfix open-iscsi
