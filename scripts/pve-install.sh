76f2227d48040fb5c1d27b08c5e7b4234c6d2994
Piero Proietti <piero.proietti@gmail.com>
2017-10-23 16:37:50 +0200

    proxmox

--- scripts/pve-install.sh
+++ scripts/pve-install.sh
@@ -5,6 +5,8 @@
 echo "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-5.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-5.x.gpg
 apt-get remove os-prober resolvconf network-manager --purge
+apt-get autoremove
 apt-get update
 apt-get dist-upgrade
 apt-get install proxmox-ve postfix open-iscsi
+wget https://github.com/pieroproietti/penguins-wallpapers/blob/master/fabrica.jpg /usr/share/wallpapers
