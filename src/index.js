7b234f42b9082c6db3908e1d232837b25a1d43fa
Piero Proietti <piero.proietti@gmail.com>
2017-10-24 11:59:30 +0200

    add parted to dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /*
   penguins-eggs:
-  depend on: squashfs-tools xorriso live-boot syslinux syslinux-common isolinux pxelinux
+  depend on: parted squashfs-tools xorriso live-boot syslinux syslinux-common isolinux pxelinux
 
 */
 "use strict";
