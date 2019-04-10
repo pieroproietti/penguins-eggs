b4ddbc3b1ac570313c2f24275c9a0f38bab0227d
Piero Proietti <piero.proietti@gmail.com>
2019-04-10 17:27:46 +0200

    sistemato getDistro

--- src/lib/getDistros.js
+++ src/lib/getDistros.js
@@ -28,41 +28,41 @@ os['BUG_REPORT_URL'] = "BUG_REPORT_URL=";
 read('/etc/os-release', function (data) {
   for (var temp in data) {
     if (!data[temp].search(os['PRETTY_NAME'])) {
-      os['PRETTY_NAME'] = data[temp].substring(os['PRETTY_NAME'].length);
+      os['PRETTY_NAME'] = data[temp].substring(os['PRETTY_NAME'].length).replace(/"/g, "");  
     };
 
     if (!data[temp].search(os['NAME'])) {
-      os['NAME'] = data[temp].substring(os['NAME'].length, 100);
+      os['NAME'] = data[temp].substring(os['NAME'].length).replace(/"/g, "");  
     };
 
     if (!data[temp].search(os['ID'])) {
-      os['ID'] = data[temp].substring(os['ID'].length);
+      os['ID'] = data[temp].substring(os['ID'].length).replace(/"/g, "");  
     };
 
     if (!data[temp].search(os['VERSION_ID'])) {
-      os['VERSION_ID'] = data[temp].substring(os['VERSION_ID'].length);
+      os['VERSION_ID'] = data[temp].substring(os['VERSION_ID'].length).replace(/"/g, "");  
     };
 
     if (!data[temp].search(os[`HOME_URL`])) {
-      os[`HOME_URL`] = data[temp].substring(os[`HOME_URL`].length);
+      os[`HOME_URL`] = data[temp].substring(os[`HOME_URL`].length).replace(/"/g, "");  
     };
 
     if (!data[temp].search(os[`SUPPORT_URL`])) {
-      os[`SUPPORT_URL`] = data[temp].substring(os[`SUPPORT_URL`].length);
+      os[`SUPPORT_URL`] = data[temp].substring(os[`SUPPORT_URL`].length).replace(/"/g, "");  
     };
 
     if (!data[temp].search(os[`BUG_REPORT_URL`])) {
-      os[`BUG_REPORT_URL`] = data[temp].substring(os[`BUG_REPORT_URL`].length);
+      os[`BUG_REPORT_URL`] = data[temp].substring(os[`BUG_REPORT_URL`].length).replace(/"/g, "");  
     };
   }
   var oJson = "{ ";
-  oJson += '"prettyName": ' + os['PRETTY_NAME'] + ', ';
+  oJson += '"prettyName": "' + os['PRETTY_NAME'] + '", ';
   oJson += '"name": "' + os['NAME'] + '", ';
   oJson += '"id": "' + os['ID'] + '", ';
-  oJson += '"versionId": ' + os['VERSION_ID'] + ', ';
-  oJson += '"homeUrl": ' + os['HOME_URL'] + ', ';
-  oJson += '"supportUrl": ' + os['SUPPORT_URL'] + ', ';
-  oJson += '"bugReportUrl": ' + os['BUG_REPORT_URL'] + ' }';
+  oJson += '"versionId": "' + os['VERSION_ID'] + '", ';
+  oJson += '"homeUrl": "' + os['HOME_URL'] + '", ';
+  oJson += '"supportUrl": "' + os['SUPPORT_URL'] + '", ';
+  oJson += '"bugReportUrl": "' + os['BUG_REPORT_URL'] + '"}';
 
   var obj = JSON.parse(oJson);
     console.log(obj.prettyName);
