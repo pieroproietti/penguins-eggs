c0f4899afb7fd1690f37118c48e0f51eb23bf2cf
Piero Proietti <piero.proietti@gmail.com>
2019-04-17 19:31:10 +0200

    last check

--- src/lib/Iso.ts
+++ src/lib/Iso.ts
@@ -145,7 +145,7 @@ class Iso {
       `rsync -a ${syslinuxPath}ldlinux.c32 ${this.distro.pathIso}/isolinux/`
     );
     utils.exec(
-      `rsync -a ${isolinuxPath}libcom32.c32 ${this.distro.pathIso}/isolinux/`
+      `rsync -a ${syslinuxPath}libcom32.c32 ${this.distro.pathIso}/isolinux/`
     );
     utils.exec(
       `rsync -a ${syslinuxPath}libutil.c32 ${this.distro.pathIso}/isolinux/`
