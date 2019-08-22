a9699fc7562e3ab5d7eebc1021c260055e9b6cf3
Piero Proietti <piero.proietti@gmail.com>
2019-08-22 07:29:33 +0200

    aggiunto kernel in menu.cfg

--- src/classes/Iso.ts
+++ src/classes/Iso.ts
@@ -319,395 +319,394 @@ utils.bashWrite(file, text);
     let text = `
     INCLUDE stdmenu.cfg
     MENU title Main Menu
-    DEFAULT ^${this.distro.name}
-    LABEL ${this.distro.name}
+    DEFAULT ^${this.distro.name} 
+    LABEL ${this.distro.name} (kernel ${this.distro.kernel})
         SAY "Booting ${this.distro.name}"
         linux /live/vmlinuz
         ${o.append}
     
-
     MENU begin advanced
     MENU title ${this.distro.name} with Localisation Support
     
-    LABEL Albanian (sq)
+    LABEL Albanian (sq) (kernel ${this.distro.kernel})
           SAY "Booting Albanian (sq)..."
           linux /live/vmlinuz
-          ${o.append} locales=sq_AL.UTF-8;
+          ${o.append} components locales=sq_AL.UTF-8;
 
-    LABEL Amharic (am)
+    LABEL Amharic (am) (kernel ${this.distro.kernel})
           SAY "Booting Amharic (am)..."
           linux /live/vmlinuz
-          ${o.append} locales=am_ET.UTF-8
+          ${o.append} components locales=am_ET.UTF-8
           
-    LABEL Arabic (ar)
+    LABEL Arabic (ar) (kernel ${this.distro.kernel})
           SAY "Booting Arabic (ar)..."
           linux /live/vmlinuz
-          ${o.append} locales=ar_EG.UTF-8
+          ${o.append} components locales=ar_EG.UTF-8
     
-    LABEL Asturian (ast)
+    LABEL Asturian (ast) (kernel ${this.distro.kernel})
           SAY "Booting Asturian (ast)..."  
           linux /live/vmlinuz
-          ${o.append} locales=ast_ES.UTF-8
+          ${o.append} components locales=ast_ES.UTF-8
     
-    LABEL Basque (eu)
+    LABEL Basque (eu) (kernel ${this.distro.kernel})
           SAY "Booting Basque (eu)..."
           linux /live/vmlinuz
-          ${o.append} locales=eu_ES.UTF-8
+          ${o.append} components locales=eu_ES.UTF-8
           
-    LABEL Belarusian (be)
+    LABEL Belarusian (be) (kernel ${this.distro.kernel})
           SAY "Booting Belarusian (be)..."
           linux /live/vmlinuz
-          ${o.append}locales=be_BY.UTF-8
+          ${o.append}components locales=be_BY.UTF-8
     
-    LABEL Bangla (bn)
+    LABEL Bangla (bn) (kernel ${this.distro.kernel})
           SAY "Booting Bangla (bn)..."
           linux /live/vmlinuz
-          ${o.append}locales=bn_BD
+          ${o.append}components locales=bn_BD
     
     LABEL Bosnian (bs)
           SAY "Booting Bosnian (bs)..."
           linux /live/vmlinuz
-          ${o.append} locales=bs_BA.UTF-8
+          ${o.append} components locales=bs_BA.UTF-8
     
     LABEL Bulgarian (bg)
           SAY "Booting Bulgarian (bg)..."
           linux /live/vmlinuz
-          ${o.append} locales=bg_BG.UTF-8
+          ${o.append} components locales=bg_BG.UTF-8
         
     LABEL Tibetan (bo)
           SAY "Booting Tibetan (bo)..."
           linux /live/vmlinuz
-          ${o.append} locales=bo_IN
+          ${o.append} components locales=bo_IN
         
         LABEL C (C)
           SAY "Booting C (C)..."
           linux /live/vmlinuz
-          ${o.append} locales=C
+          ${o.append} components locales=C
         
         LABEL Catalan (ca)
           SAY "Booting Catalan (ca)..."
           linux /live/vmlinuz
-          ${o.append} locales=ca_ES.UTF-8
+          ${o.append} components locales=ca_ES.UTF-8
         
         LABEL Chinese (Simplified) (zh_CN)
           SAY "Booting Chinese (Simplified) (zh_CN)..."
           linux /live/vmlinuz
-          ${o.append} locales=zh_CN.UTF-8
+          ${o.append} components locales=zh_CN.UTF-8
         
         LABEL Chinese (Traditional) (zh_TW)
           SAY "Booting Chinese (Traditional) (zh_TW)..."
           linux /live/vmlinuz
-          ${o.append} locales=zh_TW.UTF-8
+          ${o.append} components locales=zh_TW.UTF-8
         
         LABEL Croatian (hr)
           SAY "Booting Croatian (hr)..."
           linux /live/vmlinuz
-          ${o.append} locales=hr_HR.UTF-8
+          ${o.append} components locales=hr_HR.UTF-8
         
         LABEL Czech (cs)
           SAY "Booting Czech (cs)..."
           linux /live/vmlinuz
-          ${o.append} locales=cs_CZ.UTF-8
+          ${o.append} components locales=cs_CZ.UTF-8
         
         LABEL Danish (da)
           SAY "Booting Danish (da)..."
           linux /live/vmlinuz
-          ${o.append} locales=da_DK.UTF-8
+          ${o.append} components locales=da_DK.UTF-8
         
         LABEL Dutch (nl)
           SAY "Booting Dutch (nl)..."
           linux /live/vmlinuz
-          ${o.append} locales=nl_NL.UTF-8
+          ${o.append} components locales=nl_NL.UTF-8
         
         LABEL Dzongkha (dz)
           SAY "Booting Dzongkha (dz)..."
           linux /live/vmlinuz
-          ${o.append} locales=dz_BT
+          ${o.append} components locales=dz_BT
         
         LABEL English (en)
           SAY "Booting English (en)..."
           linux /live/vmlinuz
-          ${o.append} locales=en_US.UTF-8
+          ${o.append} components locales=en_US.UTF-8
         
         LABEL Esperanto (eo)
           SAY "Booting Esperanto (eo)..."
           linux /live/vmlinuz
-          ${o.append} locales=eo.UTF-8
+          ${o.append} components locales=eo.UTF-8
         
         LABEL Estonian (et)
           SAY "Booting Estonian (et)..."
           linux /live/vmlinuz
-          ${o.append} locales=et_EE.UTF-8
+          ${o.append} components locales=et_EE.UTF-8
         
         LABEL Finnish (fi)
           SAY "Booting Finnish (fi)..."
           linux /live/vmlinuz
-          ${o.append} locales=fi_FI.UTF-8
+          ${o.append} components locales=fi_FI.UTF-8
         
         LABEL French (fr)
           SAY "Booting French (fr)..."
           linux /live/vmlinuz
-          ${o.append} locales=fr_FR.UTF-8
+          ${o.append} components locales=fr_FR.UTF-8
         
         LABEL Galician (gl)
           SAY "Booting Galician (gl)..."
           linux /live/vmlinuz
-          ${o.append} locales=gl_ES.UTF-8
+          ${o.append} components locales=gl_ES.UTF-8
         
         LABEL Georgian (ka)
           SAY "Booting Georgian (ka)..."
           linux /live/vmlinuz
-          ${o.append} locales=ka_GE.UTF-8
+          ${o.append} components locales=ka_GE.UTF-8
         
         LABEL German (de)
           SAY "Booting German (de)..."
           linux /live/vmlinuz
-          ${o.append} locales=de_DE.UTF-8
+          ${o.append} components locales=de_DE.UTF-8
         
         LABEL Greek (el)
           SAY "Booting Greek (el)..."
           linux /live/vmlinuz
-          ${o.append} locales=el_GR.UTF-8
+          ${o.append} components locales=el_GR.UTF-8
         
         LABEL Gujarati (gu)
           SAY "Booting Gujarati (gu)..."
           linux /live/vmlinuz
-          ${o.append} locales=gu_IN
+          ${o.append} components locales=gu_IN
         
         LABEL Hebrew (he)
           SAY "Booting Hebrew (he)..."
           linux /live/vmlinuz
-          ${o.append} locales=he_IL.UTF-8
+          ${o.append} components locales=he_IL.UTF-8
         
         LABEL Hindi (hi)
           SAY "Booting Hindi (hi)..."
           linux /live/vmlinuz
-          ${o.append} locales=hi_IN
+          ${o.append} components locales=hi_IN
         
         LABEL Hungarian (hu)
           SAY "Booting Hungarian (hu)..."
           linux /live/vmlinuz
-          ${o.append} locales=hu_HU.UTF-8
+          ${o.append} components locales=hu_HU.UTF-8
         
         LABEL Icelandic (is)
           SAY "Booting Icelandic (is)..."
           linux /live/vmlinuz
-          ${o.append} locales=is_IS.UTF-8
+          ${o.append} components locales=is_IS.UTF-8
         
         LABEL Indonesian (id)
           SAY "Booting Indonesian (id)..."
           linux /live/vmlinuz
-          ${o.append} locales=id_ID.UTF-8
+          ${o.append} components locales=id_ID.UTF-8
         
         LABEL Irish (ga)
           SAY "Booting Irish (ga)..."
           linux /live/vmlinuz
-          ${o.append} locales=ga_IE.UTF-8
+          ${o.append} components locales=ga_IE.UTF-8
         
         LABEL Italian (it)
           SAY "Booting Italian (it)..."
           linux /live/vmlinuz
-          ${o.append} locales=it_IT.UTF-8
+          ${o.append} components locales=it_IT.UTF-8
         
         LABEL Japanese (ja)
           SAY "Booting Japanese (ja)..."
           linux /live/vmlinuz
-          ${o.append} locales=ja_JP.UTF-8
+          ${o.append} components locales=ja_JP.UTF-8
         
         LABEL Kazakh (kk)
           SAY "Booting Kazakh (kk)..."
           linux /live/vmlinuz
-          ${o.append} locales=kk_KZ.UTF-8
+          ${o.append} components locales=kk_KZ.UTF-8
         
         LABEL Khmer (km)
           SAY "Booting Khmer (km)..."
           linux /live/vmlinuz
-          ${o.append} locales=km_KH
+          ${o.append} components locales=km_KH
         
         LABEL Kannada (kn)
           SAY "Booting Kannada (kn)..."
           linux /live/vmlinuz
-          ${o.append} locales=kn_IN
+          ${o.append} components locales=kn_IN
         
         LABEL Korean (ko)
           SAY "Booting Korean (ko)..."
           linux /live/vmlinuz
-          ${o.append} locales=ko_KR.UTF-8
+          ${o.append} components locales=ko_KR.UTF-8
         
         LABEL Kurdish (ku)
           SAY "Booting Kurdish (ku)..."
           linux /live/vmlinuz
-          ${o.append} locales=ku_TR.UTF-8
+          ${o.append} components locales=ku_TR.UTF-8
         
         LABEL Lao (lo)
           SAY "Booting Lao (lo)..."
           linux /live/vmlinuz
-          ${o.append} locales=lo_LA
+          ${o.append} components locales=lo_LA
         
         LABEL Latvian (lv)
           SAY "Booting Latvian (lv)..."
           linux /live/vmlinuz
-          ${o.append} locales=lv_LV.UTF-8
+          ${o.append} components locales=lv_LV.UTF-8
         
         LABEL Lithuanian (lt)
           SAY "Booting Lithuanian (lt)..."
           linux /live/vmlinuz
-          ${o.append} locales=lt_LT.UTF-8
+          ${o.append} components locales=lt_LT.UTF-8
         
         LABEL Malayalam (ml)
           SAY "Booting Malayalam (ml)..."
           linux /live/vmlinuz
-          ${o.append} locales=ml_IN
+          ${o.append} components locales=ml_IN
         
         LABEL Marathi (mr)
           SAY "Booting Marathi (mr)..."
           linux /live/vmlinuz
-          ${o.append} locales=mr_IN
+          ${o.append} components locales=mr_IN
         
         LABEL Macedonian (mk)
           SAY "Booting Macedonian (mk)..."
           linux /live/vmlinuz
-          ${o.append} locales=mk_MK.UTF-8
+          ${o.append} components locales=mk_MK.UTF-8
         
         LABEL Burmese (my)
           SAY "Booting Burmese (my)..."
           linux /live/vmlinuz
-          ${o.append} locales=my_MM
+          ${o.append} components locales=my_MM
         
         LABEL Nepali (ne)
           SAY "Booting Nepali (ne)..."
           linux /live/vmlinuz
-          ${o.append} locales=ne_NP
+          ${o.append} components locales=ne_NP
         
         LABEL Northern Sami (se_NO)
           SAY "Booting Northern Sami (se_NO)..."
           linux /live/vmlinuz
-          ${o.append} locales=se_NO
+          ${o.append} components locales=se_NO
         
         LABEL Norwegian Bokmaal (nb_NO)
           SAY "Booting Norwegian Bokmaal (nb_NO)..."
           linux /live/vmlinuz
-          ${o.append} locales=nb_NO.UTF-8
+          ${o.append} components locales=nb_NO.UTF-8
         
         LABEL Norwegian Nynorsk (nn_NO)
           SAY "Booting Norwegian Nynorsk (nn_NO)..."
           linux /live/vmlinuz
-          ${o.append} locales=nn_NO.UTF-8
+          ${o.append} components locales=nn_NO.UTF-8
         
         LABEL Persian (fa)
           SAY "Booting Persian (fa)..."
           linux /live/vmlinuz
-          ${o.append} locales=fa_IR
+          ${o.append} components locales=fa_IR
         
         LABEL Polish (pl)
           SAY "Booting Polish (pl)..."
           linux /live/vmlinuz
-          ${o.append} locales=pl_PL.UTF-8
+          ${o.append} components locales=pl_PL.UTF-8
         
         LABEL Portuguese (pt)
           SAY "Booting Portuguese (pt)..."
           linux /live/vmlinuz
-          ${o.append} locales=pt_PT.UTF-8
+          ${o.append} components locales=pt_PT.UTF-8
         
         LABEL Portuguese (Brazil) (pt_BR)
           SAY "Booting Portuguese (Brazil) (pt_BR)..."
           linux /live/vmlinuz
-          ${o.append} locales=pt_BR.UTF-8
+          ${o.append} components locales=pt_BR.UTF-8
         
         LABEL Punjabi (Gurmukhi) (pa)
           SAY "Booting Punjabi (Gurmukhi) (pa)..."
           linux /live/vmlinuz
-          ${o.append} locales=pa_IN
+          ${o.append} components locales=pa_IN
         
         LABEL Romanian (ro)
           SAY "Booting Romanian (ro)..."
           linux /live/vmlinuz
-          ${o.append} locales=ro_RO.UTF-8
+          ${o.append} components locales=ro_RO.UTF-8
         
         LABEL Russian (ru)
           SAY "Booting Russian (ru)..."
           linux /live/vmlinuz
-          ${o.append} locales=ru_RU.UTF-8
+          ${o.append} components locales=ru_RU.UTF-8
         
         LABEL Sinhala (si)
           SAY "Booting Sinhala (si)..."
           linux /live/vmlinuz
-          ${o.append} locales=si_LK
+          ${o.append} components locales=si_LK
         
         LABEL Serbian (Cyrillic) (sr)
           SAY "Booting Serbian (Cyrillic) (sr)..."
           linux /live/vmlinuz
-          ${o.append} locales=sr_RS
+          ${o.append} components locales=sr_RS
         
         LABEL Slovak (sk)
           SAY "Booting Slovak (sk)..."
           linux /live/vmlinuz
-          ${o.append} locales=sk_SK.UTF-8
+          ${o.append} components locales=sk_SK.UTF-8
         
         LABEL Slovenian (sl)
           SAY "Booting Slovenian (sl)..."
           linux /live/vmlinuz
-          ${o.append} locales=sl_SI.UTF-8
+          ${o.append} components locales=sl_SI.UTF-8
         
         LABEL Spanish (es)
           SAY "Booting Spanish (es)..."
           linux /live/vmlinuz
-          ${o.append} locales=es_ES.UTF-8
+          ${o.append} components locales=es_ES.UTF-8
         
         LABEL Swedish (sv)
           SAY "Booting Swedish (sv)..."
           linux /live/vmlinuz
-          ${o.append} locales=sv_SE.UTF-8
+          ${o.append} components locales=sv_SE.UTF-8
         
         LABEL Tagalog (tl)
           SAY "Booting Tagalog (tl)..."
           linux /live/vmlinuz
-          ${o.append} locales=tl_PH.UTF-8
+          ${o.append} components locales=tl_PH.UTF-8
         
         LABEL Tamil (ta)
           SAY "Booting Tamil (ta)..."
           linux /live/vmlinuz
-          ${o.append} locales=ta_IN
+          ${o.append} components locales=ta_IN
         
         LABEL Telugu (te)
           SAY "Booting Telugu (te)..."
           linux /live/vmlinuz
-          ${o.append} locales=te_IN
+          ${o.append} components locales=te_IN
         
         LABEL Tajik (tg)
           SAY "Booting Tajik (tg)..."
           linux /live/vmlinuz
-          ${o.append} locales=tg_TJ.UTF-8
+          ${o.append} components locales=tg_TJ.UTF-8
         
         LABEL Thai (th)
           SAY "Booting Thai (th)..."
           linux /live/vmlinuz
-          ${o.append} locales=th_TH.UTF-8
+          ${o.append} components locales=th_TH.UTF-8
         
         LABEL Turkish (tr)
           SAY "Booting Turkish (tr)..."
           linux /live/vmlinuz
-          ${o.append} locales=tr_TR.UTF-8
+          ${o.append} components locales=tr_TR.UTF-8
         
         LABEL Uyghur (ug)
           SAY "Booting Uyghur (ug)..."
           linux /live/vmlinuz
-          ${o.append} locales=ug_CN
+          ${o.append} components locales=ug_CN
         
         LABEL Ukrainian (uk)
           SAY "Booting Ukrainian (uk)..."
           linux /live/vmlinuz
-          ${o.append} locales=uk_UA.UTF-8
+          ${o.append} components locales=uk_UA.UTF-8
         
         LABEL Vietnamese (vi)
           SAY "Booting Vietnamese (vi)..."
           linux /live/vmlinuz
-          ${o.append} locales=vi_VN
+          ${o.append} components locales=vi_VN
         
         LABEL Welsh (cy)
           SAY "Booting Welsh (cy)..."
           linux /live/vmlinuz
-          ${o.append} locales=cy_GB.UTF-8
+          ${o.append} components locales=cy_GB.UTF-8
         
          LABEL mainmenu 
           MENU label Back
