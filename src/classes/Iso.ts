6b2829cff95df8feedccf0ea0344c45ab9804ced
Piero Proietti <piero.proietti@gmail.com>
2019-09-23 16:14:24 +0200

    rimossa eggs

--- src/classes/Iso.ts
+++ src/classes/Iso.ts
@@ -194,6 +194,7 @@ class Iso {
       --filter="+ /lib/live/config/*" \
       --filter="+ /lib/live/init-config-sh" \
       --filter="+ /lib/live/setup-network.sh" \
+      --filter="- /home/eggs" \
       --filter="+ /home/live/*" \
       / ${this.distro.pathFs}`;
       
