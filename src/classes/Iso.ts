7df664733b648dd5f590326c9c86128dc5641627
Piero Proietti <piero.proietti@gmail.com>
2019-09-23 17:17:24 +0200

    something

--- src/classes/Iso.ts
+++ src/classes/Iso.ts
@@ -179,6 +179,20 @@ class Iso {
     let specificFilters:string;
     let cmd:string = "";
 
+    cmd = `\
+      rsync \
+      --archive \
+      --delete-before \
+      --delete-excluded \
+      --filter="- ${this.distro.pathHome}" \
+      /home/live/* ${this.distro.pathFs}/home/live/*`;
+
+      console.log("==========================================");
+      console.log(`system2egg: copyng... home ${cmd} \n`);
+      console.log("==========================================");
+      shell.exec(cmd.trim(), {
+        async: false
+      });
     
 
     // Copia la home di live da system ad egg
@@ -204,17 +218,6 @@ class Iso {
         async: false
       });
   
-    cmd = `\
-      rsync \
-      --archive \
-      /home/live/* ${this.distro.pathFs}/home/live/*`;
-
-      console.log("==========================================");
-      console.log(`system2egg: copyng... ${cmd}\n`);
-      console.log("==========================================");
-      shell.exec(cmd.trim(), {
-        async: false
-      });
   
 
   }
