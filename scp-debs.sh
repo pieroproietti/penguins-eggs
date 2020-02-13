657c583a97d38f6bc0bcd498de8564cb49fb1495
Piero Proietti <piero.proietti@gmail.com>
2020-02-13 10:17:22 +0100

    scp-dev.sh

---
+++ scp-debs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+HOST='192.168.61.2'
+USER='root'
+PATH_DEBS='/home/artisan/sourceforge/DEBS/'
+FILES='eggs_7.1.??-?_amd64.deb'
+ssh ${USER}@${HOST} rm -rf ${PATH_DEBS}$FILES
+scp ./dist/deb/${FILES} ${USER}@${HOST}:${PATH_DEBS}
+
