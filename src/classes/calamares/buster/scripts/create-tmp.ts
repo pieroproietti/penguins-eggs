b66225da1eb2b4317866d06aa35599c6d1170b75
Piero Proietti <piero.proietti@gmail.com>
2020-03-16 12:10:17 -0400

    create-tmp modulo per calamares

---
+++ src/classes/calamares/buster/scripts/create-tmp.ts
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+export function createTmp(): string {
+    let text = ``
+    text += `#!/bin/bash\n`
+    text += `\n`
+    text += `CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")\n`
+    text += `\n`
+    text += `# Creo la directory $CHROOT/tmp se mancante \n`
+    text += `TMPDIR=$CHROOT/tmp\n`
+    text += `if [ ! -d $TMPDIR ]; then\n`
+    text += `    echo mkdir $TMPDIR\n`
+    text += `fi\n`
+    return text
+}
