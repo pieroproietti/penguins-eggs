5e4a2e4d8e18c1962dd30558e835c782f5a59c43
Piero Proietti <piero.proietti@gmail.com>
2020-06-06 16:24:05 +0200

    boot 3

--- src/classes/hatching.ts
+++ src/classes/hatching.ts
@@ -642,7 +642,7 @@ adduser ${username} \
 
     if (this.devices.boot.device !== 'nome') {
       Utils.warning(`Formatting ${this.devices.boot.device} as ${this.devices.boot.fsType}`)
-      if (this.devices.boot.fsType === '') {
+      if (this.devices.boot.fsType === undefined) {
         this.devices.boot.fsType = `ext2`
       }
       await exec(`mkfs -t ${this.devices.boot.fsType} ${this.devices.boot.device}`, echo)
