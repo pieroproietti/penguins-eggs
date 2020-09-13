c84415f33c8fbd6e964bcb4958a9401bfa31defe
Piero Proietti <piero.proietti@gmail.com>
2020-09-13 09:23:58 +0200

    comando locales solo creazione locales, rimosso localepurge

---
+++ src/commands/locales.ts
@@ -0,0 +1,39 @@
+import { Command, flags } from '@oclif/command'
+
+import shx = require('shelljs')
+import path = require('path')
+
+import Utils from '../classes/utils'
+import Distro from '../classes/distro'
+
+
+export default class Locales extends Command {
+    static description = 'install/clean localess'
+
+    static flags = {
+        help: flags.help({ char: 'h' }),
+        verbose: flags.boolean({ char: 'v', description: 'verbose' }),
+    }
+
+    static args = [{ name: 'file' }]
+    async run() {
+        const { args, flags } = this.parse(Locales)
+
+        let verbose = false
+        if (flags.verbose) {
+            verbose = true
+        }
+
+        const echo = Utils.setEcho(verbose)
+
+        if (Utils.isRoot()) {
+            if (verbose) {
+                console.log('creating /etc/locale.gen')
+                console.log('recreating /etc/locale.gen')
+            }
+            console.log(process.execPath)
+            shx.cp(path.resolve(__dirname, '../../conf/locale.gen.template'), '/etc/locale.gen')
+            shx.exec('/usr/sbin/locale-gen', echo)
+        }
+    }
+}
