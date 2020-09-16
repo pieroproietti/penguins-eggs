7b43624b1fe2ef87f3c435babdb4090055d891ce
Piero Proietti <piero.proietti@gmail.com>
2020-09-16 06:47:22 -0400

    slim autologin

--- src/classes/xdg.ts
+++ src/classes/xdg.ts
@@ -86,7 +86,7 @@ export default class Xdg {
    static async autologin(olduser: string, newuser: string, chroot = '/') {
       if (Pacman.packageIsInstalled('lightdm')) {
          shx.sed('-i', `autologin-user=${olduser}`, `autologin-user=${newuser}`, `${chroot}/etc/lightdm/lightdm.conf`)
-      } (Pacman.packageIsInstalled('slim')) {
+      } else if (Pacman.packageIsInstalled('slim')) {
          shx.sed('-i', `default_user=${olduser}`, `default_user=${newuser}`, `${chroot}/etc/slim.conf`)
       }
    }
