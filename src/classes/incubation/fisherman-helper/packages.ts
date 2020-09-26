a4960c754c36990a801fcd5edd8deecec38056f7
Piero Proietti <piero.proietti@gmail.com>
2020-09-26 16:36:15 +0200

    locales

--- src/classes/incubation/fisherman-helper/packages.ts
+++ src/classes/incubation/fisherman-helper/packages.ts
@@ -25,36 +25,51 @@ export function remove(distro: IDistro): string {
 
  */
 export function tryInstall(distro: IDistro): string {
+   const LANG = process.env.LANG
+
+   let lang = 'en_gb'
+
+   if (LANG === 'it_IT.UTF-8') {
+      lang = 'it'
+   } else if (LANG === 'en_US.UTF-8') {
+      lang = 'en_gb'
+   } else if (LANG === 'es_PE.UTF-8') {
+      lang = 'es_es'
+   } else if (LANG === 'pt_BR.UTF-8') {
+      lang = 'pt_br'
+   } else if (LANG === 'fr_FR.UTF-8') {
+      lang = 'fr'
+   } else if (LANG === 'de_DE.UTF-8') {
+      lang = 'de'
+   }
+
    let text = `  - try_install:\n`
 
 
    // Pacchetti da installare sempre
-   text += '    - hunspell-$LOCALE\n'
+   text += `    - hunspell-${lang}\n`
 
    // Pacchetti da installare a seconda della distribuzione
    if ((distro.versionLike === 'focal') || (distro.versionLike === 'bionic')) {
-      text += '    - language-pack-$LOCALE\n'
+      text += `    - language-pack-${lang}\n`
    }
 
    // Pacchetti da installare se sono presenti
    if (Pacman.packageIsInstalled(`libreoffice-base-core`)) {
-      text += '    - libreoffice-l10n-$LOCALE\n'
-      text += '    - libreoffice-help-$LOCALE\n'
+      text += `    - libreoffice-l10n-${lang}\n`
+      text += `    - libreoffice-help-${lang}\n`
    }
 
    if (Pacman.packageIsInstalled('firefox-esr')) {
-      text += '    - firefox-esr-$LOCALE\n'
+      text += `    - firefox-esr-${lang}\n`
    }
 
    if (Pacman.packageIsInstalled('thunderbird')) {
-      text += '    - thunderbird-locale-$LOCALE\n'
+      text += `    - thunderbird-locale-${lang}\n`
    }
 
    return text
 }
-
-
-
 
 /**
  * 
