ad449f6b0dda6219b4b3ec2c6f0f0249fdcc85c4
Piero Proietti <piero.proietti@gmail.com>
2020-11-14 10:53:21 +0100

    mount di penguins-eggs e di mx

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -87,7 +87,7 @@ async function minstall() {
    // Creazione delle partizioni di mount
    shx.exec('mkdir /mnt/antiX/ -p')
    shx.exec('mkdir /mnt/antiX/boot/efi -p')
-   shx.exec('mount --bind /boot/efi /mnt/antiX/boot/efi ')
+   // shx.exec('mount --bind /boot/efi /mnt/antiX/boot/efi ')
 
    shx.exec('mkdir /mnt/antiX/proc -p')
    shx.exec('mount --bind /proc /mnt/antiX/proc ')
@@ -103,3 +103,62 @@ async function minstall() {
 
    //shx.exec('minstall')
 }
+
+/*
+I mount originali di MXLINUX
+
+/dev/sr0 on /live/boot-dev type iso9660 (ro,relatime,nojoliet,check=s,map=n,blocksize=2048)
+/live/boot-dev/antiX/linuxfs on /live/linux type squashfs (ro,relatime)
+tmpfs on /live/aufs-ram type tmpfs (rw,noatime,size=1589248k)
+overlay on / type overlay (rw,relatime,lowerdir=/live/linux,upperdir=/live/aufs-ram/upper,workdir=/live/aufs-ram/work)
+tmpfs on /media type tmpfs (rw,noatime,size=10240k)
+tmpfs on /run type tmpfs (rw,nosuid,nodev,noexec,noatime,size=204268k,mode=755)
+tmpfs on /live type tmpfs (rw,noatime,size=10240k,mode=755)
+tmpfs on /tmp type tmpfs (rw,noatime)
+proc on /proc type proc (rw,nosuid,nodev,noexec,relatime)
+sys on /sys type sysfs (rw,nosuid,nodev,noexec,relatime)
+devtmpfs on /dev type devtmpfs (rw,relatime,size=1015084k,nr_inodes=253771,mode=755)
+devpts on /dev/pts type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000)
+overlay on /live/aufs type overlay (rw,relatime,lowerdir=/live/linux,upperdir=/live/aufs-ram/upper,workdir=/live/aufs-ram/work)
+tmpfs on /etc/live/config type tmpfs (rw,noatime,size=10240k,mode=755)
+tmpfs on /etc/live/bin type tmpfs (rw,noatime,size=10240k,mode=755)
+tmpfs on /run/lock type tmpfs (rw,nosuid,nodev,noexec,relatime,size=5120k)
+pstore on /sys/fs/pstore type pstore (rw,relatime)
+tmpfs on /dev/shm type tmpfs (rw,nosuid,nodev,noexec,relatime,size=408520k)
+rpc_pipefs on /run/rpc_pipefs type rpc_pipefs (rw,relatime)
+cgroup on /sys/fs/cgroup type tmpfs (rw,relatime,size=12k,mode=755)
+systemd on /sys/fs/cgroup/systemd type cgroup (rw,nosuid,nodev,noexec,relatime,release_agent=/run/cgmanager/agents/cgm-release-agent.systemd,name=systemd)
+tmpfs on /run/user/1000 type tmpfs (rw,nosuid,nodev,relatime,size=204264k,mode=700,uid=1000,gid=1000)
+gvfsd-fuse on /run/user/1000/gvfs type fuse.gvfsd-fuse (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
+*/
+
+/*
+I mount di penguins-eggs
+
+sysfs on /sys type sysfs (rw,nosuid,nodev,noexec,relatime)
+proc on /proc type proc (rw,nosuid,nodev,noexec,relatime)
+udev on /dev type devtmpfs (rw,nosuid,relatime,size=1985720k,nr_inodes=496430,mode=755)
+devpts on /dev/pts type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000)
+tmpfs on /run type tmpfs (rw,nosuid,noexec,relatime,size=404116k,mode=755)
+/dev/sr0 on /run/live/medium type iso9660 (ro,noatime,nojoliet,check=s,map=n,blocksize=2048)
+/dev/loop0 on /run/live/rootfs/filesystem.squashfs type squashfs (ro,noatime)
+tmpfs on /run/live/overlay type tmpfs (rw,noatime,size=2020564k,mode=755)
+overlay on / type overlay (rw,noatime,lowerdir=/run/live/rootfs/filesystem.squashfs/,upperdir=/run/live/overlay/rw,workdir=/run/live/overlay/work)
+tmpfs on /usr/lib/live/mount type tmpfs (rw,nosuid,noexec,relatime,size=404116k,mode=755)
+/dev/sr0 on /usr/lib/live/mount/medium type iso9660 (ro,noatime,nojoliet,check=s,map=n,blocksize=2048)
+/dev/loop0 on /usr/lib/live/mount/rootfs/filesystem.squashfs type squashfs (ro,noatime)
+tmpfs on /usr/lib/live/mount/overlay type tmpfs (rw,noatime,size=2020564k,mode=755)
+tmpfs on /run/lock type tmpfs (rw,nosuid,nodev,noexec,relatime,size=5120k)
+pstore on /sys/fs/pstore type pstore (rw,relatime)
+tmpfs on /dev/shm type tmpfs (rw,nosuid,nodev,noexec,relatime,size=808220k)
+tmpfs on /tmp type tmpfs (rw,nosuid,nodev,relatime)
+rpc_pipefs on /run/rpc_pipefs type rpc_pipefs (rw,relatime)
+cgroup on /sys/fs/cgroup type tmpfs (rw,relatime,size=12k,mode=755)
+systemd on /sys/fs/cgroup/systemd type cgroup (rw,nosuid,nodev,noexec,relatime,release_agent=/run/cgmanager/agents/cgm-release-agent.systemd,name=systemd)
+tmpfs on /run/user/1000 type tmpfs (rw,nosuid,nodev,relatime,size=404112k,mode=700,uid=1000,gid=1000)
+gvfsd-fuse on /run/user/1000/gvfs type fuse.gvfsd-fuse (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
+/dev/sda1 on /media/sda1 type ext4 (rw,relatime)
+udev on /media/sda1/dev type devtmpfs (rw,nosuid,relatime,size=1985720k,nr_inodes=496430,mode=755)
+proc on /media/sda1/proc type proc (rw,nosuid,nodev,noexec,relatime)
+sysfs on /media/sda1/sys type sysfs (rw,nosuid,nodev,noexec,relatime)
+*/
