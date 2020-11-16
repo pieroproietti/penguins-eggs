947862a56a5d6678aa5ec99e937ae44aab580422
Piero Proietti <piero.proietti@gmail.com>
2020-11-16 08:31:17 +0100

    tmpfs

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -111,61 +111,61 @@ async function antiX() {
    gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,nodev,relatime,user_id=1000,group_id=1000 0 0
 */   
 
-shx.exec('rm /live -rf')
-shx.exec('mkdir /live')
-shx.exec('mkdir /live/aufs-ram -p')
+showexec('rm /live -rf')
+showexec('mkdir /live')
+showexec('mkdir /live/aufs-ram -p')
 // /dev/sr0 /live/boot-dev iso9660 ro,relatime,nojoliet,check=s,map=n,blocksize=2048 0 0
 // /dev/loop0 /live/linux squashfs ro,relatime 0 0
-shx.exec('mount -t tmpfs /live/aufs-ram tmpfs rw,noatime,size=1589248k') // 0 0
+showexec('mount -t tmpfs -o rw,noatime,size=1589248k tmpfs /live/aufs-ram')
 // overlay / overlay rw,relatime,lowerdir=/live/linux,upperdir=/live/aufs-ram/upper,workdir=/live/aufs-ram/work 0 0
-shx.exec('mount -t tmpfs /media tmpfs rw,noatime,size=10240k') // 0 0
-// shx.exec('mount -t tmpfs /run tmpfs rw,nosuid,nodev,noexec,noatime,size=204268k,mode=755') // 0 0
-shx.exec('mount -t tmpfs /live tmpfs rw,noatime,size=10240k,mode=755') // 0 0
-// shx.exec('mount -t tmpfs /tmp tmpfs rw,noatime') // 0 0
-// shx.exec('mount -t proc /proc proc rw,nosuid,nodev,noexec,relatime') 0 0
-// shx.exec('mount -t sys /sys sysfs rw,nosuid,nodev,noexec,relatime') 0 0
-// shx.exec('mount -t devtmpfs /dev devtmpfs rw,relatime,size=1015072k,nr_inodes=253768,mode=755') // 0 0
-// shx.exec('mount -t devpts /dev/pts devpts rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000') // 0 0
-shx.exec('mount -t overlay overlay -o lowerdir=/live/linux,upperdir=/live/aufs-ram/upper,workdir=/live/aufs-ram/work /live/aufs')
-shx.exec('mount -t tmpfs /etc/live/config tmpfs rw,noatime,size=10240k,mode=755') // 0 0
-shx.exec('mount -t tmpfs /etc/live/bin tmpfs rw,noatime,size=10240k,mode=755') // 0 0
-shx.exec('mount -t tmpfs /run/lock tmpfs rw,nosuid,nodev,noexec,relatime,size=5120k') // 0 0
-shx.exec('mount -t pstore /sys/fs/pstore pstore rw,relatime') // 0 0
-shx.exec('mount -t tmpfs /dev/shm tmpfs rw,nosuid,nodev,noexec,relatime,size=408520k') // 0 0
-shx.exec('mount -t rpc_pipefs /run/rpc_pipefs rpc_pipefs rw,relatime') // 0 0
-shx.exec('mount -t cgroup /sys/fs/cgroup tmpfs rw,relatime,size=12k,mode=755') // 0 0
-shx.exec('mount -t systemd /sys/fs/cgroup/systemd cgroup rw,nosuid,nodev,noexec,relatime,release_agent=/run/cgmanager/agents/cgm-release-agent.systemd,name=systemd') // 0 0
-shx.exec('mount -t tmpfs /run/user/1000 tmpfs rw,nosuid,nodev,relatime,size=204264k,mode=700,uid=1000,gid=1000') // 0 0
-shx.exec('mount -t gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,nodev,relatime,user_id=1000,group_id=1000') // 0 0
+showexec('mount -t tmpfs -o rw,noatime,size=10240k tmpfs /media')
+// showexec('mount -t tmpfs /run tmpfs rw,nosuid,nodev,noexec,noatime,size=204268k,mode=755') // 0 0
+showexec('mount -t tmpfs -o rw,noatime,size=10240k,mode=755 tmpfs /live')
+// showexec('mount -t tmpfs /tmp tmpfs rw,noatime', {silent: false}) // 0 0
+// showexec('mount -t proc /proc proc rw,nosuid,nodev,noexec,relatime', {silent: false}) // 0 0
+// showexec('mount -t sys /sys sysfs rw,nosuid,nodev,noexec,relatime', {silent: false}) // 0 0
+// showexec('mount -t devtmpfs /dev devtmpfs rw,relatime,size=1015072k,nr_inodes=253768,mode=755', {silent: false}) // 0 0
+// showexec('mount -t devpts /dev/pts devpts rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000, {silent: false}) // 0 0
+showexec('mount -t overlay overlay -o lowerdir=/live/linux,upperdir=/live/aufs-ram/upper,workdir=/live/aufs-ram/work /live/aufs', {silent: false}) // 0 0
+showexec('mount -t tmpfs -o rw,noatime,size=10240k,mode=755 tmpfs /etc/live/config')
+showexec('mount -t tmpfs -o rw,noatime,size=10240k,mode=755 tmpfs /etc/live/bin')
+showexec('mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=5120k tmpfs /run/lock')
+showexec('mount -t pstore /sys/fs/pstore pstore rw,relatime') // 0 0
+showexec('mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=408520k tmpfs /dev/shm')
+showexec('mount -t rpc_pipefs /run/rpc_pipefs rpc_pipefs rw,relatime') // 0 0
+showexec('mount -t cgroup /sys/fs/cgroup tmpfs rw,relatime,size=12k,mode=755') // 0 0
+showexec('mount -t systemd /sys/fs/cgroup/systemd cgroup rw,nosuid,nodev,noexec,relatime,release_agent=/run/cgmanager/agents/cgm-release-agent.systemd,name=systemd') // 0 0
+showexec('mount -F tmpfs /run/user/1000 tmpfs rw,nosuid,nodev,relatime,size=204264k,mode=700,uid=1000,gid=1000') // 0 0
+showexec('mount -t gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,nodev,relatime,user_id=1000,group_id=1000') // 0 0
 
 
    // monto su linuxfs il filesystem squash
-   // shx.exec('mkdir /live/boot-dev/antiX/linuxfs -p')
-   // shx.exec('ln -s /run/live/medium/live/filesystem.squashfs /live/boot-dev/antiX/linuxfs')
+   // showexec('mkdir /live/boot-dev/antiX/linuxfs -p')
+   // showexec('ln -s /run/live/medium/live/filesystem.squashfs /live/boot-dev/antiX/linuxfs')
 
-   // shx.exec('mkdir /live/linux')
-   // shx.exec('mount -t squashfs /live/boot-dev/antiX/linuxfs /live/linux'))
+   // showexec('mkdir /live/linux')
+   // showexec('mount -t squashfs /live/boot-dev/antiX/linuxfs /live/linux'))
 
-   // shx.exec(`mount -t overlay overlay -o lowerdir=/live/linux,upperdir=/run/live/overlay/rw,workdir=/run/live/overlay/work /live/linux`)
-   // shx.exec('ln -s /live/linux/home/live /live/linux/home/demo') 
+   // showexec(`mount -t overlay overlay -o lowerdir=/live/linux,upperdir=/run/live/overlay/rw,workdir=/run/live/overlay/work /live/linux`)
+   // showexec('ln -s /live/linux/home/live /live/linux/home/demo') 
    
-   // shx.exec('ln -s /run/live/medium /live/boot-dev')
+   // showexec('ln -s /run/live/medium /live/boot-dev')
    
    // Fino qua OK minstall parte
 
    // Creazione delle partizioni di mount
-   // shx.exec('mkdir /mnt/antiX/ -p')
-   // shx.exec('mkdir /mnt/antiX/boot/efi -p')
-   // shx.exec('mount --bind /boot/efi /mnt/antiX/boot/efi ')
+   // showexec('mkdir /mnt/antiX/ -p')
+   // showexec('mkdir /mnt/antiX/boot/efi -p')
+   // showexec('mount --bind /boot/efi /mnt/antiX/boot/efi ')
 
-   shx.exec('mkdir /mnt/antiX/proc -p')
-   shx.exec('mount --bind /proc /mnt/antiX/proc ')
+   showexec('mkdir /mnt/antiX/proc -p')
+   showexec('mount --bind /proc /mnt/antiX/proc ')
 
-   shx.exec('mkdir /mnt/antiX/sys -p')
-   shx.exec('mount --bind /sys /mnt/antiX/sys ')
+   showexec('mkdir /mnt/antiX/sys -p')
+   showexec('mount --bind /sys /mnt/antiX/sys ')
 
-   shx.exec('mkdir /mnt/antiX/dev -p')
-   shx.exec('mount --bind /dev /mnt/antiX/dev ')
+   showexec('mkdir /mnt/antiX/dev -p')
+   showexec('mount --bind /dev /mnt/antiX/dev ')
 
    console.log('exportimental!!!')
    console.log('Try to use:')
@@ -178,4 +178,10 @@ shx.exec('mount -t gvfsd-fuse /run/user/1000/gvfs fuse.gvfsd-fuse rw,nosuid,node
 
    //shx.exec('minstall')
 }
+
+function showexec (cmd = '') {
+   console.log( cmd )
+   shx.exec(cmd)
+}
+
 
