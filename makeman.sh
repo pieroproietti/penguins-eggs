4baed95fb1e84fa92797920b37650598816b7245
Piero Proietti <piero.proietti@postecert.it>
2021-01-19 16:13:38 +0100

    eggs1

--- makeman.sh
+++ makeman.sh
@@ -5,8 +5,9 @@ rm man/man1 -rf
 mkdir man/man1
 ronn --roff --html man/sources/eggs.md  --manual='eggs manual' --style=toc,80c --section 1 -o man/man1
 mv man/man1/eggs.md.1 man/man1/eggs.1
-gzip man/man1/eggs.1 
-sudo cp man/man1/eggs.1.gz /usr/local/man/man1/eggs.1
+gzip man/man1/eggs.1
+mv  man/man1/eggs.1.gz man/man1/eggs.1
+sudo cp man/man1/eggs.1 /usr/local/man/man1/eggs.1
 sudo mandb
 man eggs
 
