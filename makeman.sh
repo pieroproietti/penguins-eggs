97b4dd4f3c0c8fdbda5ff2242fbe97ced96cc292
Piero Proietti <piero.proietti@gmail.com>
2021-01-28 13:42:24 +0100

    vaffanculo

--- makeman.sh
+++ makeman.sh
@@ -6,11 +6,9 @@ cp README.md man/sources/eggs.md
 ronn --roff --html man/sources/eggs.md  --manual='eggs manual' --style=toc,80c --section 1 -o man/man1/
 mv man/man1/eggs.md.1 man/man1/eggs.1
 gzip man/man1/eggs.1
-mv  man/man1/eggs.1.gz man/man1/eggs.1
-
-#rm /usr/local/man/man1 -rf
-#mkdir -p /usr/local/man/man1
-#sudo cp man/man1/eggs.1 /usr/local/man/man1/eggs.1
-#sudo mandb
-#man eggs
+sudo mv  man/man1/eggs.1.gz man/man1/eggs.1
+sudo rm /usr/local/man/man1 -rf
+sudo mkdir -p /usr/local/man/man1
+sudo cp man/man1/eggs.1 /usr/local/man/man1/eggs.1
+sudo mandb
 
