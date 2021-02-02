571f5d5c7113de0c91ca8a15da97d354b0a11e41
Piero Proietti <piero.proietti@gmail.com>
2021-02-02 14:57:02 +0100

    resolving the question in dad

--- src/classes/daddy.ts
+++ src/classes/daddy.ts
@@ -103,7 +103,6 @@ export default class Daddy {
                 flags += '--max'
             }
 
-            if (config.theme !== '') {
                 flags += ` --theme=${config.theme} `
             }
             Utils.titles('produce' + ' ' + flags)
