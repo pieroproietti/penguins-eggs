65a227c61c1ac3acce34405dc4d413a36da35d5e
Piero Proietti <piero.proietti@gmail.com>
2021-03-08 06:01:50 +0100

    stat

--- src/commands/tools/statistics.ts
+++ src/commands/tools/statistics.ts
@@ -1,5 +1,6 @@
 import { Command, flags } from '@oclif/command'
 import Utils from '../../classes/utils'
+const yaml = require('js-yaml')
 
 export default class ToolsStatistics extends Command {
   static description = 'describe the command here'
@@ -12,15 +13,35 @@ export default class ToolsStatistics extends Command {
     year: flags.boolean({ char: 'y', description: 'stats year' }),
   }
 
-  static args = [{ name: 'file' }]
-
   static aliases = ['stat']
 
   async run() {
     const { args, flags } = this.parse(ToolsStatistics)
     Utils.titles(this.id + ' ' + this.argv)
-    const start = Utils.formatDate(new Date()).substr(0,10)
-    const end = Utils.formatDate(new Date()).substr(0,10)
+
+    let date_ob = new Date();
+
+    // current date
+    // adjust 0 before single digit date
+    let day = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    let end = year + '-' + month + '-' + day 
+    let start = year + '-' + month + '-' + day
+
+    if (flags.month) {
+      start= year + '-' + month + '-01' 
+    }
+
+    if (flags.year) {
+      start= year + '-01-01' 
+    }
+
 
     let url = 'https://sourceforge.net/projects/penguins-eggs/files/stats/json'
     const request = '?start_date=' + start + '&end_date=' + end
@@ -29,7 +50,7 @@ export default class ToolsStatistics extends Command {
     const axios = require('axios').default
     const res = await axios.get(url)
     const data = res.data
-    console.log(data)
+    console.log(yaml.dump(data))
 
   }
 }
