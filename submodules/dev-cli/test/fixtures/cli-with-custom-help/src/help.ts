2dddabb02cdc506555c94bbe26ea9d8da46bec75
Piero Proietti <piero.proietti@gmail.com>
2021-04-22 16:51:49 +0200

    rimossi i submodules - scomodi - ed introdotti i vendors... si spera meglio

--- submodules/dev-cli/test/fixtures/cli-with-custom-help/src/help.ts
+++
@@ -1,8 +0,0 @@
-import {Help} from '@oclif/plugin-help'
-import {Command} from '@oclif/config'
-
-export default class CustomHelp extends Help {
-  formatCommand(command: Command) {
-    return `Custom help for ${command.id}`
-  }
-}
--- submodules/perrisbrewery/src/interfaces/index.ts
+++
@@ -1,1 +0,0 @@
-export { IPackage } from './i-package'
