8a9c0cd083c4daedddd560d041240d1433e899eb
Piero Proietti <piero.proietti@postecert.it>
2021-05-21 18:02:21 +0200

    changed autologin shared with krill

--- src/lib/cli-autologin.ts
+++ src/lib/cli-autologin.ts
@@ -2,6 +2,7 @@ import shx = require('shelljs')
 import fs = require('fs')
 import path = require('path')
 import Utils from '../classes/utils'
+import Pacman from '../classes/pacman'
 
 // Comando per avviare ubiquity: sudo --preserve-env DBUS_SESSION_BUS_ADDRESS, XDG_RUNTIME sh -c 'calamares'
 
@@ -9,7 +10,7 @@ import Utils from '../classes/utils'
  * 
  * @param chroot 
  */
-export async function add(user: string, userPasswd: string, rootPasswd: string, chroot = '/') {
+export async function add(distro: string, version: string, user: string, userPasswd: string, rootPasswd: string, chroot = '/') {
     if (Utils.isSystemd()) {
         const fileOverride = `${chroot}/etc/systemd/system/getty@.service.d/override.conf`
         const dirOverride = path.dirname(fileOverride)
@@ -23,7 +24,7 @@ export async function add(user: string, userPasswd: string, rootPasswd: string, 
         content += 'ExecStart=-/sbin/agetty --noclear --autologin ' + user + ' %I $TERM' + '\n'
         fs.writeFileSync(fileOverride, content)
         shx.exec(`chmod +x ${fileOverride}`)
-        await motdAdd(user, userPasswd, rootPasswd, chroot)
+        await motdAdd(distro, version, user, userPasswd, rootPasswd, chroot)
     }
 }
 
@@ -46,19 +47,23 @@ export async function remove(chroot = '/') {
  * 
  * @param chroot 
  */
-async function motdAdd(user: string, userPasswd: string, rootPasswd: string, chroot = '/') {
+async function motdAdd(distro: string, version: string, user: string, userPasswd: string, rootPasswd: string, chroot = '/') {
     const fileMotd = `${chroot}/etc/motd`
+
+    let installer = 'sudo eggs install'
+    if (Pacman.packageIsInstalled('krill')) {
+        installer = 'sudo krill install'
+    } else if (Pacman.packageIsInstalled('calamares')) {
+        installer = 'calamares'
+    }
 
     motdRemove(chroot)
 
     let eggsMotd = fs.readFileSync(fileMotd, 'utf-8')
     eggsMotd += '>>> eggs\n'
-    eggsMotd += 'This is a live eggs system!\n'
+    eggsMotd += `This is a live ${distro}/${version} system createb by penguin's eggs.\n`
     eggsMotd += `You are logged as ${user}, your password is: ${userPasswd}. root password: ${rootPasswd}\n`
-    eggsMotd += 'After installation you can add your favorite desktop, for example xfce4, cinnammon, \n'
-    eggsMotd += 'mate, gnome, kde, etc and use the calamares graphical installation\n'
-    eggsMotd += 'You can hatch the egg with inside cli installer: sudo eggs install\n'
-    eggsMotd += 'or with krill: sudo krill install\n'
+    eggsMotd += `to install the system, type: ${installer}\n`
     eggsMotd += 'eggs <<<\n'
     fs.writeFileSync(fileMotd, eggsMotd)
 }
@@ -71,22 +76,23 @@ async function motdRemove(chroot = '/') {
     const fileMotd = `${chroot}/etc/motd`
     let motd = fs.readFileSync(fileMotd, 'utf-8').split('\n')
     let cleanMotd = ''
-    const eggsMessageStart = '>>> eggs'
-    let isEggsMessage = false
-    const eggsMessageStop = 'eggs <<<'
+    let remove = false
+    const startRemove = '>>> eggs'
+    const stopRemove = 'eggs <<<'
     for (let i = 0; i < motd.length; i++) {
-        if (motd[i].includes(eggsMessageStart)) {
-            isEggsMessage = true
+        if (motd[i].includes(startRemove)) {
+            remove = true
+        } 
+        if (!remove) {
+            if (motd[i]!=='\n') {
+                cleanMotd += motd[i] + '\n'
+            }
         }
-
-        if (!isEggsMessage) {
-            cleanMotd += motd[i] + '\n'
-        }
-
-        if (motd[i].includes(eggsMessageStop)) {
-            isEggsMessage = false
+        if (motd[i].includes(stopRemove)) {
+            remove = false
         }
     }
+    cleanMotd+='\n'
     fs.writeFileSync(fileMotd, cleanMotd)
 }
 
