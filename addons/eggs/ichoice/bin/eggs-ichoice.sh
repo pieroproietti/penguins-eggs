dd3222d58fba023ec82c9413551decbe428cf48b
Piero Proietti <piero.proietti@gmail.com>
2021-05-26 19:33:25 +0200

    parecchie sistemazioni, forzo la sovrascrittura del fs

--- addons/eggs/ichoice/bin/eggs-ichoice.sh
+++ addons/eggs/ichoice/bin/eggs-ichoice.sh
@@ -9,8 +9,7 @@ function main() {
       answer=$(
       whiptail --title "penguin's eggs" --menu "Choose the system installer" 22 75 10 \
          "gui"      "use gui installer calamares (reccomanded)" \
-         "krill"      "use CLI installer krill (desktop with few RAM)" \
-         "eggs"     "use CLI installer eggs (servers just CLI)" \
+         "cli"      "use CLI installer eggs (servers just CLI)" \
          "quit"     "exit" 3>&2 2>&1 1>&3
       )
 
@@ -18,11 +17,8 @@ function main() {
          "quit")
             theEnd ;;
 
-         "eggs")
+         "cli")
             eggs ;;
-
-         "krill")
-            krill ;;
 
          "gui")
             calamares ;;
@@ -32,11 +28,6 @@ function main() {
 ################################
 function eggs {
    sudo eggs install --cli
-}
-
-################################
-function krill {
-   sudo krill install
 }
 
 ################################
