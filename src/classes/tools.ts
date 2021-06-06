8d264aa02fa905ebb9e7f42003277c31e90dcd9b
Piero Proietti <piero.proietti@postecert.it>
2021-06-06 20:19:50 +0200

    sempre ia32 al posto di i386

--- src/classes/tools.ts
+++ src/classes/tools.ts
@@ -14,15 +14,15 @@ import Settings from './settings'
 // import { IConfigTools } from '../interfaces/i-config-tools'
 
 interface IConfigTools {
-    remoteHost: string
-    remoteUser: string
-    remotePathDeb: string
-    remotePathDoc: string
-    remotePathIso: string
-    localPathDeb: string
-    localPathDoc: string
-    localPathIso: string
-    filterDeb: string
+  remoteHost: string
+  remoteUser: string
+  remotePathDeb: string
+  remotePathDoc: string
+  remotePathIso: string
+  localPathDeb: string
+  localPathDoc: string
+  localPathIso: string
+  filterDeb: string
 }
 
 /**
@@ -30,59 +30,59 @@ interface IConfigTools {
  * @remarks all the utilities
   */
 export default class Tools {
-    tools_yaml = '/etc/penguins-eggs.d/tools.yaml'
+  tools_yaml = '/etc/penguins-eggs.d/tools.yaml'
 
-    snapshot_dir = ''
+  snapshot_dir = ''
 
-    snapshot_name = ''
+  snapshot_name = ''
 
-    config = {} as IConfigTools
+  config = {} as IConfigTools
 
-    /*
-    * Load configuration from /etc/penguins-eggs.yaml
-    * @returns {boolean} Success
-    */
-    async loadSettings(): Promise<boolean> {
-        let foundSettings = false
+  /*
+  * Load configuration from /etc/penguins-eggs.yaml
+  * @returns {boolean} Success
+  */
+  async loadSettings(): Promise<boolean> {
+    let foundSettings = false
 
-        if (fs.existsSync(this.tools_yaml)) {
-            foundSettings = true
-            const loaded = yaml.load(fs.readFileSync(this.tools_yaml, 'utf-8')) as IConfigTools
-            this.config.remoteHost = loaded.remoteHost
-            this.config.remoteUser = loaded.remoteUser
-            this.config.remotePathDeb = loaded.remotePathDeb
-            this.config.remotePathDoc = loaded.remotePathDoc
-            this.config.remotePathIso = loaded.remotePathIso
+    if (fs.existsSync(this.tools_yaml)) {
+      foundSettings = true
+      const loaded = yaml.load(fs.readFileSync(this.tools_yaml, 'utf-8')) as IConfigTools
+      this.config.remoteHost = loaded.remoteHost
+      this.config.remoteUser = loaded.remoteUser
+      this.config.remotePathDeb = loaded.remotePathDeb
+      this.config.remotePathDoc = loaded.remotePathDoc
+      this.config.remotePathIso = loaded.remotePathIso
 
-            this.config.localPathDeb = loaded.localPathDeb
-            this.config.localPathDoc = loaded.localPathDoc
-            this.config.localPathIso = loaded.localPathIso
+      this.config.localPathDeb = loaded.localPathDeb
+      this.config.localPathDoc = loaded.localPathDoc
+      this.config.localPathIso = loaded.localPathIso
 
-            this.config.filterDeb  = loaded.filterDeb
+      this.config.filterDeb = loaded.filterDeb
 
-            /**
-             * da eggs
-             */
-            const settings = new Settings()
-            settings.load()
-            /*
-            let arch = 'x64'
-            if (process.arch === 'i386') {
-              arch = 'i386'
-            } else if (process.arch === 'arm64') {
-              arch = 'arm64'
-            } else if (process.arch === 'armel') {
-              arch = 'armel'
-            }
-            */
-            this.snapshot_dir = settings.config.snapshot_dir
-            this.snapshot_name = settings.config.snapshot_prefix + settings.config.snapshot_basename + '-' + process.arch + '_'
-        } else {
-            console.log(`Can't find: ${this.tools_yaml}`)
-            process.exit(1)
-        }
-        return foundSettings
+      /**
+       * da eggs
+       */
+      const settings = new Settings()
+      settings.load()
+      let arch = 'amd64'
+      if (process.arch === 'x64') {
+        arch = 'amd64'
+      } else if (process.arch === 'ia32') {
+        arch = 'i386'
+      } else if (process.arch === 'arm64') {
+        arch = 'arm64'
+      } else if (process.arch === 'armel') {
+        arch = 'armel'
+      }
+      this.snapshot_dir = settings.config.snapshot_dir
+      this.snapshot_name = settings.config.snapshot_prefix + settings.config.snapshot_basename + '-' + arch + '_'
+    } else {
+      console.log(`Can't find: ${this.tools_yaml}`)
+      process.exit(1)
     }
+    return foundSettings
+  }
 }
 
 
