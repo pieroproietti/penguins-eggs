45c03edf7ee27be1d2c15dc89976fe0506b70c80
Piero Proietti <piero.proietti@postecert.it>
2021-06-25 06:55:37 +0200

    ok static and dhcp configuration

---
+++ src/lib/get_address.ts
@@ -0,0 +1,21 @@
+
+'use strict'
+import inquirer = require('inquirer')
+
+export default async function getAddress(initial: string): Promise<string> {
+
+   return new Promise(function (resolve) {
+      const questions: Array<Record<string, any>> = [
+         {
+            type: 'input',
+            name: 'address',
+            message: "What is ip address of this computer? ",
+            default: initial
+         },
+      ]
+
+      inquirer.prompt(questions).then(function (options) {
+         resolve(options.address)
+      })
+   })
+}
