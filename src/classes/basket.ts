566ce31d42c15640a9e89eae69e7a464fe4bd526
Piero Proietti <piero.proietti@gmail.com>
2021-10-20 12:09:42 +0200

    Rimosso controllo https di axios.get problemi di individuazione certificato

--- src/classes/basket.ts
+++ src/classes/basket.ts
@@ -69,9 +69,18 @@ export default class Basket {
         }
 
         const url = 'https://penguins-eggs.net/versions/all/' + Utils.eggsArch() + '/'
-        const axios = require('axios').default
+        //const axios = require('axios').default
+        const axios = require('axios')
 
-        const res = await axios.get(url)
+        console.log(url)
+        const https = require('https');
+        // At request level
+        const agent = new https.Agent({
+        rejectUnauthorized: false
+        });
+
+        const res = await axios.get(url, { httpsAgent: agent })
+        //const res = await axios.get(url)
         const data = res.data
 
         // Ordino le versioni
