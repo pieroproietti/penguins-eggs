4bb54f04f71309751d14b8f0eb6a3ef6ae46f82a
Piero Proietti <piero.proietti@gmail.com>
2021-12-09 16:24:09 +0100

    vfs mount

--- dracut/fedora/colorpatch.sh
+++ dracut/fedora/colorpatch.sh
@@ -7,3 +7,10 @@ mkdir /eggs/.workdir
 mount /dev/sr0 /eggs/medium
 mount -t squashfs /eggs/medium/live/filesystem.squashfs /eggs/.lowerdir -o loop
 mount -t overlay overlay -o lowerdir=/eggs/.lowerdir,upperdir=/eggs/.upperdir,workdir=/eggs/.workdir /sysroot
+# Now we need to mount vfs
+mount -o bind /dev /sysroot/dev
+mount -o bind /dev/pts /sysroot/dev/pts
+mount -o bind /proc /sysroot/proc
+mount -o bind /sys /sysroot/sys
+mount -o bind /run /sysroot/run
+
