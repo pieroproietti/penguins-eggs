81a5bd7f4a243d273b5d59909324ece727e480f0
Piero Proietti <piero.proietti@gmail.com>
2021-12-14 17:57:53 +0100

    verso la versione 9.0.0

---
+++ src/commands/info.ts
@@ -0,0 +1,33 @@
+/**
+ * info
+ */
+
+import { Command, Flags }from '@oclif/core'
+import information from '../components/elements/information'
+
+/**
+ * 
+ */
+export default class Info extends Command {
+   static description = 're-thinking for a different approach to CLI'
+
+   static flags = {
+      verbose: Flags.boolean({ char: 'v' }),
+      help: Flags.help({ char: 'h' })
+   }
+
+   /**
+    * 
+    */
+   async run() :Promise <void> {
+      const { flags } = await this.parse(Info)
+
+      let verbose = false
+      if (flags.verbose) {
+         verbose = true
+      }
+
+      await information(verbose)
+   }
+}
+
