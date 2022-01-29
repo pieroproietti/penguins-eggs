6635ac2065b687d7d6fe0e4adcee2b67ad93b08c
pieroproietti <piero.proietti@gmail.com>
2022-01-29 12:01:23 +0100

    building a new backup users & servers

--- src/classes/users.ts
+++ src/classes/users.ts
@@ -1,8 +1,13 @@
 /**
  * User
  */
+import { integer } from '@oclif/core/lib/parser/flags'
+import { flagUsage } from '@oclif/core/lib/parser/help'
 import fs from 'fs'
 import { exec } from '../lib/utils'
+import { access } from 'fs/promises'
+import { constants } from 'fs'
+
 
 /**
  * 
@@ -37,57 +42,96 @@ export default class Users {
     * @param verbose
     */
     async getValues() {
+        let fLevels = [
+            { path: 'bin', saveIt: false },
+            { path: 'boot', saveIt: false },
+            { path: 'dev', saveIt: false },
+            { path: 'etc', saveIt: false },
+            { path: 'lib', saveIt: false },
+            { path: 'lib32,', saveIt: false },
+            { path: 'libx32', saveIt: false },
+            { path: 'lib64', saveIt: false },
+            { path: 'home', saveIt: true },
+            { path: 'media', saveIt: false },
+            { path: 'mnt', saveIt: false },
+            { path: 'opt', saveIt: true },
+            { path: 'proc', saveIt: false },
+            { path: 'root', saveIt: true },
+            { path: 'run', saveIt: false },
+            { path: 'sbin', saveIt: false },
+            { path: 'sys', saveIt: false },
+            { path: 'srv', saveIt: true },
+            { path: 'tmp', saveIt: false },
+            { path: 'usr', saveIt: true },
+            { path: 'var', saveIt: true }
+        ]
+
         let hasHome = false
         let saveIt = false
         let size = 0
 
-        if (fs.existsSync(this.home)) {
-            hasHome = true
-            const sizeUser = await exec(` du --block-size=1 --summarize ${this.home} | awk '{print $1}'`, { echo: false, ignore: false, capture: true })
-            size = Number.parseInt(sizeUser.data)
-        }
-
-        /**
-         * /dev, /proc, /run, /sys, /tmp
-         * are excluded to be save
-         */
         if (this.home != undefined) {
-            if (this.home.substring(0, 4) === '/dev/' ||
-            this.home.substring(0, 4) === '/proc' ||
-            this.home.substring(0, 4) === '/run/' ||
-            this.home.substring(0, 4) === '/sys/' ||
-            this.home.substring(0, 4) === '/tmp/') {
-                size = 0
-            } else {
+            /**
+             * analyze firstLevel and 
+             * excluded to be saved
+             */
+            let fLevel = this.home.split('/')[1]
+            for (let i = 0; i < fLevels.length; i++) {
+                if (fLevels[i].path === fLevel) {
+                    saveIt = fLevels[i].saveIt
+                }
+            }
+
+            /**
+             * analize second level
+             * examples: /var/run, /var/cache, /var/spool, etc
+             */
+            if (saveIt) {
+                let sLevel = this.home.split('/')[2]
+                if (sLevel === 'cache' || sLevel === 'run' || sLevel === 'spool') {
+                    saveIt = false
+                }
+            }
+
+            if (saveIt) {
+
                 switch (this.home) {
-                    case '/':
-                    case '/root':
-                    case '/not/existent': {
-                        size = 0
-                        break
-                    }
                     /**
-                     * under /usr
+                     * exclude always /
+                     */
+                    case '/':
+                        {
+                            break
+                        }
+
+                    /**
+                     * excludes: under /usr
                      */
                     case '/usr/bin':
                     case '/usr/sbin': {
-                        size = 0
+                        saveIt = false
                         break
                     }
 
                     /** 
-                     * under var
+                     * excludes: under var
                      */
                     case '/var/backups':
                     case '/var/lib/colord':
                     case '/var/lib/geoclue':
                     case '/var/lib/misc':
-                    case '/var/mail': {
-                        size = 0
-                        break
-                    }
+                    case '/var/mail':
+                        {
+                            saveIt = false
+                            break
+                        }
 
                     default: {
+                        if (fs.existsSync(this.home)) {
+                            hasHome = true
+                            const sizeUser = await exec(` du --block-size=1 --summarize ${this.home} | awk '{print $1}'`, { echo: false, ignore: false, capture: true })
+                            size = Number.parseInt(sizeUser.data)
+                        }
                         saveIt = true
                         break
                     }
@@ -96,6 +140,6 @@ export default class Users {
             this.saveIt = saveIt
             this.size = size
             this.hasHome = hasHome
-}
+        }
     }
 }
