3ad96f2c087ee445097819552d83cc982340b1e3
Piero Proietti <piero.proietti@gmail.com>
2022-02-15 09:05:28 +0100

    working EFI, EFI encrypted, BIOS

--- yolk-save.sh
+++ yolk-save.sh
@@ -1,2 +1,13 @@
+clear
+echo "Installing eggs saving time..."
+# yolk-restore
 sudo rm /var/local/yolk.saved -rf
 sudo mv /var/local/yolk /var/local/yolk.saved
+# removing eggs
+sudo apt purge eggs
+sudo dpkg -i perrisbrewery/workdir/eggs_*_amd64.deb
+sudo eggs dad -d
+# yolk-restore
+sudo rm /var/local/yolk -rf
+sudo mv /var/local/yolk.saved /var/local/yolk
+
