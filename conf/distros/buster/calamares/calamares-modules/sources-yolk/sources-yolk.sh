12ea6172af67398c39502bfbc91b4d00b7bf9702
Piero Proietti <piero.proietti@gmail.com>
2022-03-04 11:58:26 -0500

    sources-yolk

--- conf/distros/buster/calamares/calamares-modules/sources-yolk/sources-yolk.sh
+++ conf/distros/buster/calamares/calamares-modules/sources-yolk/sources-yolk.sh
@@ -7,80 +7,99 @@
 # sources-yolk -u
 # rimuove yolk e reimposta le apt-list originali
 
+
+##############################
+# function main {
+##############################
 function main {
-    #####################################################################
-    # unmount: remove yolk.list
-    #####################################################################
+    # if u = unmount restore_apt
     if [ "$1" = "-u" ]; then
+        clean_apt
         restore_apt
     else
+        # backup_apt, yolk
+        clean_backup
         backup_apt
+
+        clean_apt
         yolk
     fi
 }
 
 
+
 ##############################
-# * cancella backup
-# * crea dir backop
-# ° mv sources.list in backup
-# ° mv sources.list.d in backup
+# function backup_apt
 ##############################
 function backup_apt {
-    if [ -d "$APT_BACKUP" ]; then
-        rm "$APT_BACKUP" -rf
-    fi
     mkdir "$APT_BACKUP" -p
-
     if [ -f "$APT_ROOT/sources.list" ]; then
         mv "$APT_ROOT/sources.list" "$APT_BACKUP"
-    else
-        restore_apt
     fi
-    mv "$APT_ROOT/sources.list.d/" "$APT_BACKUP"
+    if [ -d "$APT_ROOT/sources.list.d" ]; then
+        mv "$APT_ROOT/sources.list.d/" "$APT_BACKUP"
+    fi
 }
 
+
+
 ##############################
-# * cancella sources.list in apt
-# * cancella sources.list.d in apt
-# ° mv sources.list in apt
-# ° mv sources.list.d in apt
+# function restore_apt
 ##############################
 function restore_apt {
+    mv "$APT_BACKUP/sources.list" "$APT_ROOT"
+    mv "$APT_BACKUP/sources.list.d/" "$APT_ROOT"
+}
+
+
+
+##############################
+# function: clean_apt
+##############################
+function clean_apt {
     if [ -f "$APT_ROOT/sources.list" ]; then
         rm "$APT_ROOT/sources.list"
     fi
     if [ -d "$APT_ROOT/sources.list.d" ]; then
         rm "$APT_ROOT/sources.list.d" -rf
     fi
-
-    mv "$APT_BACKUP/sources.list" "$APT_ROOT"
-    mv "$APT_BACKUP/sources.list.d/" "$APT_ROOT"
 }
 
+
 ##############################
-# se non esiste crea sources.list.d
-# crea sources.list.d/yolk.list
-# esegue update
+# function: clean_backup
+##############################
+function clean_backup {
+    if [ -d "$APT_BACKUP" ]; then
+        rm "$APT_BACKUP" -rf
+    fi
+}
+
+
+
+##############################
+# function yolk {
 ##############################
 function yolk {
+
     mkdir "$APT_ROOT/sources.list.d"
+
 cat << EOF > "$CHROOT/etc/apt/sources.list.d/yolk.list"
 deb [trusted=yes] file:///var/local/yolk ./
 EOF
-chroot "${CHROOT}" apt-get --allow-unauthenticated update -y
+
+    # apt-get update
+    chroot "${CHROOT}" apt-get --allow-unauthenticated update -y
 }
 
-# Lo script inizia qui
-# attenzione agli spazi, controllare tutto
-# CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
-CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
 
+
+##############################
+# PROCESS START HERE!!!
+# CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
+##############################
+CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
 APT_ROOT="${CHROOT}/etc/apt"
 APT_BACKUP="/tmp/calamares-krill-temp"
-# clear
-echo "APT_ROOT: $APT_ROOT"
-echo "APT_BACKUP: $APT_BACKUP"
-
 main "$1"
 exit 0
