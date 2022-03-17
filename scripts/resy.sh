aa398725c75597c704f8abb97264785e086985b6
Piero Proietti <piero.proietti@gmail.com>
2022-03-17 03:02:41 -0400

    gardrobe.d wardrobe.d introduced resy.sh

---
+++ scripts/resy.sh
@@ -0,0 +1,7 @@
+clear
+echo "Re-onstall Eggs Saving Yolk..."
+sudo mv /var/local/yolk .
+sudo apt purge eggs 
+sudo dpkg -i /tmp/eggs*.deb
+sudo mv ./yolk /var/loca
+sudo eggs dad -d
--- yolk-restore.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo rm /var/local/yolk -rf
-sudo mv /var/local/yolk.saved /var/local/yolk
