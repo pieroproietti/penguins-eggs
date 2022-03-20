d590f029a7330858105fc51b9c035468ccd77ba6
Piero Proietti <piero.proietti@gmail.com>
2022-03-20 03:54:39 -0400

    revision on tailor, check if something exists before to do

--- src/classes/tailor.ts
+++ src/classes/tailor.ts
@@ -51,199 +51,212 @@ export default class Tailor {
         /**
          * Repositories
          */
+        if (this.materials.sequence.repositories === undefined) {
+            console.log('repositiories, and repositories.update MUDE be defined on sequence ')
+            process.exit()
+        }
         Utils.warning(`analyzing repositories`)
 
         /**
         * sources.list
         */
-        let step = '/etc/apt/sources.list'
-        Utils.warning(step)
-        let components = ''
+        if (this.materials.sequence.repositories.sourcesList !== undefined) {
+            let step = '/etc/apt/sources.list'
 
-        if (this.materials.sequence.repositories.sourcesList.main) {
-            components += ' main'
-        }
+            Utils.warning(step)
+            let components = ''
 
-        if (this.materials.sequence.repositories.sourcesList.contrib) {
-            components += ' contrib'
-        }
+            if (this.materials.sequence.repositories.sourcesList.main) {
+                components += ' main'
+            }
 
-        if (this.materials.sequence.repositories.sourcesList.nonFree) {
-            components += ' non-free'
+            if (this.materials.sequence.repositories.sourcesList.contrib) {
+                components += ' contrib'
+            }
+
+            if (this.materials.sequence.repositories.sourcesList.nonFree) {
+                components += ' non-free'
+            }
+            console.log(`using: ${components}`)
         }
-        console.log(`using: ${components}`)
 
         /**
          * sources.list.d
          */
-        if (this.materials.sequence.repositories.sourcesListD[0] !== null) {
-            step = `adding repositories to /etc/apt/sources.list.d`
-            Utils.warning(step)
-            this.materials.sequence.repositories.sourcesListD.forEach(async cmd => {
-                try {
-                    await exec(cmd, this.echo)
-                } catch (error) {
-                    await Utils.pressKeyToExit(JSON.stringify(error))
-                }
-            })
+        if (this.materials.sequence.repositories.sourcesListD !== undefined) {
+            if (this.materials.sequence.repositories.sourcesListD[0] !== null) {
+                let step = `adding repositories to /etc/apt/sources.list.d`
+                Utils.warning(step)
+                this.materials.sequence.repositories.sourcesListD.forEach(async cmd => {
+                    try {
+                        await exec(cmd, this.echo)
+                    } catch (error) {
+                        await Utils.pressKeyToExit(JSON.stringify(error))
+                    }
+                })
+            }
         }
+
 
         /**
          * apt-get update
          */
-        step = `updating repositories`
+        if (this.materials.sequence.repositories.update === undefined) {
+            console.log('repositiories, and repositories.update MUDE be defined on sequence ')
+            process.exit()
+        }
+        let step = `updating repositories`
         Utils.warning(step)
         if (this.materials.sequence.repositories.update) {
             if (!this.verbose) {
-                console.log('wait for: ' + step)
+                console.log('waiting for: ' + step)
             }
             await exec('apt-get update', Utils.setEcho(false))
         }
 
+
         /**
          * apt-get full-upgrade
          */
-        step = `apt-get full-upgrade`
-        Utils.warning(step)
-        if (this.materials.sequence.repositories.fullUpgrade) {
-            if (!this.verbose) {
-                console.log('wait for: ' + step)
+        if (this.materials.sequence.repositories.fullUpgrade !== undefined) {
+            let step = `apt-get full-upgrade`
+            Utils.warning(step)
+            if (this.materials.sequence.repositories.fullUpgrade) {
+                await exec('apt-get full-upgrade -y', Utils.setEcho(false))
             }
-            await exec('apt-get full-upgrade -y', Utils.setEcho(false))
         }
 
         /**
-         * checking tools
+         * checking dependencies
          */
-        if (this.materials.sequence.dependencies[0] !== null) {
-            let cmd = 'apt-get install -y '
-            let dependencies = ''
-            this.materials.sequence.dependencies.forEach(dependence => {
-                cmd += ` ${dependence}`
-                dependencies += `, ${dependence}`
-            })
-            step = `installing dependencies: ${dependencies.substring(1)}`
-            Utils.warning(step)
-            if (!this.verbose) {
-                console.log('wait for: ' + step)
+        if (this.materials.sequence.dependencies !== undefined) {
+            if (this.materials.sequence.dependencies[0] !== null) {
+                let cmd = 'apt-get install -y '
+                let dependencies = ''
+                this.materials.sequence.dependencies.forEach(dependence => {
+                    cmd += ` ${dependence}`
+                    dependencies += `, ${dependence}`
+                })
+                let step = `installing dependencies: ${dependencies.substring(1)}`
+                Utils.warning(step)
+                await exec(cmd, this.echo)
             }
-            await exec(cmd, this.echo)
         }
 
 
         /**
          * apt-get install packages
          */
-        if (this.materials.sequence.packages[0] !== null) {
-            let packages = ''
-            let cmd = 'apt-get install -y '
-            this.materials.sequence.packages.forEach(elem => {
-                cmd += ` ${elem}`
-                packages = `, ${elem}`
-            })
-            step = `installing packages: ${packages.substring(1)}`
-            Utils.warning(step)
-            if (!this.verbose) {
-                console.log('wait for: ' + step)
+        if (this.materials.sequence.packages !== undefined) {
+            if (this.materials.sequence.packages[0] !== null) {
+                let packages = ''
+                let cmd = 'apt-get install -y '
+                this.materials.sequence.packages.forEach(elem => {
+                    cmd += ` ${elem}`
+                    packages += `, ${elem}`
+                })
+                let step = `installing packages: ${packages.substring(2)}`
+                Utils.warning(step)
+                await exec(cmd, this.echo)
             }
-            await exec(cmd, this.echo)
         }
 
         /**
         * apt-get install --no-install-recommends --no-install-suggests
         */
-        if (this.materials.sequence.noInstallRecommends[0] !== null) {
-            let cmd = 'apt-get install --no-install-recommends --no-install-suggests -y '
-            let noInstallRecommends = ''
-            this.materials.sequence.noInstallRecommends.forEach(elem => {
-                cmd += ` ${elem}`
-                noInstallRecommends += `, ${elem}`
-            })
-            step = `installing packages --no-install-recommends --no-install-suggests ${noInstallRecommends.substring(1)}`
-            Utils.warning(step)
-            if (!this.verbose) {
-                console.log('wait for: ' + step)
+        if (this.materials.sequence.noInstallRecommends !== undefined) {
+
+            if (this.materials.sequence.noInstallRecommends[0] !== null) {
+                let cmd = 'apt-get install --no-install-recommends --no-install-suggests -y '
+                let noInstallRecommends = ''
+                this.materials.sequence.noInstallRecommends.forEach(elem => {
+                    cmd += ` ${elem}`
+                    noInstallRecommends += `, ${elem}`
+                })
+                let step = `installing packages --no-install-recommends --no-install-suggests ${noInstallRecommends.substring(2)}`
+                Utils.warning(step)
+                await exec(cmd, this.echo)
             }
-            await exec(cmd, this.echo)
         }
 
         /**
          * dpkg -i *.deb
          */
-        if (this.materials.sequence.debs) {
-            step = `installing local packages`
-            Utils.warning(step)
-            if (!this.verbose) {
-                console.log('wait for: ' + step)
+        if (this.materials.sequence.debs !== undefined) {
+
+            if (this.materials.sequence.debs) {
+                let step = `installing local packages`
+                Utils.warning(step)
+                await exec(`dpkg -i ${this.wardrobe}\*.deb`)
             }
-            await exec(`dpkg -i ${this.wardrobe}\*.deb`)
         }
 
         /**
          * customizations/scripts
          */
-        if (this.materials.sequence.customizations.scripts[0] !== null) {
-            step = `customizations scripts`
-            Utils.warning(step)
-            if (!this.verbose) {
-                console.log('wait for: ' + step)
+        if (this.materials.sequence.customizations.scripts !== undefined) {
+            if (this.materials.sequence.customizations.scripts[0] !== null) {
+                let step = `customizations scripts`
+                Utils.warning(step)
+                this.materials.sequence.customizations.scripts.forEach(async script => {
+                    await exec(`${this.wardrobe}/${this.costume}/${script}`, Utils.setEcho(true))
+                })
             }
-            this.materials.sequence.customizations.scripts.forEach(async script => {
-                await exec(`${this.wardrobe}/${this.costume}/${script}`, Utils.setEcho(true))
-            })
         }
 
         /**
          * customizations/skel
          */
-        if (this.materials.sequence.customizations.skel) {
-            step = `customizations skel`
-            if (fs.existsSync(`${this.wardrobe}/skel`)) {
-                Utils.warning(step)
-                if (!this.verbose) {
-                    console.log('wait for: ' + step)
+        if (this.materials.sequence.customizations.skel !== undefined) {
+            if (this.materials.sequence.customizations.skel) {
+                let step = `customizations skel`
+                if (fs.existsSync(`${this.wardrobe}/${this.costume}/skel`)) {
+                    Utils.warning(step)
+                    await exec(`rm /etc/skel -rf`)
+                    await exec(`mkdir /etc/skel`)
+                    await exec(`cp -r ${this.wardrobe}/${this.costume}/skel/.* /etc/skel/`)
+                    // copy skel in current user
+                    // await exec(`cp -r ${this.wardrobe}/${this.costume}/skel/.* ~/ `)
+                } else {
+                    Utils.warning(`${this.wardrobe}/${this.costume}/skel not found!`)
                 }
-                await exec(`rm /etc/skel -rf`)
-                await exec(`mkdir /etc/skel`)
-                await exec(`cp -r ${this.wardrobe}/skel/.* /etc/skel/`)
-                // copy skel in current user
-                await exec(`cp -r ${this.wardrobe}/skel/.* ~/ `)
-            } else {
-                Utils.warning(`${this.wardrobe}/skel not found!`)
             }
         }
 
         /**
          * customizations/usr
          */
-        if (this.materials.sequence.customizations.usr) {
-            step = `customizations usr`
-            if (fs.existsSync(`${this.wardrobe}/skel`)) {
-                Utils.warning(step)
-                if (!this.verbose) {
-                    console.log('wait for: ' + step)
+        if (this.materials.sequence.customizations.usr !== undefined) {
+            if (this.materials.sequence.customizations.usr) {
+                let step = `customizations usr`
+                if (fs.existsSync(`${this.wardrobe}/${this.costume}/usr`)) {
+                    Utils.warning(step)
+                    await exec(`cp -r ${this.wardrobe}/${this.costume}/usr/* /usr/`)
+                } else {
+                    Utils.warning(`${this.wardrobe}/${this.costume}/usr not found!`)
                 }
-                await exec(`cp -r ${this.wardrobe}/usr/* /usr/`)
-            } else {
-                Utils.warning(`${this.wardrobe}/usr not found!`)
             }
         }
 
         /**
          * hostname and hosts
          */
-        if (this.materials.sequence.hostname) {
-            Utils.warning(`changing hostname = ${this.materials.name}`)
-            await this.hostname()
+        if (this.materials.sequence.hostname !== undefined) {
+            if (this.materials.sequence.hostname) {
+                Utils.warning(`changing hostname = ${this.materials.name}`)
+                await this.hostname()
+            }
         }
 
         /**
          * reboot
          */
-        if (this.materials.sequence.reboot) {
-            Utils.warning(`Reboot`)
-            await Utils.pressKeyToExit('system need to reboot', true)
-            await exec('reboot')
+        if (this.materials.sequence.reboot !== undefined) {
+            if (this.materials.sequence.reboot) {
+                Utils.warning(`Reboot`)
+                await Utils.pressKeyToExit('system need to reboot', true)
+                await exec('reboot')
+            }
         }
     }
 
