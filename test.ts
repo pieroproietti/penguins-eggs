a37bc88cce64929eb4dd5ad2d49c60aba66df176
Piero Proietti <piero.proietti@gmail.com>
2022-03-22 12:11:55 -0400

    rimessa copia dirs precedentemente cancellata

--- test.ts
+++ test.ts
@@ -7,10 +7,34 @@
 
 import { exec } from './src/lib/utils'
 import Utils from './src/classes/utils'
+import path from 'path'
+import fs from 'fs'
+import N8 from  './src/classes/n8'
 
 console.clear()
 
-testuser('artisan')
+// testuser('artisan')
+
+// const filesInSkel: string [] = []
+for (const elem of fs.readdirSync(`/etc/skel/`, {withFileTypes: true})) {
+   if (elem.isDirectory()) {
+      console.log(`dir: ${elem.name}`)
+   } else {
+      console.log(`file: ${elem.name}`)
+   }
+   
+}
+// [ '.bash_logout', '.bashrc', '.config', '.profile' ]
+
+
+
+const files: string [] = []
+for (const elem of fs.readdirSync(`/etc/skel/`)) {
+    files.push(path.basename(elem))
+}
+//[ '.bash_logout', '.bashrc', '.config', '.profile' ]
+
+console.log(files)
 
 async function testuser (user = '') {
    let cmd = `#!/bin/sh\ngetent passwd "${user}"  > /dev/null`
