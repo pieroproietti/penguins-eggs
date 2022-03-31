967f58249dc4940ef9c92dd27d3212a1f06db560
Piero Proietti <piero.proietti@gmail.com>
2022-03-31 07:20:00 -0400

    cercando un altro egitto

--- testing/analyze.ts
+++ testing/analyze.ts
@@ -9,31 +9,126 @@ import { exec } from '../src/lib/utils'
 import Utils from '../src/classes/utils'
 import fs from 'fs'
 import yaml from 'js-yaml'
+import { string } from '@oclif/core/lib/flags'
 
 startPoint()
 
 
 
 async function startPoint() {
-   const installeds: string[] = []
-   const oInstalleds = await exec('apt list --installed', {echo: false, ignore: false, capture: true})
-   if (oInstalleds.code === 0) {
-      const elements =oInstalleds.data.split(`\n`)
-      console.log(elements)
-      for (const elem of elements) {
-         const pacchetto = elem.substring(0, elem.indexOf(`/`))
-         console.log(pacchetto)
-         await getSection(pacchetto)
+   Utils.titles('order')
+   await makeInstalled()
+   await makeOrder()
+   const sections = await getSections()
+
+   /**
+    * per ogni sezione. creaiamo uan lista
+    *     drivers_printer:
+    * - autoconf
+    */
+   for (const section of sections) {
+      console.log(section + ':')
+      getPackages(section)
+   }
+}
+
+/**
+ * 
+  */
+async function getPackages(section: string) {
+   interface wif { name: string, section: string }
+   let wifs: wif[] = []
+
+   const content = fs.readFileSync('./testing/ordinati.yml', 'utf8')
+   wifs = yaml.load(content) as wif[]
+
+   for (const wif of wifs) {
+      if (wif.section === section) {
+         console.log(`- ${wif.name}`)
       }
-   } else {
-      console.log('error')
    }
 }
 
 /**
  * 
  */
+async function makeInstalled() {
+   interface wif { name: string, section: string }
+   let wifs: wif[] = []
+
+   const installeds: string[] = []
+   const oInstalleds = await exec('apt list --installed', { echo: false, ignore: false, capture: true })
+   if (oInstalleds.code === 0) {
+      const elements = oInstalleds.data.split(`\n`)
+      for (const elem of elements) {
+         if (!elem.includes('Listing...')) {
+            if (elem !== '') {
+               const pacchetto = elem.substring(0, elem.indexOf(`/`))
+               const section = await getSection(pacchetto)
+               const wif = { name: pacchetto, section: section }
+               wifs.push(wif)
+            }
+         }
+      }
+   } else {
+      console.log('error')
+   }
+   fs.writeFileSync('./testing/installed.yml', yaml.dump(wifs))
+}
+
+/**
+ * 
+ */
 async function getSection(pacchetto: string) {
-   const sh = `#!/bin/sh\napt-cache show ${pacchetto} | grep "Section: "\n`
-   console.log(sh)
+   let section = ''
+   try {
+      const result = await exec(`apt-cache show ${pacchetto}|grep Section:`, { echo: false, ignore: false, capture: true })
+      if (result.code === 0) {
+         section = result.data.substring(result.data.indexOf(':') + 2).replace(/(\r\n|\n|\r)/gm, '')
+      }
+   } catch (e) {
+      console.log(e)
+   }
+   return section
+}
+
+
+/**
+* 
+*/
+async function getSections(): Promise<string[]> {
+   interface wif { name: string, section: string }
+   let wifs: wif[] = []
+
+   /**
+    * Ora abbiamo wifs ordinati per section
+    * creiamo la array sections
+    */
+   const content = fs.readFileSync('./testing/ordinati.yml', 'utf8')
+   wifs = yaml.load(content) as wif[]
+
+   const sections: string[] = []
+   let currentSection = ''
+   for (const wif of wifs) {
+      if (currentSection !== wif.section) {
+         sections.push(wif.section)
+         currentSection = wif.section
+      }
+   }
+   return sections
+}
+
+/**
+* 
+*/
+async function makeOrder() {
+   interface wif { name: string, section: string }
+   let wifs: wif[] = []
+   let ordinati: wif[] = []
+
+   const content = fs.readFileSync('./testing/installed.yml', 'utf8')
+   wifs = yaml.load(content) as wif[]
+   ordinati = wifs.sort((a, b) => a.section.localeCompare(b.section))
+   fs.writeFileSync('./testing/ordinati.yml', yaml.dump(ordinati))
+
 }
