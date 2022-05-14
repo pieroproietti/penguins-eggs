a62df70b5e81d038910ff52eee7c30df3e2cf460
Piero Proietti <piero.proietti@gmail.com>
2022-05-14 09:01:24 +0200

    remove customized isos

---
+++ g4/g4remove-custom-isos.sh
@@ -0,0 +1,5 @@
+sudo rm /var/lib/vz/template/iso/eggs-of*colibri*.iso
+sudo rm /var/lib/vz/template/iso/eggs-of*duck*.iso
+sudo rm /var/lib/vz/template/iso/eggs-of*owl*.iso
+sudo rm /var/lib/vz/template/iso/eggs-of*wagtail*.iso
+sudo rm /var/lib/vz/template/iso/eggs-of*warbier*.iso
