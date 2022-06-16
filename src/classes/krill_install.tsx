d9a78768d29bf956ec04100b5c000e2969801cf1
Piero Proietti <piero.proietti@gmail.com>
2022-06-16 07:57:40 +0200

    mkinitcpio manjaro

--- src/classes/krill_install.tsx
+++ src/classes/krill_install.tsx
@@ -641,9 +641,13 @@ export default class Hatching {
       } else if (this.distro.familyId === 'archlinux') {
          let initrdImg = Utils.initrdImg()
          initrdImg = initrdImg.substring(initrdImg.lastIndexOf('/') + 1)
-         const cmd = `mkinitcpio -c ${path.resolve(__dirname, '../../mkinitcpio/manjaro/mkinitcpio.conf')} -g ${this.installTarget}/boot/${initrdImg} ${this.toNull}`
-         await exec(cmd, Utils.setEcho(true))
-         Utils.pressKeyToExit(cmd)
+         const cmd = `mkinitcpio -c ${path.resolve(__dirname, '../../mkinitcpio/manjaro/mkinitcpio.conf')} -g ${this.installTarget}/boot/${initrdImg}` // ${this.toNull}
+         try {
+            await exec(cmd, Utils.setEcho(true))
+         } catch (error) {
+            await Utils.pressKeyToExit(cmd)
+         }
+   
       }
    }
 
