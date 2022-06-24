b7c46938d239e790eed0668463962f7d32afae36
Piero Proietti <piero.proietti@gmail.com>
2022-06-24 18:23:10 +0200

    using setxkbmap -layout in place of localectl set-keymap --no-convert

--- src/classes/krill_install.tsx
+++ src/classes/krill_install.tsx
@@ -795,7 +795,8 @@ export default class Hatching {
             Utils.pressKeyToExit(cmd, true)
          }
       } else if (this.distro.familyId === 'archlinux') {
-         let cmd = `chroot ${this.installTarget} localectl set-keymap --no-convert ${this.keyboardLayout}`
+         // let cmd = `chroot ${this.installTarget} localectl set-keymap --no-convert ${this.keyboardLayout}`
+         let cmd = `chroot ${this.installTarget} setxkbmap -layout ${this.keyboardLayout}`
          try {
             await exec(cmd, this.echo)
          } catch (error) {
