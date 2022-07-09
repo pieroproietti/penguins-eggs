465accf1a480966e304b6743b7d720a539b402bb
Piero Proietti <piero.proietti@gmail.com>
2022-07-09 09:51:01 +0200

    uffa

--- mkinitcpio/archlinux/archlinux-swap-root.sh
+++ mkinitcpio/archlinux/archlinux-swap-root.sh
@@ -1,5 +1,4 @@
-. /init_functions
-
+#. /init_functions non serve... 
 mkdir /run/archiso/cowspace/.lower
 mount /run/archiso/bootmnt/live/filesystem.squashfs /run/archiso/cowspace/.lower
 mkdir /run/archiso/cowspace/.upper
@@ -8,4 +7,4 @@ mount -t overlay overlay -o lowerdir=/run/archiso/cowspace/.lower,upperdir=/run/
 
 init="/sbin/init"
 #echo rdlogger_stop
-exec env -i "TERM=$TERM" \usr\bin\swap_root $init "$@"
+exec env -i "TERM=$TERM" /usr/bin/swap_root $init "$@"
