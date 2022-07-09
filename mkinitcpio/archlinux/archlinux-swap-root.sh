159bebf7163d71293378b2b622cf7484010a86fd
Piero Proietti <piero.proietti@gmail.com>
2022-07-09 09:25:51 +0200

    swap-root

--- mkinitcpio/archlinux/archlinux-swap-root.sh
+++ mkinitcpio/archlinux/archlinux-swap-root.sh
@@ -2,4 +2,8 @@ mkdir /run/archiso/cowspace/.lower
 mount /run/archiso/bootmnt/live/filesystem.squashfs /run/archiso/cowspace/.lower
 mkdir /run/archiso/cowspace/.upper
 mkdir /run/archiso/cowspace/.work
-mount -t overlay overlay -o lowerdir=/run/archive/.lower,upperdir=/run/archive/.upper,workdir=/run/archive/.work /new_root
+mount -t overlay overlay -o lowerdir=/run/archiso/cowspace/.lower,upperdir=/run/archiso/cowspace/.upper,workdir=/run/archiso/cowspace/.work /new_root
+
+init="/sbin/init"
+#echo rdlogger_stop
+exec_env -i "TERM=$TERM" \usr\bin\swap_root $init "$@"
