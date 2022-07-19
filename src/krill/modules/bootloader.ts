32ad922ff4026f1f1fd2b8a9da55e04f3743fb70
Piero Proietti <piero.proietti@gmail.com>
2022-07-19 07:10:00 +0200

    First Arch linux successfully remastered and installed!

--- src/krill/modules/bootloader.ts
+++ src/krill/modules/bootloader.ts
@@ -12,12 +12,21 @@ import Utils from '../../classes/utils'
    * @param options
    */
 export default async function bootloader(this: Sequence) {
-    let echoYes = Utils.setEcho(true)
-    // await exec(`chroot ${this.installTarget} grub-install ${this.partitions.installationDevice} ${this.toNull}`, this.echo)
-    // await exec(`chroot ${this.installTarget} update-grub ${this.toNull}`, this.echo)
-    // await exec(`sleep 1 ${this.toNull}`, this.echo)
+    /**
+     * update-grub it's just a script:
+       #!/bin/sh
+       set -e
+       exec grub-mkconfig -o /boot/grub/grub.cfg "$@"
+     *
+     * not present on Arch, so we use grub-mkconfig
+     */
 
-    await exec(`chroot ${this.installTarget} grub-install ${this.partitions.installationDevice} ${this.toNull}`, echoYes)
-    await exec(`chroot ${this.installTarget} grub-mkconfig -o /boot/grub/grub.cfg ${this.toNull}`, echoYes)
-    await exec(`sleep 1 ${this.toNull}`, echoYes)
+    await exec(`chroot ${this.installTarget} grub-install ${this.partitions.installationDevice} ${this.toNull}`, this.echo)
+    await exec(`chroot ${this.installTarget} grub-mkconfig -o /boot/grub/grub.cfg ${this.toNull}`, this.echo)
+    await exec(`sleep 1 ${this.toNull}`, this.echo)
+
+    // let echoYes = Utils.setEcho(true)
+    // await exec(`chroot ${this.installTarget} grub-install ${this.partitions.installationDevice} ${this.toNull}`, echoYes)
+    // await exec(`chroot ${this.installTarget} grub-mkconfig -o /boot/grub/grub.cfg ${this.toNull}`, echoYes)
+    // await exec(`sleep 1 ${this.toNull}`, echoYes)
 }
