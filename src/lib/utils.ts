19e9d39827dff6d9d262e0d2fb76748bcdfd795d
pieroproietti-kraken <piero.proietti@gmail.com>
2022-08-01 19:53:15 +0200

    a lot of work for nothing for now... don't await me!

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,41 +7,66 @@
  *                           Set `capture` to TRUE, to capture and return stdout.
  *
  * @returns {Promise<{code: number, data: string | undefined, error: Object}>}
+ * 
+ * https://github.com/oclif/core/issues/453#issuecomment-1200778612
+ * codespool: 
+ * You could wrap spawn in a promise, listen to exit event, and resolve when it happens. That should play nicely with oclif/core.
+ * We are using it here:
+ * https://github.com/AstarNetwork/swanky-cli/blob/master/src/commands/compile/index.ts
  */
 
+import { string } from '@oclif/core/lib/flags'
+import { resolve } from 'path'
+import { CodeAction, getAllJSDocTagsOfKind } from 'typescript'
 import { IExec } from '../interfaces'
+// import { spawn } from 'child_process'
 
-export function exec(command: string, { echo = false, ignore = false, capture = false } = {}): Promise<IExec> {
-  if (echo) {
-    console.log(command)
-  }
+/**
+ * 
+ * @param command 
+ * @param param1 
+ * @returns 
+ */
 
-  const spawn = require('child_process').spawn
-  // const childProcess = spawn('bash', ['-c', command], { stdio: capture ? 'pipe' : 'inherit' });
-  // const childProcess = spawn('bash', ['-c', command], { stdio: capture ? 'pipe' : ignore ? 'ignore' : 'inherit' });
-  const childProcess = spawn('bash', ['-c', command], {
-    stdio: ignore ? 'ignore' : capture ? 'pipe' : 'inherit'
-  })
+export async function exec(command: string, { echo = false, ignore = false, capture = false } = {}): Promise<IExec> {
 
+  /**
+   * dovrebbe andare... ma non va!
+   */
   return new Promise((resolve, reject) => {
+    if (echo) {
+      console.log(command)
+    }
+  
+    const spawn = require('child_process').spawn
+
+    const child = spawn('bash', ['-c', command], {
+      stdio: ignore ? 'ignore' : capture ? 'pipe' : 'inherit'
+    })
+
     let stdout = ''
 
     if (capture) {
-      childProcess.stdout.on('data', (data: string) => {
+      child.stdout.on('data', (data: string) => {
         stdout += data
       })
     }
 
-    childProcess.on('error', function (error: string) {
+    // 'error' event
+    child.on('error', function (error: string) {
       reject({ code: 1, error: error })
     })
 
-    childProcess.on('close', function (code: number) {
-      if (code > 0) {
-        reject({ code: code, error: 'Command failed with code ' + code })
-      } else {
-        resolve({ code: code, data: stdout })
-      }
+    // The 'close' event is emitted after a process has ended and the stdio streams of a child process have been closed. 
+    // This is distinct from the 'exit' event, since multiple processes might share the same stdio streams. 
+    // The 'close' event will always emit after 'exit' was already emitted, or 'error' if the child failed to spawn.
+
+    // The 'exit' event is emitted after the child process ends. If the process exited, code is the final exit code of the process, 
+    // otherwise null. If the process terminated due to receipt of a signal, signal is the string name of the signal, otherwise null. 
+    // One of the two will always be non-null.
+    child.on('exit', (code: number) => {
+      resolve({ code: code, data: stdout })
     })
+    // end promise
   })
 }
