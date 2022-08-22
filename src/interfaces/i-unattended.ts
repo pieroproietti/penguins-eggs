a066bab7095bf5d7ab5c26ab017e94017ab8e2a0
Piero Proietti <piero.proietti@gmail.com>
2022-08-22 18:57:16 +0200

    completed unattended until network

---
+++ src/interfaces/i-unattended.ts
@@ -0,0 +1,26 @@
+export interface IUnattended {
+    language: string
+    region: string
+    zone: string
+    keyboardModel: string
+    keyboardLayout: string
+    keyboardVariant: string
+    keyboardOption: string
+    installationDevice: string
+    installationMode: string
+    filesystemType: string
+    userSwapChoice: string
+    name: string
+    fullname: string
+    password: string
+    rootPassword: string
+    autologin: boolean
+    hostname: string
+    iface: string
+    addressType: string
+    address: string
+    netmask: string
+    gateway: string
+    domain: string
+    dns: string
+}
