bd0ec977a8666c4e79c10f5655db2180dd6c56b2
Piero Proietti <piero.proietti@gmail.com>
2022-08-22 19:38:18 +0200

    updated autocomplete

--- scripts/eggs.bash
+++ scripts/eggs.bash
@@ -19,12 +19,11 @@ dad --help --clean --default --verbose
 export:deb --help --clean --amd64 --i386 --armel --arm64 --all
 export:docs --help
 export:iso --help --backup --clean
-info --verbose --help
-install --crypted --pve --help --verbose
-krill --crypted --pve --help --verbose
+install --unattended --crypted --pve --help --verbose
 kill --help --verbose
 mom --help
 produce --prefix --basename --backup --clone --fast --normal --max --verbose --yolk --script --help --theme --addons --release
+status --verbose --help
 syncfrom --delete --file --rootdir --help --verbose
 syncto --delete --file --help --verbose
 tools:clean --help --verbose
