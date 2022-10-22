456fdc19c55c11c5b7cd0754ccfc6529928fe574
Piero Proietti <piero.proietti@gmail.com>
2022-10-22 09:50:02 -0400

    using sshfs: just a command for export export deb/doc/iso -c

--- src/commands/export/docs.ts
+++ src/commands/export/docs.ts
@@ -8,6 +8,7 @@ export default class ExportDocs extends Command {
   static description = 'remove and export docType documentation of the sources in the destination host'
 
   static flags = {
+    verbose: Flags.boolean({ char: 'v', description: 'verbose' }),
     help: Flags.help({ char: 'h' })
   }
 
@@ -19,7 +20,20 @@ export default class ExportDocs extends Command {
     Utils.warning(ExportDocs.description)
 
     await Tu.loadSettings()
-    await exec(`ssh ${Tu.config.remoteUser}@${Tu.config.remoteHost} rm -rf ${Tu.config.remotePathDoc}*`, { echo: true, capture: true })
-    await exec(`scp -r ${Tu.config.localPathDoc}/* ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathDoc}`, { echo: true, capture: true })
+
+    const echo = Utils.setEcho(flags.verbose)
+
+    const rmount = `/tmp/eggs-${(Math.random() + 1).toString(36).substring(7)}`
+    let cmd = `rm -f ${rmount}\n`
+    cmd += `mkdir ${rmount}\n`
+    cmd += `sshfs ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathDoc} ${rmount}\n`
+    cmd += `rm -f ${rmount}/*\n`
+    cmd += `cp ${Tu.config.localPathDoc}/* ${rmount}\n`
+    cmd += `sync\n`
+    cmd += `umount ${rmount}\n`
+    cmd += `rm -f ${rmount}\m`
+
+    await exec(cmd, echo)
+
   }
 }
