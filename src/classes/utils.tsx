95113f6d4c5a1aefcff2d634f5f65d8940fa5a17
Piero Proietti <piero.proietti@gmail.com>
2022-12-23 18:30:09 +0100

    ok

--- src/classes/utils.tsx
+++ src/classes/utils.tsx
@@ -180,8 +180,9 @@ export default class Utils {
     */
    static async getPrimaryUser(): Promise<string> {
       let primaryUser = (await exec('/usr/bin/logname', { echo: false, ignore: false, capture: true })).data.trim()
-      if (primaryUser.startsWith('logname:')) {
-         primaryUser = (await exec(`echo $(SUDO_USER)`, { echo: false, capture: true })).data.trim()
+      // if logname don't work seem we get '' so:
+      if (primaryUser === '') {
+         primaryUser = shx.exec('echo $SUDO_USER', { silent: true }).stdout.trim()
       }
       if (primaryUser === '') {
          console.log(`Cannot find your user name...`)
