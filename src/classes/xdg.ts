d28dfa91bdeede6d7433c94028d5ce041e290386
Piero Proietti <piero.proietti@gmail.com>
2022-12-28 14:01:24 +0100

    xdg patch for linuxfx

--- src/classes/xdg.ts
+++ src/classes/xdg.ts
@@ -221,15 +221,18 @@ export default class Xdg {
       await rsyncIfExist(`/home/${user}/.local/share/recently-used.xbel`, '/etc/skel/.local/share', verbose)
     }
 
-    // linuxFX special cases
+    /**
+     * special cases
+     */
+
+    // LinuxFX
     if (fs.existsSync(`/home/${user}/.linuxfx`)) {
       // we need to copy: .linuxfx ,kde and ,cinnamon
-      await rsyncIfExist(`/home/${user}/.linuxfx`, `/etc/skel`, verbose)
-      await rsyncIfExist(`/home/${user}/.kde`, '/etc/skel', verbose)
       await rsyncIfExist(`/home/${user}/.cinnamon`, '/etc/skel', verbose)
+      await rsyncIfExist(`/home/${user}/.kde`, '/etc/skel', verbose)
+      await rsyncIfExist(`/home/${user}/.linuxfx`, `/etc/skel`, verbose)
+      await rsyncIfExist(`/home/${user}/.local`, '/etc/skel', verbose)
     }
-
-    // 
 
     await exec('chown root:root /etc/skel -R', echo)
     await exec('chmod a+rwx,g-w,o-w /etc/skel/ -R', echo)
