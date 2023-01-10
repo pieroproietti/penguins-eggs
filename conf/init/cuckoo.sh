c2c5c8f57f24119754526cd78374c385f54a1604
Piero Proietti <piero.proietti@gmail.com>
2023-01-10 12:07:38 -0500

    create two specific init: unattend and cuckoo

---
+++ conf/init/cuckoo.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# read https://www.linux.it/~rubini/docs/init/init.html 
+#
+/usr/bin/eggs cuckoo
+
+
---
+++ conf/init/unattended.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# read https://www.linux.it/~rubini/docs/init/init.html 
+#
+# export PATH=/usr/bin:/bin:/sbin:/usr/sbin
+/usr/bin/eggs install --unattended
+#/sbin/getty tty1
+
