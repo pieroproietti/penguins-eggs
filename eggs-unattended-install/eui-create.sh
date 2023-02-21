354937a440434bf5808ac386bc44324312d0cf48
artisan <artisan@colibri..local>
2023-02-21 12:33:33 +0100

    creato un service per autologin ed uno per eui

--- eggs-unattended-install/eui-create.sh
+++ eggs-unattended-install/eui-create.sh
@@ -1,10 +1,12 @@
 #!/bin/env bash
 sudo eggs kill
 sudo cp eui.sh /opt
+sudo cp eggs-autologin.service /etc/systemd/system/getty@.service.d/override.conf
 sudo cp eui.service /etc/systemd/system/
 sudo systemctl enable eui.service
 sudo eggs produce
 sudo systemctl disable eui.service
+sudo rm /etc/systemd/system/getty@.service.d/override.conf
 sudo rm /etc/systemd/system/eui.service
 sudo rm /opt/eui.sh
 sudo eggs cuckoo
