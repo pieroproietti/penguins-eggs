6d6e9143f61528b3d836622ed555e1164560903e
artisan <artisan@colibri..local>
2023-02-22 08:25:47 +0100

    porca pupa...

--- eui/eui-create.sh
+++ eui/eui-create.sh
@@ -4,7 +4,7 @@ cp eui.sudoers /etc/sudoers.d/
 cp eui-start.sh /usr/bin/
 cp eui.desktop /etc/xdg/autostart/
 eggs produce
-rm /etc/sudoers.d/eui.sudoers 
-rm /usr/bin/eui-start.sh
-rm /etc/xdg/autostart/eui.desktop
+#rm /etc/sudoers.d/eui.sudoers 
+#rm /usr/bin/eui-start.sh
+#rm /etc/xdg/autostart/eui.desktop
 eggs cuckoo
--- eui/eui-start.sh
+++ eui/eui-start.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 echo "eggs unattended install"
-echo "evolution" | sudo -S eggs install -un
+echo "evolution" | sudo -S eggs install -unrd .local
