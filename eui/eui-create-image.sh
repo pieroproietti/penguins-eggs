b16a84055d8af77fc8fcefa1652d72718660c530
artisan <artisan@colibri..local>
2023-02-22 18:57:41 +0100

    produce --release -n

--- eui/eui-create-image.sh
+++ eui/eui-create-image.sh
@@ -3,7 +3,7 @@ sudo eggs kill -n
 sudo cp eui-users /etc/sudoers.d/
 sudo cp eui-start.sh /usr/bin/
 sudo cp eui.desktop /etc/xdg/autostart/
-sudo eggs produce -n
+sudo eggs produce --release -n
 sudo rm /etc/sudoers.d/eui-users
 sudo rm /usr/bin/eui-start.sh
 sudo rm /etc/xdg/autostart/eui.desktop
