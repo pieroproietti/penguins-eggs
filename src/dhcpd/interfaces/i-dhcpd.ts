99f5845dea26674af49ad570ce74e5beac6be00a
Piero Proietti <piero.proietti@gmail.com>
2023-02-24 12:03:54 +0100

    intefaces dhcpd

--- src/dhcpd/interfaces/i-dhcpd.ts
+++ src/dhcpd/interfaces/i-dhcpd.ts
@@ -4,44 +4,46 @@
   mail: piero.proietti@gmail.com
 
   interface per classe dhcpd
+
   https://blog.logrocket.com/when-how-use-interfaces-classes-typescript/
+  
   We can only contain declarations of variables and
   methods in the body of the interface. 
 */
 
 export interface IDhcpd {
 	// constructor(opts) 
-  pre_init: (pkt: any) => void
-	discover: (pkt: any) => void
-  request: (pkt: any) => void
-  inform: (pkt: any) => void
-  proxy_request: (pkt: any) => any
+  pre_init: (pkt: IPacket) => void
+	discover: (pkt: IPacket) => void
+  request: (pkt: IPacket) => void
+  inform: (pkt: IPacket) => void
+  proxy_request: (pkt: IPacket) =>  IPacket
 }
 
 export interface IProxy {
   // constructor(type, opts) 
-  bind: (port: any, addr: any, cb: any) => any 
+  bind: (port: any, addr: any, cb: any) =>  IPacket
 }
 
 export interface IServer {
   // constructor(type, opts) 
-  bind: (port: any, addr: any, cb: any) => any 
+  bind: (port: any, addr: any, cb: any) => IPacket
 }
 
 export interface IPacket {
   getRequestedIPAddress: () => string // this.options[50]
-  op: (op: any) => any // set op
-  htype: (htype: any) => any // set htype
-  hlen: (hlen: any) => any // set hlen
-  hops: (hops: any) => any // set hops
-  xid: (xid: any) => any // set xid
-  secs: (secs: any) => any // set secs
-  flags:(flags: any) => any // set flags
-  ciaddr: (ciaddr: string) => any // set coaddr
-  siaddr: (siaddr: string) => any // set siaddr
-  giaddr:(giaddr: string) => any // set giaddr
-  chaddr:(chaddr: any)  => any  // set chaddr
-  sname:(sname: any) => any // set snme
-  fname: (fname: any) => any // set fname
-  options:(options: any) => any
+  op: (op: any) => IPacket // set op
+  htype: (htype: any) => IPacket // set htype
+  hlen: (hlen: any) => IPacket // set hlen
+  hops: (hops: any) => IPacket // set hops
+  xid: (xid: any) => IPacket // set xid
+  secs: (secs: any) => IPacket // set secs
+  flags:(flags: any) => IPacket // set flags
+  ciaddr: (ciaddr: string) => IPacket // set coaddr
+  siaddr: (siaddr: string) => IPacket // set siaddr
+  giaddr:(giaddr: string) => IPacket // set giaddr
+  chaddr:(chaddr: any)  => IPacket  // set chaddr
+  sname:(sname: any) => IPacket // set snme
+  fname: (fname: any) => IPacket // set fname
+  options:(options: any) => IPacket
 }
