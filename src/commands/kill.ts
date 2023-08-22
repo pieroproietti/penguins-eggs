9d945c1e36694f1d53be18c63891b38ae47441ac
Piero Proietti <piero.proietti@gmail.com>
2023-08-22 11:01:28 +0200

    testing

--- src/commands/kill.ts
+++ src/commands/kill.ts
@@ -4,20 +4,21 @@
  * email: piero.proietti@gmail.com
  * license: MIT
  */
-import {Command, Flags} from '@oclif/core'
-
+import { Command, Flags } from '@oclif/core'
 import fs from 'fs'
 import Utils from '../classes/utils'
 import Settings from '../classes/settings'
-import {IWorkDir} from '../interfaces/i-workdir'
+import { IWorkDir } from '../interfaces/i-workdir'
+import { exec } from '../lib/utils'
 
-import {exec} from '../lib/utils'
-
+/**
+ * 
+ */
 export default class Kill extends Command {
   static flags = {
-    help: Flags.help({char: 'h'}),
-    nointeractive: Flags.boolean({char: 'n', description: 'no user interaction'}),
-    verbose: Flags.boolean({char: 'v', description: 'verbose'}),
+    help: Flags.help({ char: 'h' }),
+    nointeractive: Flags.boolean({ char: 'n', description: 'no user interaction' }),
+    verbose: Flags.boolean({ char: 'v', description: 'verbose' }),
   }
 
   static description = 'kill the eggs/free the nest'
@@ -32,7 +33,7 @@ export default class Kill extends Command {
   async run(): Promise<void> {
     Utils.titles(this.id + ' ' + this.argv)
 
-    const {flags} = await this.parse(Kill)
+    const { flags } = await this.parse(Kill)
     let verbose = false
     if (flags.verbose) {
       verbose = true
@@ -47,11 +48,12 @@ export default class Kill extends Command {
       await settings.load()
       await settings.listFreeSpace()
       if (nointeractive || await Utils.customConfirm()) {
-        if (checkMPs(`${settings.work_dir.path}/filesystem.squashfs`)) {
+        const chkPath = `${settings.work_dir.path}filesystem.squashfs`
+        if (!checkMPs(chkPath)) {
           await exec(`rm ${settings.work_dir.path}/* -rf`, echo)
           await exec(`rm ${settings.config.snapshot_dir} -rf`, echo)
         } else {
-          console.log(`cannot kill`)
+          console.log(`Cannot kill!\nWhere are mountpoint under ${chkPath}`)
         }
       }
     } else {
@@ -60,17 +62,24 @@ export default class Kill extends Command {
   }
 }
 
-function checkMPs(path: string) : Boolean {
+/**
+ * 
+ * @param path 
+ * @returns 
+ */
+function checkMPs(path: string): Boolean {
   let retVal = false
   const dirs = ['etc', 'boot', 'usr', 'var']
 
-  for (const dir in dirs) {
+  for (const dir of dirs) {
     const dirToCheck = `${path}/${dir}`
-    if (Utils.isMountpoint(dirToCheck)) {
-      console.log(`${dirToCheck}, is a mountpoint!`)
-      retVal= true 
+    // console.log("checking " + dirToCheck)
+    if (fs.existsSync(dirToCheck)) {
+      if (Utils.isMountpoint(dirToCheck)) {
+        console.log(`Warning: ${dirToCheck}, is a mountpoint!`)
+        retVal = true
+      }
     }
   }
-
   return retVal
 }
