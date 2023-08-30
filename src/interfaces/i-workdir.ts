1e365ca5365450a021d4e86a5bc45b0507a87dd8
Piero Proietti <piero.proietti@gmail.com>
2023-08-30 15:33:25 +0200

    qualcosa avrei pure tirato fuori...

--- src/interfaces/i-workdir.ts
+++ src/interfaces/i-workdir.ts
@@ -1,6 +1,8 @@
 export interface IWorkDir {
   path: string
-  pathIso: string
+  pathIso: string 
+  pathDest: string // this is new
+  pathMP: string // this is new
   lowerdir: string
   upperdir: string
   workdir: string
