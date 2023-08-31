0ba3a667f3111b8544e6292400e1355d7d91456e
Piero Proietti <piero.proietti@gmail.com>
2023-08-31 07:58:48 +0200

    broken

--- src/interfaces/i-workdir.ts
+++ src/interfaces/i-workdir.ts
@@ -1,8 +1,8 @@
 export interface IWorkDir {
   path: string
   pathIso: string 
-  pathDest: string // this is new
-  pathMP: string // this is new
+  // pathDest: string // this is new
+  // pathMP: string // this is new
   lowerdir: string
   upperdir: string
   workdir: string
