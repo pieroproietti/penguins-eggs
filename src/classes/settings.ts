8196cedcab7077d16e707b03f08e98cd49aab7b7
Piero Proietti <piero.proietti@gmail.com>
2023-08-31 21:40:50 +0200

    README per snapshot_dir

--- src/classes/settings.ts
+++ src/classes/settings.ts
@@ -15,14 +15,14 @@ import chalk from 'chalk'
 const pjson = require('../../package.json')
 
 // interfaces
-import {IRemix, IDistro, IApp, IWorkDir} from '../interfaces/index'
+import { IRemix, IDistro, IApp, IWorkDir } from '../interfaces/index'
 
 // classes
 import Utils from './utils'
 import Incubator from './incubation/incubator'
 import Distro from './distro'
 import Pacman from './pacman'
-import {IEggsConfig} from '../interfaces/index'
+import { IEggsConfig } from '../interfaces/index'
 
 const config_file = '/etc/penguins-eggs.d/eggs.yaml' as string
 
@@ -110,7 +110,7 @@ export default class Settings {
     if (!this.config.snapshot_mnt.endsWith('/')) {
       this.config.snapshot_mnt += '/'
     }
-    
+
     this.work_dir.merged = this.config.snapshot_mnt + 'filesystem.squashfs'
     this.efi_work = this.config.snapshot_mnt + 'efi-work/'
     this.work_dir.pathIso = this.config.snapshot_mnt + 'iso/'
@@ -154,15 +154,83 @@ export default class Settings {
     }
 
     if (this.config.timezone === undefined || this.config.timezone === '') {
-      this.config.timezone = shx.exec('cat /etc/timezone', {silent: true}).stdout.trim()
+      this.config.timezone = shx.exec('cat /etc/timezone', { silent: true }).stdout.trim()
     }
 
     return foundSettings
   }
 
   /**
-   * show NOT USED MORE
+   * Calculate and show free space on the disk
+   * @returns {void}
    */
+  async listFreeSpace(): Promise<void> {
+    if (!fs.existsSync(this.config.snapshot_dir)) {
+      fs.mkdirSync(this.config.snapshot_dir)
+      if (!fs.existsSync(this.config.snapshot_mnt)) {
+        fs.mkdirSync(this.config.snapshot_mnt)
+      }
+    }
+
+    /** Lo spazio usato da SquashFS non è stimabile da live
+     * errore buffer troppo piccolo
+     */
+    const gb = 1_048_576
+    let spaceAvailable = 0
+    if (!Utils.isLive()) {
+      console.log(`Disk space used: ${Math.round((Utils.getUsedSpace() / gb) * 10) / 10} GB`)
+    }
+
+    spaceAvailable = Number(
+      shx
+        .exec(`df "${this.config.snapshot_mnt}" | /usr/bin/awk 'NR==2 {print $4}'`, {
+          silent: true,
+        })
+        .stdout.trim(),
+    )
+    console.log(`Space available: ${Math.round((spaceAvailable / gb) * 10) / 10} GB`)
+    console.log(`There are ${Utils.getSnapshotCount(this.config.snapshot_mnt)} snapshots taking ${Math.round((Utils.getSnapshotSize(this.config.snapshot_mnt) / gb) * 10) / 10} GB of disk space.`)
+    console.log()
+
+    if (spaceAvailable > gb * 3) {
+      console.log(chalk.cyanBright('The free space should  be sufficient to hold the'))
+      console.log(chalk.cyanBright('compressed data from the system'))
+    } else {
+      console.log(chalk.redBright('The free space should be insufficient') + '.')
+      console.log()
+      if (Utils.isMountpoint(this.config.snapshot_mnt)) {
+        console.log('If necessary, you can create more available space')
+        console.log('by removing previous  snapshots and saved copies.')
+      } else {
+        console.log(`You can mount a free partition under ${this.config.snapshot_mnt}`)
+      }
+      console.log()
+    }
+  }
+
+  /**
+   *
+   * @param basename
+   * @param theme
+   */
+  async loadRemix(basename = '', theme = '') {
+    this.remix.versionNumber = Utils.getPackageVersion()
+    this.remix.kernel = Utils.kernelVersion()
+
+    this.remix.branding = theme === '' ? 'eggs' : this.remix.branding = theme.slice(Math.max(0, theme.lastIndexOf('/') + 1))
+
+    this.remix.name = this.config.snapshot_basename
+    let name = this.config.snapshot_prefix + this.config.snapshot_basename
+    name = name.replace(/-/g, ' ').replace('egg of ', '')
+    this.remix.fullname = name
+    this.remix.versionName = name.toUpperCase()
+  }
+
+
+  
+  /**
+  * show NOT USED MORE
+  */
   async show_not_used_more() {
     console.log(`application_name:  ${this.app.name} ${this.app.version}`)
     // console.log(`config_file:       ${config_file}`)
@@ -211,71 +279,5 @@ export default class Settings {
         this.config.make_efi = false
       }
     }
-  }
-
-  /**
-   * Calculate and show free space on the disk
-   * @returns {void}
-   */
-  async listFreeSpace(): Promise<void> {
-    if (!fs.existsSync(this.config.snapshot_dir)) {
-      fs.mkdirSync(this.config.snapshot_dir)
-      if (!fs.existsSync(this.config.snapshot_mnt)) {
-        fs.mkdirSync(this.config.snapshot_mnt)
-      }
-    }
-
-    /** Lo spazio usato da SquashFS non è stimabile da live
-     * errore buffer troppo piccolo
-     */
-    const gb = 1_048_576
-    let spaceAvailable = 0
-    if (!Utils.isLive()) {
-      console.log(`Disk space used: ${Math.round((Utils.getUsedSpace() / gb) * 10) / 10} GB`)
-    }
-
-    spaceAvailable = Number(
-      shx
-      .exec(`df "${this.config.snapshot_mnt}" | /usr/bin/awk 'NR==2 {print $4}'`, {
-        silent: true,
-      })
-      .stdout.trim(),
-    )
-    console.log(`Space available: ${Math.round((spaceAvailable / gb) * 10) / 10} GB`)
-    console.log(`There are ${Utils.getSnapshotCount(this.config.snapshot_mnt)} snapshots taking ${Math.round((Utils.getSnapshotSize(this.config.snapshot_mnt) / gb) * 10) / 10} GB of disk space.`)
-    console.log()
-
-    if (spaceAvailable > gb * 3) {
-      console.log(chalk.cyanBright('The free space should  be sufficient to hold the'))
-      console.log(chalk.cyanBright('compressed data from the system'))
-    } else {
-      console.log(chalk.redBright('The free space should be insufficient') + '.')
-      console.log()
-      if (Utils.isMountpoint(this.config.snapshot_mnt)) {
-        console.log('If necessary, you can create more available space')
-        console.log('by removing previous  snapshots and saved copies.')
-      } else {
-        console.log(`You can mount a free partition under ${this.config.snapshot_mnt}`)
-      }
-      console.log()
-    }
-  }
-
-  /**
-   *
-   * @param basename
-   * @param theme
-   */
-  async loadRemix(basename = '', theme = '') {
-    this.remix.versionNumber = Utils.getPackageVersion()
-    this.remix.kernel = Utils.kernelVersion()
-
-    this.remix.branding = theme === '' ? 'eggs' : this.remix.branding = theme.slice(Math.max(0, theme.lastIndexOf('/') + 1))
-
-    this.remix.name = this.config.snapshot_basename
-    let name = this.config.snapshot_prefix + this.config.snapshot_basename
-    name = name.replace(/-/g, ' ').replace('egg of ', '')
-    this.remix.fullname = name
-    this.remix.versionName = name.toUpperCase()
   }
 }
