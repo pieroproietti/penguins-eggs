4828388fe2d25fc557a8eff3a5b750b5d09703f0
Piero Proietti <piero.proietti@gmail.com>
2023-10-02 06:23:59 +0200

    escluso yolk su i36 ed arm

--- src/classes/yolk.ts
+++ src/classes/yolk.ts
@@ -10,7 +10,7 @@ import fs from 'fs'
 import Utils from './utils'
 import Pacman from './pacman'
 import Bleach from './bleach'
-import {exec} from '../lib/utils'
+import { exec } from '../lib/utils'
 import shx from 'shelljs'
 
 /**
@@ -32,67 +32,67 @@ export default class Yolk {
     Utils.warning("Creating a local repo on /var/local/yolk")
 
     if (Utils.machineArch() !== 'amd64') {
-      Utils.error(`Is not possible to create yolk on ${Utils.machineArch()}`)
-      process.exit
-    }
-
-    Utils.warning('Updating system')
-    if (!Pacman.commandIsInstalled('dpkg-scanpackages')) {
-      process.exit(0)
-    }
-
-    let cmd = ''
-    try {
-      cmd = 'apt-get update --yes'
-      await exec(cmd, this.echo)
-    } catch (error) {
-      console.log(error)
-      await Utils.pressKeyToExit(cmd)
-    }
-
-    if (!this.yolkExists()) {
-      await exec(`mkdir ${this.yolkDir} -p`, this.echo)
-      await exec(`chown _apt:root ${this.yolkDir} -R`, this.echo)
+      Utils.warning(`yolk is not applicable on the architecture ${Utils.machineArch()}`)
     } else {
-      await this.yolkClean()
+      Utils.warning('Updating system')
+      if (!Pacman.commandIsInstalled('dpkg-scanpackages')) {
+        Utils.warning(`I cannot find the command dpkg-scanpackages`)
+        process.exit(0)
+      }
+
+      let cmd = ''
+      try {
+        cmd = 'apt-get update --yes'
+        await exec(cmd, this.echo)
+      } catch (error) {
+        console.log(error)
+        await Utils.pressKeyToExit(cmd)
+      }
+
+      if (!this.yolkExists()) {
+        await exec(`mkdir ${this.yolkDir} -p`, this.echo)
+        await exec(`chown _apt:root ${this.yolkDir} -R`, this.echo)
+      } else {
+        await this.yolkClean()
+      }
+
+      // packages we need
+      const pkgs = [
+        'cryptsetup',
+        //      'grub-efi-amd64-bin',
+        'grub-efi-amd64',
+        'grub-pc',
+        'keyutils',
+        'shim-signed',
+      ]
+
+      process.chdir(this.yolkDir)
+      Utils.warning(`Downloading packages and its dependencies`)
+
+      for (const pkg of pkgs) {
+        Utils.warning(`- ${pkg}`)
+        cmd = `apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances ${pkg} | grep "^\\w" | sort -u`
+        let depends = pkg + '\n'
+        depends += (await exec(cmd, { echo: false, capture: true })).data
+        await this.installDeps(depends.split('\n'))
+      }
+
+      // create Package.gz
+      cmd = 'dpkg-scanpackages -h  md5,sha1,sha256 . | gzip -c > Packages.gz'
+      Utils.warning(cmd)
+      await exec(cmd, { echo: false, capture: true })
+
+      // Create Release date: Sat, 14 Aug 2021 07:42:00 UTC
+      const now = shx.exec('date -R -u').stdout.trim()
+      const content = `Archive: stable\nComponent: yolk\nOrigin: penguins-eggs\nArchitecture: ${Utils.machineArch()} \nDate: ${now}\n`
+      Utils.warning('Writing Release')
+      fs.writeFileSync('Release', content)
+
+      // Cleaning
+      Utils.warning('Cleaning apt cache')
+      const bleach = new Bleach()
+      await bleach.clean(verbose)
     }
-
-    // packages we need
-    const pkgs = [
-      'cryptsetup', 
-//      'grub-efi-amd64-bin',
-      'grub-efi-amd64',
-      'grub-pc',
-      'keyutils', 
-      'shim-signed',
-    ] 
-
-    process.chdir(this.yolkDir)
-    Utils.warning(`Downloading packages and its dependencies`)
-
-    for (const pkg of pkgs) {
-      Utils.warning(`- ${pkg}`)
-      cmd = `apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances ${pkg} | grep "^\\w" | sort -u`
-      let depends = pkg + '\n'
-      depends += (await exec(cmd, {echo: false, capture: true})).data
-      await this.installDeps(depends.split('\n'))
-    }
-
-    // create Package.gz
-    cmd = 'dpkg-scanpackages -h  md5,sha1,sha256 . | gzip -c > Packages.gz'
-    Utils.warning(cmd)
-    await exec(cmd, {echo: false, capture: true})
-
-    // Create Release date: Sat, 14 Aug 2021 07:42:00 UTC
-    const now = shx.exec('date -R -u').stdout.trim()
-    const content = `Archive: stable\nComponent: yolk\nOrigin: penguins-eggs\nArchitecture: ${Utils.machineArch()} \nDate: ${now}\n`
-    Utils.warning('Writing Release')
-    fs.writeFileSync('Release', content)
-
-    // Cleaning
-    Utils.warning('Cleaning apt cache')
-    const bleach = new Bleach()
-    await bleach.clean(verbose)
   }
 
   /**
@@ -105,7 +105,7 @@ export default class Yolk {
     const toDownloads: string[] = []
     for (const depend of depends) {
       // if (depend !== '' && !Pacman.packageIsInstalled(depend)) {
-        toDownloads.push(depend)
+      toDownloads.push(depend)
       // }
     }
 
