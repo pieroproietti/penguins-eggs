47ae6ffb0cbe59f4ecb3d01d28c6c72c47952f38
Piero Proietti <piero.proietti@gmail.com>
2023-10-03 21:24:36 +0200

    we need tests

--- make-i386.sh
+++ make-i386.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [ "$EUID" -ne 0 ]; then 
-  echo "Please run as root: sudo ./i386make.sh 9.6.1"
+  echo "Please run as root: sudo ${0} 9.6.1"
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  echo "You need to specify version, ex: 9.6.1"
   exit
 fi
 
@@ -10,9 +15,9 @@ SRC="${FR}eggs_${VER}_amd64"
 DEST="${FR}eggs_${VER}_i386"
 rm -rf "DEST"
 rm -f "${SRC}*.deb"
-cp ${SRC} ${DEST} -R
-sed -i 's/grub-efi-amd64-bin/nodejs/g' ${DEST}/DEBIAN/control
-sed -i 's/amd64/i386/g' ${DEST}/DEBIAN/control
+cp "${SRC}" "${DEST}" -R
+sed -i 's/grub-efi-amd64-bin/nodejs/g' "${DEST}/DEBIAN/control"
+sed -i 's/amd64/i386/g' "${DEST}/DEBIAN/control"
 rm -f "${DEST}/usr/lib/penguins-eggs/bin/node"
 clear
 echo "Continue with following commands"
