786500163fa59097800ca7151366dd4051799929
Piero Proietti <piero.proietti@gmail.com>
2023-12-09 15:45:13 +0100

    see if work...

--- src/classes/compressors.ts
+++ src/classes/compressors.ts
@@ -37,7 +37,7 @@ export default class Compressors {
   fast(): string {
     let comp = 'gzip'
     if (this.isEnabled.zstd) {
-      comp = 'zstd -b 256K -Xcompression-level 1'
+      comp = 'zstd -b 1M -Xcompression-level 1'
     } else if (this.isEnabled.lz4) {
       comp = 'lz4'
     }
