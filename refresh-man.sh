6383393979010445d7bf77874a8b7eb3d108b6ee
Piero Proietti <piero.proietti@gmail.com>
2023-12-28 16:36:10 +0100

    v9.6.23

--- refresh-man.sh
+++ refresh-man.sh
@@ -1,2 +1,2 @@
 rm manpages/* -rf
-cp perrisbrewery/workdir/eggs_9.6.22_amd64/usr/lib/penguins-eggs/manpages/* manpages/ -R
+cp perrisbrewery/workdir/eggs_9.6.23_amd64/usr/lib/penguins-eggs/manpages/* manpages/ -R
