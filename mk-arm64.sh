3f2757e2fa473dfcfbde82398d21c94e9578d378
Piero Proietti <piero.proietti@gmail.com>
2024-02-10 22:40:00 +0100

    AI is powerfull

--- mk-arm64.sh
+++ mk-arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if running as root
-if [ "$EUID" -ne 0 ]; then  echo "Please run as root:do ${0} 9..1"
+if [ "$EUID" -ne 0 ]; then  echo "Please run this script as root"
   exit 1
 fi
 
