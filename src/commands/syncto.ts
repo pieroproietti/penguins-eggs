204fc47fd5b398fb56449855f55c4e68580fcfa2
Piero Proietti <piero.proietti@gmail.com>
2024-03-18 17:12:48 +0100

    ritocchi per la giusta via... si spera!

--- src/commands/syncto.ts
+++ src/commands/syncto.ts
@@ -22,140 +22,146 @@
  * if needed to be passed so that everything else is removed, but this
  * this should not be available by default
  */
-import {Command, Flags} from '@oclif/core'
-import fs from 'fs'
+import { Command, Flags } from '@oclif/core'
 import path from 'path'
 import Utils from '../classes/utils'
-import {exec} from '../lib/utils'
-
-// backup
-import {access} from 'fs/promises'
-import {constants} from 'fs'
-import Users from '../classes/users'
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript'
+import { exec } from '../lib/utils'
+import Settings from '../classes/settings'
 
 /**
  *
  */
 export default class Syncto extends Command {
-    static flags = {
-      delete: Flags.string({description: 'rsync --delete delete extraneous files from dest dirs'}),
-      file: Flags.string({char: 'f', description: 'file LUKS volume encrypted'}),
-      exclusion: Flags.boolean({char: 'e', description: 'exclude files using exclude.list.cryptedclone template' }),
-      help: Flags.help({char: 'h'}),
-      verbose: Flags.boolean({char: 'v', description: 'verbose'}),
+  static flags = {
+    delete: Flags.string({ description: 'rsync --delete delete extraneous files from dest dirs' }),
+    file: Flags.string({ char: 'f', description: 'file LUKS volume encrypted' }),
+    exclusion: Flags.boolean({ char: 'e', description: 'exclude files using exclude.list.cryptedclone template' }),
+    help: Flags.help({ char: 'h' }),
+    verbose: Flags.boolean({ char: 'v', description: 'verbose' }),
+  }
+
+  static description = 'saves users and user data in a LUKS volume inside the iso'
+  static examples = [
+    'sudo eggs syncto',
+    'sudo eggs syncto --file /path/to/fileLUKS',
+    'sudo eggs syncto -e'
+  ]
+
+  verbose = false
+
+  echo = {}
+
+  luksName = 'luks-eggs-backup'
+
+  luksFile = `/run/live/medium/live/${this.luksName}`
+
+  luksDevice = `/dev/mapper/${this.luksName}`
+
+  luksMountpoint = '/tmp/eggs-backup'
+
+  excludeFile = '/etc/penguins-eggs.d/exclude.list.d/exclude.list.cryptedclone'
+
+  excludeFiles = false
+
+  settings = {} as Settings
+
+  nest = '/home/eggs'
+
+  compression = '-comp=xz'
+  
+
+  /**
+   *
+   */
+  async run(): Promise<void> {
+    const { flags } = await this.parse(Syncto)
+
+    if (flags.verbose) {
+      this.verbose = true
     }
 
-    static description = 'saves users and user data in a LUKS volume inside the iso'
-    static examples = [
-      'sudo eggs syncto',
-      'sudo eggs syncto --file /path/to/fileLUKS',
-      'sudo eggs syncto -e'
-    ]
+    this.echo = Utils.setEcho(this.verbose)
 
-    verbose = false
-
-    echo = {}
-
-    luksName = 'luks-eggs-backup'
-
-    luksFile = `/run/live/medium/live/${this.luksName}`
-
-    luksDevice = `/dev/mapper/${this.luksName}`
-
-    luksMountpoint = '/tmp/eggs-backup'
-
-    exclude_file = '/etc/penguins-eggs.d/exclude.list.d/exclude.list.cryptedclone'
-
-    /**
-     *
-     */
-    async run(): Promise<void> {
-      const {flags} = await this.parse(Syncto)
-
-      if (flags.verbose) {
-        this.verbose = true
-      }
-
-      this.echo = Utils.setEcho(this.verbose)
-
-      let fileVolume = ''
-      if (flags.file) {
-        fileVolume = flags.file
-      }
-
-      let destDelete = false
-      if (flags.delete) {
-        destDelete = true
-      }
-
-      let excludeFiles = false
-      if (flags.exclusion) {
-        excludeFiles = true
-      }
-
-      if (Utils.isRoot()) {
-        /**
-             * restore con file
-             */
-        if (fileVolume === '') {
-          fileVolume = '/tmp/luks-eggs-data'
-        }
-
-        this.luksName = path.basename(fileVolume)
-        this.luksFile = fileVolume
-        this.luksDevice = `/dev/mapper/${this.luksName}`
-        this.luksMountpoint = '/tmp/eggs-data'
-        await this.luksCreate()
-      } else {
-        Utils.useRoot(this.id)
-      }
+    let fileVolume = ''
+    if (flags.file) {
+      fileVolume = flags.file
     }
 
+    let destDelete = false
+    if (flags.delete) {
+      destDelete = true
+    }
 
-    /**
-     *
-     */
-    async luksCreate() {
-      Utils.warning(`Erasing previous LUKS Volume on ${this.luksFile}`)
-      let clean=`rm -rf /tmp/${this.luksName}`
-      await exec(clean)
+    if (flags.exclusion) {
+      this.excludeFiles = true
+    }
 
-      let maxSize="10G"
-      Utils.warning(`Creating LUKS Volume on ${this.luksFile}, size ${maxSize}`)
-      await exec(`truncate -s ${maxSize} ${this.luksFile}`)
+    if (Utils.isRoot()) {
+      if (fileVolume === '') {
+        fileVolume = '/tmp/luks-eggs-data'
+      }
 
-      Utils.warning(`Set up encryption`)
-      await exec(`cryptsetup luksFormat ${this.luksFile}`)
-      
-      Utils.warning(`Open LUKS volume`)
-      await exec(`cryptsetup luksOpen ${this.luksFile} ${this.luksName}`)
+      this.luksName = path.basename(fileVolume)
+      this.luksFile = fileVolume
+      this.luksDevice = `/dev/mapper/${this.luksName}`
+      this.luksMountpoint = '/tmp/eggs-data'
+      await this.luksCreate()
+    } else {
+      Utils.useRoot(this.id)
+    }
+  }
 
-      let destHome="/home"
-      let destEtc = "/etc"
 
-      Utils.warning(`Create a dummy filesystem`)
-      let dummy_fs="/tmp/dummy_fs"
-      await exec(`mkdir -p ${dummy_fs}${destEtc}`)
-      await exec(`cp /etc/group /etc/passwd /etc/shadow ${dummy_fs}${destEtc}`)
-      //await exec(`mkdir -p ${dummy_fs}${destHome}`)
-      //await exec(`touch ${dummy_fs}${destHome}.keepIt`)
-      await exec(`mksquashfs ${dummy_fs} /dev/mapper/${this.luksName} -progress -noappend`)     
-      await exec(`rm -rf ${dummy_fs}`)
+  /**
+   *
+   */
+  async luksCreate() {
+    Utils.warning(`Erasing previous LUKS Volume on ${this.luksFile}`)
+    let clean = `rm -rf /tmp/${this.luksName}`
+    await exec(clean)
 
-      Utils.warning(`Appending /home`)
-      let srcHome="/home/"
-      await exec(`mksquashfs ${srcHome} /dev/mapper/${this.luksName} -keep-as-directory -progress -e /home/eggs -ef /etc/penguins-eggs.d/exclude.list.d/exclude.list.cryptedclone`)
+    let maxSize = "10G"
+    Utils.warning(`Creating LUKS Volume on ${this.luksFile}, size ${maxSize}`)
+    await exec(`truncate -s ${maxSize} ${this.luksFile}`)
+
+    Utils.warning(`Set up encryption`)
+    await exec(`cryptsetup luksFormat ${this.luksFile}`)
+
+    Utils.warning(`Open LUKS volume`)
+    await exec(`cryptsetup luksOpen ${this.luksFile} ${this.luksName}`)
+
+    Utils.warning(`Create a dummy filesystem for /etc`)
+    let dummy_fs = "/tmp/dummy_fs"
+    await exec(`mkdir -p ${dummy_fs}/etc`)
+    await exec(`cp /etc/group /etc/passwd /etc/shadow ${dummy_fs}/etc`)
+    await exec(`mksquashfs ${dummy_fs} /dev/mapper/${this.luksName} ${this.compression} -progress -noappend`)
+    await exec(`rm -rf ${dummy_fs}`)
+
+    Utils.warning(`Appending /home`)
+    let srcHome = "/home/"
+
+    let e = '' // exclude nest
+    let c = '' // compression
+    if (await this.settings.load()) {
+      e = `-e ${this.settings.config.snapshot_mnt}`
+      c = `-comp ${this.settings.config.compression}`
+    }
+
+    let ef = ''  // exclude file
+    if (this.excludeFiles) {
+      ef = `-ef ${this.excludeFile}`
+
+      await exec(`mksquashfs ${srcHome} /dev/mapper/${this.luksName} ${c} -keep-as-directory -progress ${e} ${ef}`)
 
       Utils.warning(`Calculate used up space of squashfs`)
-      let cmd=`unsquashfs -s /dev/mapper/${this.luksName} | grep "Filesystem size"| sed -e 's/.*size //' -e 's/ .*//'`
+      let cmd = `unsquashfs -s /dev/mapper/${this.luksName} | grep "Filesystem size"| sed -e 's/.*size //' -e 's/ .*//'`
       let sizeString = (await exec(cmd, { echo: false, capture: true })).data
-      let size=parseInt(sizeString)
+      let size = parseInt(sizeString)
       Utils.warning(`Squashfs size: ${size} bytes`)
 
-      const blockSize=131072
+      const blockSize = 131072
       // get number of blocks and pad squashfs
-      let blocks=size / blockSize
+      let blocks = size / blockSize
       if (blocks % 1 != 0) { // Se il numero di blocchi non è un numero intero
         blocks = Math.ceil(blocks); // Arrotonda all'intero successivo
         size = blocks * blockSize; // Calcola la nuova dimensione
@@ -163,56 +169,17 @@ export default class Syncto extends Command {
       Utils.warning(`Padded squashfs on device, size ${size} bytes, bloks: ${blocks} of ${blockSize} bytes `)
 
       // Shrink LUKS volume
-      let luksBlockSize=512
-      let luksBlocks=size / luksBlockSize
+      let luksBlockSize = 512
+      let luksBlocks = size / luksBlockSize
       Utils.warning(`Shrinking LUKS volume to ${luksBlocks} blocks of ${luksBlockSize} bytes`)
       await exec(`cryptsetup resize ${this.luksName} -b ${luksBlocks}`)
 
       // # Get final size and shrink image file
-      let luksOffset=+(await exec(`cryptsetup status ${this.luksName} | grep offset | sed -e 's/ sectors$//' -e 's/.* //'`, {echo:false, capture:true})).data
-      let finalSize=luksOffset * luksBlockSize + size
+      let luksOffset = +(await exec(`cryptsetup status ${this.luksName} | grep offset | sed -e 's/ sectors$//' -e 's/.* //'`, { echo: false, capture: true })).data
+      let finalSize = luksOffset * luksBlockSize + size
       await exec(`cryptsetup luksClose ${this.luksName}`)
       await exec(`truncate -s ${finalSize} ${this.luksFile}`)
       Utils.warning(`Final size is ${finalSize} bytes`)
-
-      // Moving the file to the right place
-      await exec(`mv ${this.luksFile} /run/live/medium/live/`)
     }
-
-    /**
-     *
-     */
-    async luksOpen() {
-      if (!fs.existsSync(this.luksDevice)) {
-        // Utils.warning(`LUKS open volume: ${this.luksName}`)
-        await exec(`cryptsetup luksOpen --type luks2 ${this.luksFile} ${this.luksName}`, Utils.setEcho(true))
-      } else {
-        Utils.warning(`LUKS volume: ${this.luksName} already open`)
-      }
-
-      if (!fs.existsSync(this.luksMountpoint)) {
-        await exec(`mkdir -p ${this.luksMountpoint}`, this.echo)
-      }
-
-      if (!Utils.isMountpoint(this.luksMountpoint)) {
-        Utils.warning(`mount volume: ${this.luksDevice} on ${this.luksMountpoint}`)
-        await exec(`mount ${this.luksDevice} ${this.luksMountpoint}`, this.echo)
-      } else {
-        Utils.warning(`mount volume: ${this.luksDevice} already mounted on ${this.luksMountpoint}`)
-      }
-    }
-
-    /**
-    *
-    */
-    async luksClose() {
-      if (Utils.isMountpoint(this.luksMountpoint)) {
-        await exec(`umount ${this.luksMountpoint}`, this.echo)
-      }
-
-      if (fs.existsSync(this.luksDevice)) {
-        Utils.warning(`LUKS close volume: ${this.luksName}`)
-        await exec(`cryptsetup luksClose ${this.luksName}`, this.echo)
-      }
-    }
+  }
 }
