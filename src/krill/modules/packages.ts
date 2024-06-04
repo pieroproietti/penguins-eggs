3ad9e2aedbcc296af643cc1fe4e4266129745c0f
Piero Proietti <piero.proietti@gmail.com>
2024-06-04 17:38:44 +0200

    ripassiamo ad ambiente live

--- src/krill/modules/packages.ts
+++ src/krill/modules/packages.ts
@@ -15,6 +15,7 @@ import Utils from '../../classes/utils.js'
 import { IPackages } from '../../interfaces/i-packages.js'
 import { exec } from '../../lib/utils.js'
 import Sequence from '../sequence.js'
+import { tryInstall } from '../../classes/incubation/fisherman-helper/packages.js'
 
 /**
  *
@@ -39,36 +40,36 @@ export default async function packages(this: Sequence): Promise<void> {
     console.log("il file di configurazione esiste")
     await Utils.pressKeyToExit()
     
-    const packages = yaml.load(fs.readFileSync(config_file, 'utf8')) as IPackages
+    const packages: IPackages = yaml.load(fs.readFileSync(config_file, 'utf8')) as IPackages
     console.log("packages.conf")
     console.log(packages)
     await Utils.pressKeyToExit("")
 
-    // sino a qua arriva
-    console.log("packages try remove")
-    console.log(packages.operations.try_remove.packages)
+    const tryRemovePackages = packages.operations.try_remove.packages;
+    console.log("tryRemovePackages")
+    console.log(tryRemovePackages)
     await Utils.pressKeyToExit("")
 
-    console.log("packages try install")
-    console.log(packages.operations.try_install.packages)
+    const tryInstallPackages = packages.operations.try_install.packages;
+    console.log("tryInstallPackages")
+    console.log(tryInstallPackages)
     await Utils.pressKeyToExit("")
-
     
 
     if (packages.backend === 'apt') {
       // Debian/Devuan/Ubuntu
-      if (packages.operations.try_remove.packages.length > 0) {
+      if (tryRemovePackages.length > 0) {
         let cmd = `chroot ${this.installTarget} apt-get purge -y `
-        for (const elem of packages.operations.try_remove.packages) {
+        for (const elem of tryRemovePackages) {
           cmd += elem + ' '
         }
         await exec(`${cmd} ${this.toNull}`, this.echo)
         await exec(`chroot ${this.installTarget} apt-get autoremove -y ${this.toNull}`, this.echo)
       }
 
-      if (packages.operations.try_install.packages.length > 0) {
+      if (tryInstallPackages.length > 0) {
         let cmd = `chroot ${this.installTarget} apt-get install -y `
-        for (const elem of packages.operations.try_install.packages) {
+        for (const elem of tryInstallPackages) {
           cmd += elem + ' '
         }
 
@@ -79,17 +80,17 @@ export default async function packages(this: Sequence): Promise<void> {
     } else if (packages.backend === 'pacman') {
 
       // arch/manjaro
-      if (packages.operations.try_remove.packages.length > 0) {
+      if (tryRemovePackages.length > 0) {
         let cmd = `chroot ${this.installTarget} pacman -R\n`
-        for (const elem of packages.operations.try_remove.packages) {
+        for (const elem of tryRemovePackages) {
           cmd += elem + ' '
         }
 
         await exec(`${cmd} ${echoYes}`, this.echo)
       }
 
-      if (packages.operations.try_install.packages.length > 0) {
-        for (const elem of packages.operations.try_install.packages) {
+      if (tryInstallPackages.length > 0) {
+        for (const elem of tryInstallPackages) {
           await exec(`chroot ${this.installTarget} pacman -S ${elem}`, echoYes)
         }
       }
