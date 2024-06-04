210031ee720ed3b5d728ca65c7ef8be93c9fcd1a
Piero Proietti <piero.proietti@gmail.com>
2024-06-04 18:14:01 +0200

    vediamo...

--- src/krill/modules/packages.ts
+++ src/krill/modules/packages.ts
@@ -37,25 +37,21 @@ export default async function packages(this: Sequence): Promise<void> {
     console.log("il file di configurazione NON esiste")
     await Utils.pressKeyToExit()
   } else {
-    console.log("il file di configurazione esiste")
-    await Utils.pressKeyToExit()
-    
     const packages: IPackages = yaml.load(fs.readFileSync(config_file, 'utf8')) as IPackages
     console.log("packages")
     console.log(packages)
 
     const tryRemovePackages = packages.operations.try_remove.packages
-    console.log("tryRemovePackages" + tryRemovePackages.length)
-    console.log(tryRemovePackages)
-
     const tryInstallPackages = packages.operations.try_install.packages
-    console.log("tryInstallPackages"  + tryInstallPackages.length)
-    console.log(tryInstallPackages)
-    
     if (tryRemovePackages.length === 0 && tryInstallPackages.length === 0) {
       console.log("nessun pacchetto da installare o rimuovere")
     }
-    await Utils.pressKeyToExit("")
+    console.log(`tryRemovePackages: ${tryRemovePackages.length}`)
+    console.log(tryRemovePackages)
+
+    console.log(`tryInstallPackages: ${tryInstallPackages.length}`)
+    console.log(tryInstallPackages)
+    await Utils.pressKeyToExit("puttana la miseria")
 
     if (packages.backend === 'apt') {
       // Debian/Devuan/Ubuntu
@@ -73,7 +69,6 @@ export default async function packages(this: Sequence): Promise<void> {
         for (const elem of tryInstallPackages) {
           cmd += elem + ' '
         }
-
         await exec(`chroot ${this.installTarget} apt-get update ${this.toNull}`, this.echo)
         await exec(`${cmd} ${this.toNull}`, this.echo)
       }
