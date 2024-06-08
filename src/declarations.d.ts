9c7b50f9de297b3fd6d114293cb4b62da4ddf66e
Piero Proietti <piero.proietti@gmail.com>
2024-06-08 11:05:19 +0200

    declarations.d.ts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,14 @@
+/*
+Present code gives an error:
+
+error TS2665: Invalid module name in augmentation. Module 'node-proxy-dhcpd' resolves to an untyped module at '/home/artisan/penguins-eggs/node_modules/.pnpm/node-proxy-dhcpd@0.0.5/node_modules/node-proxy-dhcpd/dist/index.js', which cannot be augmented.
+
+2 declare module 'node-proxy-dhcpd' {
+                 ~~~~~~~~~~~~~~~~~~
+
+
+Found 1 error in src/declarations.d.ts:2
+
 import { IPacket, IDhcpOptions } from '../src/interfaces/i-pxe.ts'
 declare module 'node-proxy-dhcpd' {
     export class dhcpd {
@@ -9,3 +20,6 @@ declare module 'node-proxy-dhcpd' {
         proxy_request: (pkt: IPacket) => IPacket;
     }
 }
+*/
+
+declare module 'node-proxy-dhcpd';
