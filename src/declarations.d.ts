637b079eb5b1119607ea48e1b46cc01f800be00d
Piero Proietti <piero.proietti@gmail.com>
2024-06-08 09:47:51 +0200

    le ho provate tutte...

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,11 @@
-declare module 'node-proxy-dhcpd';
+import { IPacket, IDhcpOptions } from '../src/interfaces/i-pxe.ts'
+declare module 'node-proxy-dhcpd' {
+    export class dhcpd {
+        constructor(opts: IDhcpOptions);
+        pre_init: (pkt: IPacket) => void;
+        discover: (pkt: IPacket) => void;
+        request: (pkt: IPacket) => void;
+        inform: (pkt: IPacket) => void;
+        proxy_request: (pkt: IPacket) => IPacket;
+    }
+}
