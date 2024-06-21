602941fcc1f1a15cd856e54a4239d294f1f76aa1
Piero Proietti <piero.proietti@gmail.com>
2024-06-21 06:58:55 +0200

    trying to use not only yolk during installation

--- conf/distros/buster/calamares/calamares-modules/sources-yolk/sources-yolk.sh
+++ conf/distros/buster/calamares/calamares-modules/sources-yolk/sources-yolk.sh
@@ -4,66 +4,40 @@
 # We use just yolk during installation
 #
 # sources-yolk.sh
-# add yolk, rimuove LIST, LIST.d ed esegue apt non autenticato
+# add yolk
 #
 # sources-yolk.sh -u
-# remove yolk, restore LIST, LIST.d ed esegue apt non autenticato
+# remove yolk
 
 #
 #
 #
 main() {
     if [ "$1" = "-u" ]; then
-        restore
+        remove
     else
-        backup
-        yolk
+        add
     fi
     sync
     exit 0
 }
 
+
 #
 #
 #
-backup() {
-    if [ -f "$BACKUP" ]; then
-        rm -f "$BACKUP"
+remove() {
+    if [ -f "YOLK_LIST" ]; then
+        rm -f "YOLK_LIST"
     fi
 
-    if [ -d "$BACKUP_D" ]; then
-        rm -rf "$BACKUP_D"
-    fi
-
-    mv "$LIST" "$BACKUP"
-
-    mv "$LIST_D" "$BACKUP_D"
 }
 
 #
 #
 #
-restore() {
-    if [ -f "$LIST" ]; then
-        rm -f "$LIST"
-    fi
-
-    if [ -d "$LIST_D" ]; then
-        rm -rf "$LIST_D"
-    fi
-
-    mv "$BACKUP" "$LIST"
-
-    mv "$BACKUP_D" "$LIST_D"
-}
-
-#
-#
-#
-yolk() {
-    mkdir -p "$LIST_D"
-    echo "deb [trusted=yes] file:/var/local/yolk ./" > "$LIST_D/yolk.list"
-    touch "$LIST"
+add() {
+    echo "deb [trusted=yes] file:/var/local/yolk ./" > "$YOLK_LIST"
     chroot "$CHROOT" apt-get --yes update
 }
 
@@ -71,12 +45,7 @@ yolk() {
 # WE START HERE
 #
 CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
-
-LIST="$CHROOT/etc/apt/sources.list"
-BACKUP="$LIST.backup"
-
-LIST_D="$LIST.d"
-BACKUP_D="$LIST_D.backup"
+YOLK_LIST="$CHROOT/etc/apt/sources.list.d/yolk.list"
 
 main "$1"
 
