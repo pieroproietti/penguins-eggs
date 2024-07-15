6a497337fb55252019a31936d022bc3ccfdd3435
Piero Proietti <piero.proietti@gmail.com>
2024-07-15 14:48:05 +0200

    cambiato cal-eggs

--- src/classes/families/archlinux.ts
+++ src/classes/families/archlinux.ts
@@ -28,7 +28,9 @@ export default class Archlinux {
     // const cal_eggs = 'arco-calamares-3.3.2-02-x86_64.pkg.tar.zst' // 2024-02-10
     // const cal_eggs = 'arco-calamares-3.3.5-02-x86_64.pkg.tar.zst' // 2024-03-10
     // const cal_eggs = 'calamares-garuda-3.3.5.r19.g10acebff4-1-x86_64.pkg.tar.zst' // 2024-03-10
-    const cal_eggs = 'arco-calamares-3.3.6-06-x86_64.pkg.tar.zst' // 24/04/27
+    // const cal_eggs = 'arco-calamares-3.3.6-06-x86_64.pkg.tar.zst' // 24/04/27
+    const cal_eggs = 'calamares-eggs-debug-3.3.8.r39.g80ef430-1-x86_64.pkg.tar.zst' // 12/07/24
+
     let cmd = `wget -O /tmp/${cal_eggs} https://sourceforge.net/projects/penguins-eggs/files/PKGBUILD/${cal_eggs}/download`
     try {
       await exec(cmd, echo)
