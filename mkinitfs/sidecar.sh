53f60de763ba574287e30af18651b0e065aa2d55
Piero Proietti <piero.proietti@gmail.com>
2024-07-26 12:03:49 +0200

    sidecar initramfs

--- mkinitfs/sidecar.sh
+++ mkinitfs/sidecar.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
-
-# Montare i filesystem necessari
-#mount -t proc proc /proc
-#mount -t sysfs sysfs /sys
-
-# Attendere che i dispositivi siano pronti
-#sleep 3
-
-# Rilevare il CD con la label 'colibri'
-# L'istruzione è corretta
-CDROM=$(blkid -l -o device -t LABEL=colibri)
-if [ -z "$CDROM" ]; then
-    echo "CD con label 'colibri' non trovato!"
-    # exec sh
-fi
-
-# Montare il CD
-#mount -r $CDROM /mnt
+#
+# At the end of the boot, initram goes into emergency,
+# we give the following commands:
+# mkdir /mnt
+# mount /dev/sr0 /mnt
+#
+# Then /mnt/live/sidecar.sh is launched.
+#  
 
 # Creare i punti di montaggio necessari
 mkdir -p /media/root-ro 
@@ -28,6 +18,7 @@ mount -t squashfs /mnt/live/filesystem.squashfs /media/root-ro
 # Montare il filesystem tmpfs per la parte writable
 mount -t tmpfs root-tmpfs /media/root-rw
 
+# Creare i punti di montaggio necessari
 mkdir -p /media/root-rw/work 
 mkdir -p /media/root-rw/root
 
@@ -38,16 +29,11 @@ mount -t overlay overlay -o lowerdir=/media/root-ro,upperdir=/media/root-rw/root
 mkdir -p /newroot/dev /newroot/proc /newroot/sys /newroot/run
 
 # Montare i filesystem virtuali nel nuovo root
-#mount --move /dev /newroot/dev
-#mount --move /proc /newroot/proc
-#mount --move /sys /newroot/sys
-#mount --move /run /newroot/run
-
 mount -t devtmpfs devtmpfs /newroot/dev
 mount -t proc proc /newroot/proc
 mount -t sysfs sysfs /newroot/sys
 mount -t tmpfs tmpfs /newroot/run
-// copia di run
+// run viene copiato
 cp -r /run/* /newroot/run
 
 # Eseguire switch_root e continuare il boot
