8a0fc7176a111edf97696c8f754b6dc0e36d48d3
Piero Proietti <piero.proietti@gmail.com>
2024-07-28 11:06:34 +0200

    sidecar again

--- mkinitfs/sidecar.sh
+++ mkinitfs/sidecar.sh
@@ -4,36 +4,35 @@
 # we give the following commands:
 # mkdir /mnt
 # mount /dev/sr0 /mnt
-#
 # Then /mnt/live/sidecar.sh is launched.
 #  
 
-# Creare i punti di montaggio necessari
+# Creating mountpoints
 mkdir -p /media/root-ro 
 mkdir -p /media/root-rw 
 
-# Montare il filesystem squashfs
+# mount filesystem squashfs on /media/root-ro
 mount -t squashfs /mnt/live/filesystem.squashfs /media/root-ro
 
-# Montare il filesystem tmpfs per la parte writable
+# Mount tmpfs on /media/root-rw
 mount -t tmpfs root-tmpfs /media/root-rw
 
 # Creare i punti di montaggio necessari
 mkdir -p /media/root-rw/work 
 mkdir -p /media/root-rw/root
 
-# Montare overlayfs
+# mount  overlayfs on /newroot
 mount -t overlay overlay -o lowerdir=/media/root-ro,upperdir=/media/root-rw/root,workdir=/media/root-rw/work /newroot
 
-# mount virtual filesystems
+# mount virtual filesystems on /newroot
 mount -t devtmpfs devtmpfs /newroot/dev
 mount -t proc proc /newroot/proc
 mount -t sysfs sysfs /newroot/sys
 mount -t tmpfs tmpfs /newroot/run
 cp -r /run/* /newroot/run
 
-echo "sidecar: /newroot was mounted correctly"
-echo "         Type 'exit' to continue boot"
+echo "sidecar: /newroot was mounted!"
+echo "         check it then type 'exit' to continue boot"
 
 
 
