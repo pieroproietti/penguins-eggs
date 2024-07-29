ba8b21ff8cb31255958340f7fc41fa60c73edd97
Piero Proietti <piero.proietti@gmail.com>
2024-07-29 21:00:54 +0200

    discovering dracut

--- src/classes/ovary.ts
+++ src/classes/ovary.ts
@@ -832,14 +832,13 @@ export default class Ovary {
    */
   async initrdAlpine() {
     Utils.warning(`creating ${path.basename(this.settings.initrdImg)} Alpine on ISO/live`)
-    // let initrdImg = '/home/artisan/initramfs/live/initramfs-live.gz'
-    // await exec(`cp ${initrdImg} ${this.settings.iso_work}live/initramfs-lts`)
-
     let initrdImg = 'initramfs-lts'
-    const pathConf = path.resolve(__dirname, `../../mkinitfs/live.conf`)
+    const pathConf = path.resolve(__dirname, `../../dracut/dracut.conf.d`)
+    await exec(`dracut --confdir ${pathConf} ${this.settings.iso_work}live/${initrdImg}`, Utils.setEcho(true))
+    // const pathConf = path.resolve(__dirname, `../../mkinitfs/live.conf`)
+    // await exec(`mkinitfs -c ${pathConf} -o ${this.settings.iso_work}live/${initrdImg}`, Utils.setEcho(true))
     const sidecar = path.resolve(__dirname, `../../mkinitfs/sidecar.sh`)
-    await exec(`mkinitfs -c ${pathConf} -o ${this.settings.iso_work}live/${initrdImg}`, Utils.setEcho(true))
-    await exec(`cp ${sidecar} ${this.settings.iso_work}live/`)
+     await exec(`cp ${sidecar} ${this.settings.iso_work}live/`)
   }
 
   /**
