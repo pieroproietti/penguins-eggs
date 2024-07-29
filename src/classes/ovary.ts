cb6888e910c51dd82c23729224b5d60c17e5df1a
artisan <artisan@colibri.my.domain>
2024-07-29 09:53:29 +0200

    ritocco

--- src/classes/ovary.ts
+++ src/classes/ovary.ts
@@ -846,9 +846,7 @@ export default class Ovary {
    */
   async initrdFedora() {
     Utils.warning(`creating ${path.basename(this.settings.initrdImg)} Fedora on ISO/live`)
-    const sidecar = path.resolve(__dirname, `../../mkinitfs/sidecar.sh`)
-    await exec(`cp ${sidecar} ${this.settings.iso_work}live/`)
-    await exec(`cp /boot/initramfs-custom.img ${this.settings.iso_work}/live/`, this.echo)
+    await exec(`cp /boot/initramfs-*.img ${this.settings.iso_work}/live/`, this.echo)
   }
 
 
