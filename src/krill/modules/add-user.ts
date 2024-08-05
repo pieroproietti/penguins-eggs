b105f4011684e5efb671b37ba40022999b13aa96
Piero Proietti <piero.proietti@gmail.com>
2024-08-05 08:06:51 +0200

    add-user modified

--- src/krill/modules/add-user.ts
+++ src/krill/modules/add-user.ts
@@ -22,26 +22,29 @@ import Sequence from '../sequence.js'
  * @param homePhone
  */
 export default async function addUser(this: Sequence, name = 'live', password = 'evolution', fullName = '', roomNumber = '', workPhone = '', homePhone = ''): Promise<void> {
-  // Debian
+  // adduser user
   let cmd = `chroot ${this.installTarget} adduser ${name} --home /home/${name} --shell /bin/bash --disabled-password --gecos "${fullName},${roomNumber},${workPhone},${homePhone}" ${this.toNull}`
   if (this.distro.familyId === 'archlinux') {
     cmd = `chroot ${this.installTarget} useradd --create-home --shell /bin/bash ${name} ${this.toNull}`
   }
-
   await exec(cmd, this.echo)
 
+  // chapasswd user
   cmd = `echo ${name}:${password} | chroot ${this.installTarget} chpasswd ${this.toNull}`
   await exec(cmd, this.echo)
 
-  // Debian
-  cmd = `chroot ${this.installTarget} usermod -aG sudo ${name} ${this.toNull}`
-  if (this.distro.familyId === 'archlinux') {
-    cmd = `chroot ${this.installTarget} usermod -aG wheel ${name}`
+  // sudo ot wheel
+  let group = 'sudo'
+  if (this.distro.familyId === 'archlinux'|| 
+      this.distro.familyId === 'alpine') {
+    group = 'wheel'
   }
+  cmd = `chroot ${this.installTarget} usermod -aG ${group} ${name} ${this.toNull}`
+  await exec(cmd, this.echo)
 
+  // autologin
   try {
     await exec(cmd, this.echo)
-    // check or create group: autologin
     if (this.distro.familyId === 'archlinux') {
       await exec(`chroot ${this.installTarget} getent group autologin || groupadd autologin`)
       await exec(`chroot ${this.installTarget} gpasswd -a ${this.settings.config.user_opt} autologin`)
