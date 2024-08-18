775a77c790a123d68cdd0f82d2ca84bc25a5f1c4
Piero Proietti <piero.proietti@gmail.com>
2024-08-18 13:40:19 +0200

    sleep

--- src/krill/sequence.tsx
+++ src/krill/sequence.tsx
@@ -331,7 +331,9 @@ export default class Sequence {
          } catch (error) {
             await Utils.pressKeyToExit(JSON.stringify(error))
          }
-
+         sleep(500) // diamo il tempo di montare
+         
+         /*
          let testRoot=`mount|grep ${this.devices.root.name}`
          let test = await exec(testRoot)
          if (test.data === '') {
@@ -339,6 +341,7 @@ export default class Sequence {
             console.log(this.devices)
             await emergencyShell(message)
          }
+         */
 
          // mountVfs
          message = "Mounting on target VFS "
