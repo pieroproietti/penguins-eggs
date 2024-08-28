628d62f52be867ca124c9f949041f975d2f13c4e
Piero Proietti <piero.proietti@gmail.com>
2024-08-28 14:38:25 +0000

    alpinelivelabel alpinelivesquashfs

--- src/classes/ovary.ts
+++ src/classes/ovary.ts
@@ -1024,7 +1024,7 @@ export default class Ovary {
       kp += `boot=live components locales=${process.env.LANG}`
       kp += isMiso(distroId) ? ` misobasedir=manjaro misolabel=${this.volid}` : ` archisobasedir=arch archisolabel=${this.volid}`
     } else if (this.familyId === 'alpine') {
-      kp += `alpinebasedirname=/mnt/live/ alpinelivelabel=${this.volid}`
+      kp += `alpinelivelabel=${this.volid} alpinelivesquashfs=/mnt/live/filesystem.squashfs`
     }
     kp += ` cow_spacesize=4G`
     return kp
