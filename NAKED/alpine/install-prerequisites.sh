0da7be7326ff890b82248a1e6980cb871249c862
Piero Proietti <piero.proietti@gmail.com>
2024-09-18 21:38:10 +0200

    NAKED

---
+++ NAKED/alpine/install-prerequisites.sh
@@ -0,0 +1,34 @@
+apk add \
+    alpine-conf \
+    apk-tools \
+    bash-completion \
+    cryptsetup \
+    curl \
+    docs \
+    dosfstools \
+    fuse \
+    git \
+    jq \
+    lsb-release \
+    lsblk \
+    lvm2 \
+    man-pages \
+    mandoc \
+    mandoc-apropos \
+    mkinitfs \
+    musl-locales \
+    musl-utils \
+    nano \
+    nodejs \
+    npm \
+    parted \
+    rsync \
+    shadow \
+    squashfs-tools \
+    sshfs \
+    syslinux \
+    xorriso
+
+echo "fuse" | tee /etc/modules-load.d/fuse.conf
+
+npm i pnpm -g
