d4b55df45cb24367caf3a959bf88edf868c25a95
Piero Proietti <piero.proietti@gmail.com>
2024-09-19 17:27:43 +0200

    fedora naked

--- NAKED/fedora/install-prerequisites.sh
+++ NAKED/fedora/install-prerequisites.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+# This script installs prerequisites for penguins-eggs
+# on fedora linux, it is intended for development purposes 
+
+# check if we are root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+# check if we are on fedora
+if [ ! -f /etc/fedora-release ]; then
+    echo "This script is intended for fedora linux only"
+    exit 1
+fi
+
+# update
+dnf -y update
+
+
 dnf -y install \
     bash-completion \
     cryptsetup \
@@ -14,6 +35,7 @@ dnf -y install \
     lvm2 \
     nodejs \
     npm \
+    nvme-cli \
     overlayfs-tools \
     parted \
     rsync \
@@ -23,19 +45,8 @@ dnf -y install \
     xorriso \
     zstd
 
+# install pnpm
 npm i pnpm -g
 
-# biosdevname \
-# cifs-utils \
-# iproute \
-# iscsi-initiator-utils \
-# net-tools \
-# nfs-utils \
-# mdadm \
-# nbd \
-# rng-tools \
-# rpcbind \
-# syslinux \
-# wicked \
-
-# nvme-cli \
+# disable selinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
