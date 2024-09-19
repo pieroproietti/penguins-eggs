06097f751e308d05f83d66dafe72297ecbe7edad
Piero Proietti <piero.proietti@gmail.com>
2024-09-19 09:43:11 +0200

    alpine

--- NAKED/alpine/install-prerequisites.sh
+++ NAKED/alpine/install-prerequisites.sh
@@ -4,11 +4,32 @@
 # on alpine linux, it is intended for development purposes 
 # only
 
+# check if we are root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
 # check if we are on alpine
 if [ ! -f /etc/alpine-release ]; then
     echo "This script is intended for alpine linux only"
     exit 1
 fi
+
+# controlla la release
+if [ "$(lsb_release -rs)" != "3.20.3" ]; then
+    echo "This script is intended for alpine 3.20.3 only"
+    exit 1
+fi
+
+# copy on /etc/apk/repositories
+cat <<EOF > /etc/apk/repositories
+#/media/cdrom/apks
+http://dl-cdn.alpinelinux.org/alpine/v3.20/main
+http://dl-cdn.alpinelinux.org/alpine/v3.20/community
+@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing
+@edge https://dl-cdn.alpinelinux.org/alpine/edge/community
+EOF
 
 # update
 apk update
