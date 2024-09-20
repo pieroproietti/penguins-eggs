7285bf042086cba5d1dbc953f23c4df7ddbf284b
Piero Proietti <piero.proietti@gmail.com>
2024-09-20 08:41:57 +0200

    tempt suse partition

--- src/krill/modules/partition.ts
+++ src/krill/modules/partition.ts
@@ -64,6 +64,24 @@ export default async function partition(this: Sequence): Promise<boolean> {
     // No default
   }
 
+
+  if (this.distro.familyId === 'opensuse') {
+    // OpenSUSE
+    await exec(`parted --script ${installDevice} mklabel gpt`, this.echo)
+    // crea partizione bootBIOS da 10MB e ext4 da 11MB a 100%
+    await exec(`parted --script mkpart primary ext2 1MiB 10MiB`, this.echo)
+    await exec(`parted --script mkpart primary ext4 11MiB 100%`, this.echo)
+    await exec(`parted --script ${installDevice} set 1 bios_grub on`, this.echo) // sda1
+
+    // ROOT
+    this.devices.root.name = `${installDevice}${p}2`
+    this.devices.root.fsType = 'ext4'
+    this.devices.root.mountPoint = '/'
+
+    return true
+
+  }
+
   if (installMode === 'standard' && !this.efi) {
     /**
      * ===========================================================================================
