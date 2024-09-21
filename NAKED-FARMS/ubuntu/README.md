75fd9222388230325b44e95c4f179112d06da66c
Piero Proietti <piero.proietti@gmail.com>
2024-09-21 17:33:34 +0200

    ubuntu

---
+++ NAKED-FARMS/ubuntu/README.md
@@ -0,0 +1,9 @@
+# NAKED Ubuntu
+
+We start with `ubuntu-24.04.1-live-server-amd64.iso` our `prerequisites.sh` is the same as for Debian, plus we remove two packages for server: cloud-init and needrestart.
+
+All you must to do is run `sudo ./penguins-eggs/NAKED-FARMS/ubunu/prerequisites.sh` then `./penguins-eggs/install-eggs-dev.sh`.
+
+
+
+
---
+++ NAKED-FARMS/ubuntu/install-prerequisites.sh
@@ -0,0 +1,47 @@
+!#/bin/bash
+
+# check if we are root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# check if we are on ubuntu
+if [ ! -f /etc/os-release ]; then
+    echo "This script is only for Ubuntu"
+    exit
+fi
+
+# install prerequisites
+apt-get install -y \
+    coreutils \
+    cryptsetup \
+    curl \
+    dosfstools \
+    dpkg-dev \
+    git \
+    isolinux \
+    jq \
+    live-boot \
+    live-boot-doc \
+    live-boot-initramfs-tools \
+    live-config-systemd \
+    live-tools \
+    lsb-release \
+    lvm2 \
+    nodejs \
+    npm \
+    parted \
+    pxelinux \
+    rsync \
+    squashfs-tools \
+    sshfs \
+    syslinux \
+    syslinux-common \
+    xorriso
+
+# Install pnpm
+npm i pnpm -g
+
+ # remove cloud-init needrestart
+ apt purge cloud-init needrestart 
