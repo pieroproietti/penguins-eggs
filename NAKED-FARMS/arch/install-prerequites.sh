b2918254a15328ca9b15960cfbae4b3c01e902d7
Piero Proietti <piero.proietti@gmail.com>
2024-09-22 07:57:04 +0200

    arch/install-prerequisites

---
+++ NAKED-FARMS/arch/install-prerequites.sh
@@ -0,0 +1,30 @@
+pacman -Syu --noconfirm --needed \
+		arch-install-scripts \
+		bash-completion \
+		dosfstools \
+		erofs-utils \
+		findutils \
+		git \
+		grub \
+		jq \
+		libarchive \
+		libisoburn \
+		lsb-release \
+		lvm2 \
+		mkinitcpio-archiso \
+		mkinitcpio-nfs-utils \
+		mtools \
+		nbd \
+		nodejs \
+		pacman-contrib \
+		parted \
+		procps-ng \
+		pv \
+		python \
+		rsync \
+		squashfs-tools \
+		sshfs \
+		syslinux \
+		xdg-utils
+        
+	
