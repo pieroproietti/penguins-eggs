fd841911421114b560e29036f0bf70409decf629
Piero Proietti <piero.proietti@gmail.com>
2024-09-22 11:12:11 +0200

    added Devuan naked

---
+++ NAKED-FARMS/devuan/install-prerequisites.sh
@@ -0,0 +1,45 @@
+!#/bin/bash
+
+# check if we are root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# check if we are on debian
+if [ ! -f /etc/os-release ]; then
+    echo "This script is only for Debian"
+    exit
+fi
+
+# install prerequisites
+apt-get install -y \
+    coreutils \
+    cryptsetup \
+    curl \
+    dosfstools \
+    dpkg-dev \
+    git \
+    isolinux \
+    jq \
+    live-boot \
+    live-boot-doc \
+    live-boot-initramfs-tools \
+    live-config-sysvinit \
+    live-tools \
+    lsb-release \
+    lvm2 \
+    nodejs \
+    npm \
+    parted \
+    pxelinux \
+    rsync \
+    squashfs-tools \
+    sshfs \
+    syslinux \
+    syslinux-common \
+    xorriso
+
+# Install pnpm
+npm i pnpm -g
+
