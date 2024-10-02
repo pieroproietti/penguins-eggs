37998fedf3584ffd9eceab18545fc38c81c4d4d7
Piero Proietti <piero.proietti@gmail.com>
2024-10-02 12:44:56 +0200

    pnpm update

--- efiboot.sh
+++ efiboot.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /tmp/efi
 # create bootx64.efi
-grub2-mkimage -o /tmp/efi/bootx64.efi -O x86_64-efi -p /EFI/BOOT part_gpt part_msdos fat ext2 search_disk_uuid normal configfile linux help loadenv test cat echo
+grub-mkimage -o /tmp/efi/bootx64.efi -O x86_64-efi -p /EFI/BOOT part_gpt part_msdos fat ext2 search_disk_uuid normal configfile linux help loadenv test cat echo
 
 # using mtools create efiboot.img
 dd if=/dev/zero of=/tmp/efi/efiboot.img bs=1M count=10
