dcc341da4a9f8b6c218df50cb698a88d6b24ff71
Piero Proietti <piero.proietti@gmail.com>
2024-11-03 11:27:29 +0100

    added rockylinux

---
+++ PREREQUISITES/rockylinux/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script installs prerequisites for penguins-eggs
+# on fedora linux, it is intended for development purposes 
+
+# check if we are root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+# check if we are on fedora
+if [ ! -f /etc/rocky-release ]; then
+    echo "This script is intended for rocky linux only"
+    exit 1
+fi
+
+# update
+dnf -y update
+
+
+dnf -y install \
+    bash-completion \
+    cryptsetup \
+    curl \
+    device-mapper \
+    dosfstools \
+    dracut \
+    dracut-live \
+    fuse \
+    git \
+    grub2-tools-extra \
+    jq \
+    lvm2 \
+    nodejs \
+    npm \
+    nvme-cli \
+    parted \
+    rsync \
+    squashfs-tools \
+    wget \
+    xdg-user-dirs \
+    xorriso \
+    zstd
+
+# install pnpm
+npm i pnpm@8 -g
+
+# mkdir /usr/share/icons
+mkdir -p /usr/share/icons
+
+# disable selinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+# overlayfs-tools
+# sshfs
+# dmraid
+# lsb_release OK
+
+
