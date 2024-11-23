09c9ff31e9a6f73245bbcdecdd65f067d41422d6
Piero Proietti <piero.proietti@gmail.com>
2024-11-23 08:30:33 -0600

    need pause

--- src/classes/ovary.ts
+++ src/classes/ovary.ts
@@ -1053,7 +1053,7 @@ export default class Ovary {
     } else if (this.familyId === 'openmamba') {
       kp += `root=live:CDLABEL=${this.volid} rd.live.image rd.live.dir=/live rd.live.squashimg=filesystem.squashfs selinux=0`
     } else if (this.familyId === 'aldos') {
-      kp += `root=live:CDLABEL=${this.volid} rd.live.image rd.live.dir=/live rd.live.squashimg=filesystem.squashfs selinux=0` //  rd.shell rd.debug  log_buf_len=1M
+      kp += `root=live:CDLABEL=${this.volid} rd.live.image rd.live.dir=/live rd.live.squashimg=filesystem.squashfs selinux=0`
     } else if (this.familyId === 'opensuse') {
       kp += `root=live:CDLABEL=${this.volid} rd.live.image rd.live.dir=/live rd.live.squashimg=filesystem.squashfs apparmor=0`
     } else if (this.familyId === 'voidlinux') {
