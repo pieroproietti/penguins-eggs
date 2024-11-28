5f252664f18f2cf397d44802faca664c18d65a12
Piero Proietti <piero.proietti@gmail.com>
2024-11-28 16:47:58 +0100

    export packages and update

--- src/commands/export/pkg.ts
+++ src/commands/export/pkg.ts
@@ -64,44 +64,111 @@ export default class ExportPkg extends Command {
     await this.Tu.loadSettings()
 
     let distro = new Distro()
-    if (distro.familyId === "debian") {
-      Utils.warning("debian packages")
-      await this.debs()
-    } else if (distro.familyId === "archlinux") {
-      if (distro.distroId === "ManjaroLinux" || distro.distroId === "BigLinux") {
-        Utils.warning("manjaro packages")
-        await this.manjaro()
-      } else {
-        Utils.warning("arch packages")
-        await this.aur()
-      }
-    } else if (distro.familyId === "alpine") {
-      Utils.warning("alpine packages")
-      if (Utils.isRoot()) {
-        await this.alpine()
-      } else {
-        Utils.useRoot(this.id)
-      }
-    }
-  }
-
-  /**
-   * alpine
-   */
-  private async alpine() {
-    let arch = 'x86_64'
-    if (process.arch === 'ia32') {
-      arch = 'i386'
-    }
-    const localPath = `/home/${this.user}/packages/alpine/${arch}`
-    const remotePath = `${this.Tu.config.remotePathPackages}/alpine/`
-    const filter = `penguins-eggs*10.?.*-r*.apk`
+    const familyId = distro.familyId
+    const distroId = distro.distroId
     const remoteMountpoint = `/tmp/eggs-${(Math.random() + 1).toString(36).slice(7)}`
+
+    let localPath = ''
+    let remotePath = ''
+    let filter = ''
+
+    /**
+     * aldos
+     */
+    if (familyId === 'aldos') {
+      Utils.warning("aldos rpm")
+      process.exit()
+
+      /**
+       * alpine
+       */
+    } else if (familyId === 'alpine') {
+      Utils.warning("alpine apk")
+      let arch = 'x86_64'
+      if (process.arch === 'ia32') {
+        arch = 'i386'
+      }
+      localPath = `/home/${this.user}/packages/alpine/${arch}`
+      remotePath = `${this.Tu.config.remotePathPackages}/alpine/`
+      filter = `penguins-eggs*10.?.*-r*.apk`
+
+    } else if (familyId === 'archLinux') {
+
+      /**
+       * Arch/Manjaro 
+       */
+      if (distroId === "ManjaroLinux" || distroId === "BigLinux") {
+
+        /**
+         * Manjaro
+         */
+        Utils.warning("manjaro PKGBUILD")
+        localPath = `/home/${this.user}/penguins-eggs-pkgbuilds/manjaro/penguins-eggs`
+        remotePath = this.Tu.config.remotePathPackages + "/manjaro"
+        filter = `penguins-eggs-10.?.*-?-any.pkg.tar.*`
+
+        /**
+         * Arch
+         */
+      } else {
+        Utils.warning("aur PKGBUILD")
+        localPath = `/home/${this.user}/penguins-eggs-pkgbuilds/aur/penguins-eggs`
+        remotePath = this.Tu.config.remotePathPackages + "/aur"
+        filter = `penguins-eggs-10.?.*-?-any.pkg.tar.zst`
+      }
+
+      /**
+       * Debian
+       */
+      } else if (familyId === "debian") {
+      Utils.warning("debian deb")
+      localPath = `/home/${this.user}/penguins-eggs/perrisbrewery/workdir`
+      remotePath = this.Tu.config.remotePathPackages + "/debs"
+      let arch = Utils.uefiArch()
+      if (this.all) {
+        arch = '*'
+      }
+      filter = `penguins-eggs_10.?.*-?_${arch}.deb`
+
+      /**
+       * fedora
+       */
+    } else if (familyId === 'fedora') {
+      Utils.warning("fedora rpm packages")
+      localPath = `/home/${this.user}/rpmbuild/RPMS/x86_64`
+      remotePath = this.Tu.config.remotePathPackages + "/fedora"
+      filter = `penguins-eggs-10.?.*-?-*.rpm`
+
+      /**
+       * openmamba
+       */
+    } else if (familyId === 'openmamba') {
+      Utils.warning("openmamba rpm packages")
+      localPath = `/usr/src/RPM/RPMS/x86_64`
+      remotePath = this.Tu.config.remotePathPackages + "/openmamba"
+      filter = `penguins-eggs-10.?.*-?-*.rpm`
+
+      /**
+       * opensuse
+       */
+    } else if (familyId === 'opensuse') {
+      Utils.warning("opensuse rpm packages")
+      process.exit()
+
+      /**
+       * voidlinux
+       */
+    } else if (familyId === 'voidlinux') {
+      Utils.warning("voidlinux packages")
+      process.exit()
+    }
+
     let cmd = `mkdir ${remoteMountpoint}\n`
     cmd += `sshfs ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePath} ${remoteMountpoint}\n`
     if (this.clean) {
       cmd += `rm -f ${remoteMountpoint}/${filter}\n`
     }
+
     cmd += `cp ${localPath}/${filter} ${remoteMountpoint}\n`
     cmd += 'sync\n'
     cmd += `umount ${remoteMountpoint}\n`
@@ -114,90 +181,5 @@ export default class ExportPkg extends Command {
     }
     await exec(cmd, this.echo)
   }
-
-  /**
-   * manjaro
-   */
-  private async manjaro() {
-    const localPathManjaro = `/home/${this.user}/penguins-eggs-pkgbuilds/manjaro/penguins-eggs`
-    const remotePathManjaro = this.Tu.config.remotePathPackages + "/manjaro"
-    const filterManjaro = `penguins-eggs-10.?.*-?-any.pkg.tar.*`
-    const remoteMountpoint = `/tmp/eggs-${(Math.random() + 1).toString(36).slice(7)}`
-    let cmd = `mkdir ${remoteMountpoint}\n`
-    cmd += `sshfs ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePathManjaro} ${remoteMountpoint}\n`
-    if (this.clean) {
-      cmd += `rm -f ${remoteMountpoint}/${filterManjaro}\n`
-    }
-
-    cmd += `cp ${localPathManjaro}/${filterManjaro} ${remoteMountpoint}\n`
-    cmd += 'sync\n'
-    cmd += `umount ${remoteMountpoint}\n`
-    cmd += `rm -rf ${remoteMountpoint}\n`
-    if (!this.verbose) {
-      if (this.clean) {
-        console.log(`remove: ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${filterManjaro}`)
-      }
-      console.log(`copy: ${localPathManjaro}/${filterManjaro} to ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePathManjaro}`)
-    }
-    await exec(cmd, this.echo)
-  }
-
-  /**
-   * AUR
-   */
-  private async aur() {
-    const localPathAur = `/home/${this.user}/penguins-eggs-pkgbuilds/aur/penguins-eggs`
-    const remotePathAur = this.Tu.config.remotePathPackages + "/aur"
-    const filterAur = `penguins-eggs-10.?.*-?-any.pkg.tar.zst`
-    const remoteMountpoint = `/tmp/eggs-${(Math.random() + 1).toString(36).slice(7)}`
-    let cmd = `mkdir ${remoteMountpoint}\n`
-    cmd += `sshfs ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePathAur} ${remoteMountpoint}\n`
-    if (this.clean) {
-      cmd += `rm -f ${remoteMountpoint}/${filterAur}\n`
-    }
-
-    cmd += `cp ${localPathAur}/${filterAur} ${remoteMountpoint}\n`
-    cmd += 'sync\n'
-    cmd += `umount ${remoteMountpoint}\n`
-    cmd += `rm -rf ${remoteMountpoint}\n`
-    if (!this.verbose) {
-      if (this.clean) {
-        console.log(`remove: ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${filterAur}`)
-      }
-      console.log(`copy: ${localPathAur}/${filterAur} to ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePathAur}`)
-    }
-    await exec(cmd, this.echo)
-  }
-
-  /**
-   * DEBS
-   */
-  private async debs() {
-    const localPathDeb = `/home/${this.user}/penguins-eggs/perrisbrewery/workdir`
-    const remotePathDeb = this.Tu.config.remotePathPackages + "/debs"
-    let arch = Utils.uefiArch()
-    if (this.all) {
-      arch = '*'
-    }
-    const filterDeb = `penguins-eggs_10.?.*-?_${arch}.deb`
-
-    const remoteMountpoint = `/tmp/eggs-${(Math.random() + 1).toString(36).slice(7)}`
-    let cmd = `mkdir ${remoteMountpoint}\n`
-    cmd += `sshfs ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePathDeb} ${remoteMountpoint}\n`
-    if (this.clean) {
-      cmd += `rm -f ${remoteMountpoint}/${filterDeb}\n`
-    }
-
-    cmd += `cp ${localPathDeb}/${filterDeb} ${remoteMountpoint}\n`
-    cmd += 'sync\n'
-    cmd += `umount ${remoteMountpoint}\n`
-    cmd += `rm -rf ${remoteMountpoint}\n`
-    if (!this.verbose) {
-      if (this.clean) {
-        console.log(`remove: ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${filterDeb}`)
-      }
-      console.log(`copy: ${localPathDeb}/${filterDeb} to ${this.Tu.config.remoteUser}@${this.Tu.config.remoteHost}:${remotePathDeb}`)
-    }
-    await exec(cmd, this.echo)
-  }
 }
+
