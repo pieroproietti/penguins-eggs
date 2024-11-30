d2b383574adfe474767ab6167fc9bd88d28bdfc0
Piero Proietti <piero.proietti@gmail.com>
2024-11-30 08:40:17 +0100

    trying to use ubiquity

--- src/classes/ovary.ts
+++ src/classes/ovary.ts
@@ -394,7 +394,9 @@ export default class Ovary {
       }
       shx.cp(path.resolve(__dirname, `../../addons/${theme}/theme/applications/install-system.desktop`), `${this.settings.work_dir.merged}/usr/share/applications/`)
     } else if (Pacman.packageIsInstalled('live-installer')) {
-      // carico la policy per live-installer
+      /**
+       * LMD£ live-installer
+       */
       const policySource = path.resolve(__dirname, '../../assets/live-installer/com.github.pieroproietti.penguins-eggs.policy')
       const policyDest = '/usr/share/polkit-1/actions/com.github.pieroproietti.penguins-eggs.policy'
       shx.cp(policySource, policyDest)
@@ -406,6 +408,22 @@ export default class Ovary {
 
       installerLink = 'penguins-live-installer.desktop'
       shx.cp(path.resolve(__dirname, '../../assets/penguins-live-installer.desktop'), `${this.settings.work_dir.merged}/usr/share/applications/`)
+    } else if (Pacman.packageIsInstalled('ubiquity')) {
+
+      /**
+       * UBUNTU ubiquity
+       */
+      const policySource = path.resolve(__dirname, '../../assets/ubiquity-installer/com.github.pieroproietti.penguins-eggs.policy')
+      const policyDest = '/usr/share/polkit-1/actions/com.github.pieroproietti.penguins-eggs.policy'
+      shx.cp(policySource, policyDest)
+      await exec(`sed -i 's/auth_admin/yes/' ${policyDest}`)
+
+      // carico in filesystem.live packages-remove
+      shx.cp(path.resolve(__dirname, '../../assets/ubiquity-installer/filesystem.packages-remove'), `${this.settings.iso_work}/live/`)
+      shx.touch(`${this.settings.iso_work}/live/filesystem.packages`)
+
+      installerLink = 'penguins-ubiquity-installer.desktop'
+      shx.cp(path.resolve(__dirname, '../../assets/penguins-ubiquity-installer.desktop'), `${this.settings.work_dir.merged}/usr/share/applications/`)
     } else {
       installerLink = 'penguins-krill.desktop'
       shx.cp(path.resolve(__dirname, '../../assets/penguins-krill.desktop'), `${this.settings.work_dir.merged}/usr/share/applications/`)
