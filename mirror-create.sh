4a9fa881cb1c71737ad8acf8e7d3ba9ffbd211a3
Piero Proietti <piero.proietti@gmail.com>
2024-12-19 08:54:38 +0100

    uffa

--- mirror-create.sh
+++ mirror-create.sh
@@ -1,1 +1,9 @@
-sudo mdadm --create --verbose /dev/md0 --level=1 --raid-devices=2 /dev/sda /dev/sdb
+
+InstallDevice=/dev/sda
+parted --script ${installDevice} mklabel msdos
+parted --script --align optimal ${installDevice} mkpart primary linux-swap         1MiB    ${swapSize + 1}MiB
+parted --script --align optimal ${installDevice} mkpart primary ext4 ${swapSize + 1}MiB                100%
+parted ${installDevice} set 1 boot on
+parted ${installDevice} set 1 esp on
+
+mdadm --create --verbose /dev/md0 --level=1 --raid-devices=2 /dev/sda /dev/sdb
