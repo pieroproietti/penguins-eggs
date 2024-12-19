2fadf852ee55b4cf67073906ceb65edc07075c2a
Piero Proietti <piero.proietti@gmail.com>
2024-12-19 09:38:40 +0100

    test

--- mirror-create.sh
+++ mirror-create.sh
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
 
-InstallDevice=/dev/sda
+disk0=/dev/sda
+disk1=/dev/sdb
+
+# Creazione delle partizioni
 parted --script ${installDevice} mklabel msdos
-parted --script --align optimal ${installDevice} mkpart primary linux-swap         1MiB    ${swapSize + 1}MiB
-parted --script --align optimal ${installDevice} mkpart primary ext4 ${swapSize + 1}MiB                100%
+parted --script --align optimal ${installDevice} mkpart primary biosgrub   1MiB  510 MiB
+parted --script --align optimal ${installDevice} mkpart primary ext4       1MiB     100%
 parted ${installDevice} set 1 boot on
 parted ${installDevice} set 1 esp on
 
-mdadm --create --verbose /dev/md0 --level=1 --raid-devices=2 /dev/sda /dev/sdb
+mdadm --create --verbose /dev/md0 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
+mdadm --create --verbose /dev/md1 --level=1 --raid-devices=2 /dev/sda2 /dev/sdb2
