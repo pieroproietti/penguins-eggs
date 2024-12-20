03cfd1af73dad3fe66bcd966400ce8bbc45a1e24
Marco Mancino <mancinomarko@hotmail.it>
2024-12-20 10:57:39 +0100

    addes script to completly remove the created raid device

--- mirror-destroy.sh
+++ mirror-destroy.sh
@@ -1,1 +1,8 @@
+# Stop the RAID Device
 sudo mdadm --stop /dev/md*
+
+# Remove the RAID Device
+sudo mdadm --remove /dev/md0
+
+# Remove RAID superblocks
+sudo mdadm --zero-superblock /dev/sda /dev/sdb
