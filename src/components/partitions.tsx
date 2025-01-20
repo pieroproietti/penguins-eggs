8cff60ca34b4609b6ad25fd32847da97c295d247
Marco Mancino <mancinomarko@hotmail.it>
2025-01-20 17:52:28 +0100

    fix: error in case settings file is missing. check file first

--- src/components/partitions.tsx
+++ src/components/partitions.tsx
@@ -28,14 +28,26 @@ export default function Partitions({ installationDevice, installationMode, files
     let productName = ''
     let version = ''
     let configRoot = '/etc/penguins-eggs.d/krill/'
-    if (fs.existsSync('/etc/calamares/settings.conf')) {
-        configRoot = '/etc/calamares/'
+    let calamaresRoot = '/etc/calamares/'
+    let settingFileName = configRoot + 'settings.conf'
+    let brandingFile = ''
+
+    if (fs.existsSync(calamaresRoot + settingFileName)) {
+        configRoot = calamaresRoot
     }
-    const settings = yaml.load(fs.readFileSync(configRoot + 'settings.conf', 'utf-8')) as unknown as ISettings
-    const branding = settings.branding
-    const calamares = yaml.load(fs.readFileSync(configRoot + 'branding/' + branding + '/branding.desc', 'utf-8')) as unknown as IBranding
-    productName = calamares.strings.productName
-    version = calamares.strings.version
+    
+    if (fs.existsSync(configRoot + settingFileName)) {
+        const settings = yaml.load(fs.readFileSync(configRoot + 'settings.conf', 'utf-8')) as unknown as ISettings
+        const branding = settings.branding
+
+        brandingFile = configRoot + 'branding/' + branding + '/branding.desc'
+        if (fs.existsSync(brandingFile)) {
+            const calamares = yaml.load(fs.readFileSync(brandingFile, 'utf-8')) as unknown as IBranding
+
+            productName = calamares.strings.productName
+            version = calamares.strings.version
+        }
+    }
 
     /**
     * totale width=75
