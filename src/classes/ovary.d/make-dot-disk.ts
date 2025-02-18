500249ea2b5ba4209952baa25ec921cc40049ed4
Piero Proietti <piero.proietti@gmail.com>
2025-02-18 08:37:29 +0100

    label on info

--- src/classes/ovary.d/make-dot-disk.ts
+++ src/classes/ovary.d/make-dot-disk.ts
@@ -39,8 +39,9 @@ export function makeDotDisk(this: Ovary, info = '', mksquashfs = '', mkisofs = '
     }
 
     shx.mkdir('-p', dotDisk)
-    let text = `# Created at: ${Utils.formatDate(new Date())}\n`
-    text += `# penguins_eggs v. ${Utils.getPackageVersion()}\n`
+    // let text = `# Created at: ${Utils.formatDate(new Date())}\n`
+    // text += `# penguins_eggs v. ${Utils.getPackageVersion()}\n`
+    let text = this.volid // We need for boot from ISO
 
     // .disk/info
     fs.writeFileSync(dotDisk + '/info', text, 'utf-8')
