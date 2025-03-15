8a8eeacb92ca37714d157f933950a32bdf2e0137
gnuhub <gnuhub@gmail.com>
2025-03-15 06:20:17 -0400

    Ci (#479)
    * add ci
    
    * using node18
    
    * pnpm install and pnpm deb
    
    * npm install -g pnpm@latest-10
    
    * just fix it
    
    * pnpm deb --all

---
+++ 3.ci.worker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+npm install -g pnpm@latest-10
+pnpm install
+pnpm deb --all
+
+cd $CMD_PATH
+which podman 
+podman --version
+df -h
+podman build \
+--no-cache \
+--pull=always \
+--build-arg GITHUB_REF_NAME=$GITHUB_REF_NAME \
+--build-arg GITHUB_REPOSITORY=$GITHUB_REPOSITORY \
+--build-arg GITHUB_RUN_NUMBER=$GITHUB_RUN_NUMBER \
+--tag ${GITHUB_REF_NAME}-${GITHUB_RUN_NUMBER} \
+--tag ${GITHUB_REF_NAME}-latest .
+
+podman images
+df -h
+date
