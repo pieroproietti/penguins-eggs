2c346f4693e7d7b8d8b0ccd5a77dc76822190112
gnuhub <gnuhub@gmail.com>
2025-03-18 06:38:20 +0800

    test ubuntu 22.04

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -15,6 +15,6 @@ cd $CMD_PATH
 which podman 
 podman --version
 df -h
-podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev ubuntu:24.04 /ci/install.sh
+podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev ubuntu:24.04 /ci/ubuntu-24.04-install.sh
 df -h
 date
