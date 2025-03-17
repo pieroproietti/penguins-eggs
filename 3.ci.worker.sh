ad33403ce3522dc19959efe192b30cc30d68a7fc
gnuhub <gnuhub@gmail.com>
2025-03-17 03:55:50 -0400

    clean (#489)

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -15,6 +15,6 @@ cd $CMD_PATH
 which podman 
 podman --version
 df -h
-podman run --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev debian:12.9 /ci/install.sh $GITHUB_REPOSITORY $GITHUB_REF_NAME $GITHUB_RUN_NUMBER
+podman run --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev debian:12.9 /ci/install.sh
 df -h
 date
