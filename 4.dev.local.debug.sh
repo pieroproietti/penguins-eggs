e6f253dc5cfd60298b14dc193d7a364ea5e9a807
gnuhub <gnuhub@gmail.com>
2025-03-18 06:59:24 +0800

    just fix it

--- 4.dev.local.debug.sh
+++ 4.dev.local.debug.sh
@@ -19,7 +19,7 @@ which podman
 podman --version
 df -h
 podman run --hostname minimal --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -it -v $PWD/mychroot/ci:/ci -v /dev:/dev debian:12.9 bash
-# in docker run
+# # when you are in the container, just run as the flowwing:
 # cd /ci/
 # ls -al
 # ./debian-12.9-install.sh
