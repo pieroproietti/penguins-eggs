472a1abf4b8413747e1fe97ce777f6ad8012e297
Piero Proietti <piero.proietti@gmail.com>
2025-03-17 14:53:38 +0100

    uuff...

--- 4.dev.local.debug.sh
+++ 4.dev.local.debug.sh
@@ -9,7 +9,7 @@ export CMD_PATH=$(cd `dirname $0`; pwd)
 export PROJECT_NAME="${CMD_PATH##*/}" 
 echo $PROJECT_NAME
 cd $CMD_PATH
-npm install -g pnpm@latest-10
+sudo npm install -g pnpm@latest-10
 pnpm install
 pnpm deb --release 13
 mv ./perrisbrewery/workdir/penguins-eggs_*_amd64.deb ./mychroot/ci/
