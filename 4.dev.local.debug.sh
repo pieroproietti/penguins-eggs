6d93ee17933b8245be3a5c939cc05d81f865c192
gnuhub <gnuhub@gmail.com>
2025-03-17 04:23:22 -0400

    for debug (#490)
    * clean
    
    * add 4.dev.local.debug.sh
    
    * just fix it

---
+++ 4.dev.local.debug.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+npm install -g pnpm@latest-10
+pnpm install
+pnpm deb # -all 
+
+mv ./perrisbrewery/workdir/penguins-eggs_*_amd64.deb ./mychroot/ci/
+
+cd $CMD_PATH
+which podman 
+podman --version
+df -h
+podman run --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev debian:12.9 bash
+# in docker run
+# cd /ci/
+# ls -al
+# ./install.sh
+# #do something
+
+# #end it
+# exit
+df -h
+date
