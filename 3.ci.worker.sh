5099f11b7d9296725af9edc5a27f6e47305e282f
gnuhub <gnuhub@gmail.com>
2025-03-18 16:10:05 -0400

    ADD TEST 1 AND TEST2 (#495)
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -9,11 +9,40 @@ npm install -g pnpm@latest-10
 pnpm install
 pnpm deb # -all 
 mv ./perrisbrewery/workdir/penguins-eggs_*_amd64.deb ./mychroot/ci/
+#################################################################################################################
+## TODO 0
+##  check the server of ci
+## the server of ci is ubuntu 24.04
+## the ci server is an azure vm on azure 
 
-##########################
+sudo cat /etc/issue
+ls -al /etc/
+
+
+#################################################################################################################
+## TODO 1
+## TEST 1
+## using eggs to remaster ubuntu server ubuntu-24.04
+## it failed now
+## Unrecognised xattr prefix system.posix_acl_default
+## see more or dowload the actions log
+
+sudo apt install -y ./perrisbrewery/workdir/penguins-eggs_*_amd64.deb
+
+sudo eggs dad -d
+sudo eggs produce --clone -n --verbose
+
+
+######################################################################################################################
 # pnpm tarballs 
 # mv ./dist/eggs*.tar.gz ./mychroot/ci/
 
+######################################################################################################################
+## TODO 2
+## TEST 2
+## using contaner image ubuntu ubuntu:24.04 on ci hosted server ubuntu-24.04 
+## install same kernal with the host of the ci server
+## it failed now
 cd $CMD_PATH
 which podman 
 podman --version
