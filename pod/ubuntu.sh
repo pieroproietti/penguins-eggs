9adcd1451b3c1ddb62de966d10cb799558fdf2e6
Piero Proietti <piero.proietti@gmail.com>
2025-03-18 10:44:48 +0100

    added pod/ubuntu.sh

---
+++ pod/ubuntu.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -x
+
+# remove previous images
+podman rmi $(podman images --quiet) -f
+
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}" 
+echo $PROJECT_NAME
+cd $CMD_PATH
+sudo npm install -g pnpm@latest-10
+pnpm install
+pnpm deb --release 13
+mv ./perrisbrewery/workdir/penguins-eggs_*_amd64.deb ./mychroot/ci/
+
+cd $CMD_PATH
+which podman 
+podman --version
+df -h
+podman run --hostname minimal --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -it -v $PWD/mychroot/ci:/ci -v /dev:/dev ubuntu:24.04 bash
+# # when you are in the container, just run as the following:
+# cd /ci/
+# ls -al
+# ./ubuntu-24.04-install.sh
+# #do something
+
+# #end it
+# exit
+df -h
+date
