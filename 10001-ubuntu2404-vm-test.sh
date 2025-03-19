11d4604bea1ae1dd8ce72985419f4e26bf4b0d95
gnuhub <gnuhub@gmail.com>
2025-03-19 02:46:14 -0400

    -no-xattrs to fix test1 remaster ubuntu server 24.04 Unrecognised xattr prefix system.posix_acl_default (#500)
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * try to ignore container for Unrecognised xattr prefix system.posix_acl_default
    
    * Unrecognised xattr prefix system.posix_acl_default
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * debug -xattrs
    
    * debug -info
    
    * debug
    
    * mksquashfs debug
    
    * strace -e trace=read mksquashfs -xattrs
    
    * -no-xattrs
    
    * -no-xattrs
    
    * fix
    
    * fix
    
    * mksquashfs -no-xattrs
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * Update 3.ci.worker.sh
    
    * ci fix
    
    * ci fix
    
    * ci fix
    
    * just fix it
    
    * add test skel
    
    * add test skel

--- 10001-ubuntu2404-vm-test.sh
+++ 10001-ubuntu2404-vm-test.sh
@@ -18,7 +18,16 @@ mv ./perrisbrewery/workdir/penguins-eggs_*_amd64.deb ./mychroot/ci/
 
 sudo cat /etc/issue
 ls -al /etc/
-
+# fix podman pull error : Get "https://registry-1.docker.io/v2/": dial tcp: lookup registry-1.docker.io on 127.0.0.53:53: read udp 127.0.0.1:53545->127.0.0.53:53: read: connection refused
+sudo cat /etc/resolv.conf
+sudo apt purge resolvconf -y
+sudo rm /etc/resolv.conf
+sudo touch /etc/resolv.conf
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+echo "nameserver 127.0.0.53" | sudo tee -a /etc/resolv.conf
+echo "options edns0 trust-ad" | sudo tee -a /etc/resolv.conf
+sudo cat /etc/resolv.conf
 
 #################################################################################################################
 ## TODO 1
@@ -29,25 +38,9 @@ ls -al /etc/
 ## see more or dowload the actions log
 cd $CMD_PATH
 sudo apt install -y ./mychroot/ci/penguins-eggs_*_amd64.deb
-
+mksquashfs -version
 sudo eggs dad -d
 sudo eggs produce --clone -n --verbose
 
-
-######################################################################################################################
-# pnpm tarballs 
-# mv ./dist/eggs*.tar.gz ./mychroot/ci/
-
-######################################################################################################################
-## TODO 2
-## TEST 2
-## using contaner image ubuntu ubuntu:24.04 on ci hosted server ubuntu-24.04 
-## install same kernal with the host of the ci server
-## it failed now
-cd $CMD_PATH
-which podman 
-podman --version
-df -h
-podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev ubuntu:24.04 /ci/ubuntu-24.04-install.sh
 df -h
 date
---
+++ mychroot/ci/10005-alpine3-container-test-install.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
+
+####################################################################################################################################
+# 1 check
+cd $CMD_PATH
+env
+pwd
+whoami
+
+####################################################################################################################################
+# 2 hostname
+echo -e "$(hostname)\n" > /etc/hostname
+
+# 2.1 hosts
+echo -e "\
+127.0.0.1  localhost
+::1        localhost ip6-localhost ip6-loopback
+ff02::1    ip6-allnodes
+ff02::2    ip6-allrouters
+# This host address
+127.0.1.1   $(hostname)" > /etc/hosts
+
+####################################################################################################################################
+# penguins-eggs mininal requisites for debian bookworm
+
+cd $CMD_PATH
+apt update -y
+apt upgrade -y
+
+# packages to be added for a minimum standard installation
+apt install \
+    apt-listchanges \
+    apt-utils \
+    bash-completion \
+    cron \
+    cron-daemon-common \
+    debconf-i18n \
+    debian-faq \
+    dialog \
+    dictionaries-common \
+    discover \
+    discover-data \
+    distro-info-data \
+    dnsutils \
+    doc-debian \
+    eject \
+    emacsen-common \
+    fdisk \
+    file \
+    git \
+    grub-efi-amd64 \
+    iamerican \
+    ibritish \
+    ienglish-common \
+    ifupdown \
+    inetutils-telnet \
+    init \
+    installation-report \
+    iproute2 \
+    iputils-ping \
+    isc-dhcp-client \
+    isc-dhcp-common \
+    iso-codes \
+    ispell \
+    iucode-tool \
+    laptop-detect \
+    less \
+    libdiscover2 \
+    liblockfile-bin \
+    libmagic-mgc \
+    libmagic1 \
+    libnewt0.52 \
+    libnftables1 \
+    libnftnl11 \
+    libpci3 \
+    libslang2 \
+    libtext-charwidth-perl \
+    libtext-iconv-perl \
+    libtext-wrapi18n-perl \
+    liburing2 \
+    libusb-1.0-0 \
+    locales \
+    logrotate \
+    lsb-release \
+    lsof \
+    mailcap \
+    man \
+    man-db \
+    manpages \
+    mime-support \
+    nano \
+    ncurses-term \
+    net-tools \
+    netcat-traditional \
+    nftables \
+    pci.ids \
+    pciutils \
+    procps \
+    python-apt-common \
+    python3-apt \
+    python3-certifi \
+    python3-chardet \
+    python3-charset-normalizer \
+    python3-debconf \
+    python3-debian \
+    python3-debianbts \
+    python3-httplib2 \
+    python3-idna \
+    python3-pkg-resources \
+    python3-pycurl \
+    python3-pyparsing \
+    python3-pysimplesoap \
+    python3-reportbug \
+    python3-requests \
+    python3-six \
+    python3-urllib3 \
+    qemu-guest-agent \
+    reportbug \
+    sensible-utils \
+    sudo \
+    systemd-sysv \
+    task-english \
+    tasksel \
+    tasksel-data \
+    traceroute \
+    tzdata \
+    ucf \
+    usbutils \
+    util-linux-locales \
+    vim \
+    vim-tiny \
+    wamerican \
+    wget \
+    whiptail -y
+
+
+# We must install the same version of the host
+apt install linux-image-$(uname -r) -y
+
+# init /usr/share/applications
+dpkg -S /usr/share/applications
+
+apt install python3 -y
+ls -al /usr/share/applications
+
+# fix linuxefi.mod
+apt-file update
+apt-file search linuxefi.mod
+apt install grub-efi-amd64-bin -y
+
+# starting with eggs
+cd /ci/
+ls -al
+apt install -y ./*.deb
+
+eggs dad -d
+eggs produce --pendrive -n # --verbose
+
+# clean debs on /ci
+rm /ci/*.deb
+
+date
+
+echo "# enable bash_completion, running:"
+echo "source /etc/bash_completion"
