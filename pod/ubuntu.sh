187192c370d4571ad2dfa76940c7e2bf05d8d566
Piero Proietti <piero.proietti@gmail.com>
2025-03-22 17:08:21 +0100

    correzione ubuntu

--- pod/ubuntu.sh
+++ pod/ubuntu.sh
@@ -15,44 +15,15 @@ TARBALLS="eggs-v10.0.60-*-linux-x64.tar.gz "
 rm ../mychroot/ci/$TARBALLS
 cp ../dist/$TARBALLS ../mychroot/ci/
 
-# se ubuntu monta /var/local/yolk
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [[ "$ID" == "debian" ]]; then
-        podman run --hostname minimal \
-                    --privileged \
-                    --ulimit nofile=32000:32000 \
-                    --pull=always \
-                    -it \
-                    -v $PWD/mychroot/ci:/ci \
-                    -v /dev:/dev \
-                    ubuntu:24.04 \
-                    bash
-    elif [[ "$ID" == "ubuntu" ]]; then
-        podman run --hostname minimal \
-                    --privileged \
-                    --ulimit nofile=32000:32000 \
-                    --pull=always \
-                    -it \
-                    -v $PWD/mychroot/ci:/ci \
-                    -v /dev:/dev \
-                    -v /var/local/yolk:/var/local/yolk \
-                    ubuntu:24.04 \
-                    bash
-    elif [[ "$ID" == "arch" ]]; then
-        podman run --hostname minimal \
-                    --privileged \
-                    --ulimit nofile=32000:32000 \
-                    --pull=always \
-                    -it \
-                    -v $PWD/mychroot/ci:/ci \
-                    -v /dev:/dev \
-                    ubuntu:24.04 \
-                    bash
-    else
-        echo "Sistema diverso: $ID"
-    fi
-fi
+podman run --hostname minimal \
+            --privileged \
+            --ulimit nofile=32000:32000 \
+            --pull=always \
+            -it \
+            -v $PWD/mychroot/ci:/ci \
+            -v /dev:/dev \
+            ubuntu:24.04 \
+            bash
 
 cd $CMD_PATH
 which podman 
