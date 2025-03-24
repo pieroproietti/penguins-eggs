da747f1e4342ef490a5489a7dbfe5dcdb0aba9bd
gnuhub <gnuhub@gmail.com>
2025-03-24 09:17:18 +0800

    just fix it

--- 10006-archlinux-container-test.sh
+++ 10006-archlinux-container-test.sh
@@ -43,6 +43,6 @@ cd $CMD_PATH
 which podman 
 podman --version
 df -h
-podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev archlinux:latest /ci/10006-archlinux-container-test.sh-install.sh
+podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev archlinux:latest /ci/10006-archlinux-container-test-install.sh
 df -h
 date
