6eb406b88b4ea0f9fdc3b9d0c450d7e3628cf13d
Piero Proietti <piero.proietti@gmail.com>
2025-03-25 12:09:31 +0100

    seem working again...

---
+++ mychroot/ci/penguins-eggs-tarballs-replace.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+
+# replace tarballs in ci
+PENGUINS_EGGS_TARBALLS=penguins-eggs_10.0.60-*-linux-x64.tar.gz
+rm $CMD_PATH/../ci/$PENGUINS_EGGS_TARBALLS
+cp $CMD_PATH/../dist/$PENGUINS_EGGS_TARBALLS ./ci/
