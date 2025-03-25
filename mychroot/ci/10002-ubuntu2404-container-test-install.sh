9cbdca12d51fa2ebae5bf0fdcc02f5269e2bb6c2
Piero Proietti <piero.proietti@gmail.com>
2025-03-25 12:18:57 +0100

    test on github

--- mychroot/ci/10002-ubuntu2404-container-test-install.sh
+++ mychroot/ci/10002-ubuntu2404-container-test-install.sh
@@ -35,7 +35,7 @@ cd $CMD_PATH
 apt update -y
 apt upgrade -y
 
-# We must install the same version of the host
+# We must install kernel
 apt install linux-image-generic -y
 
 # packages to be added for a minimum standard installation
@@ -60,66 +60,8 @@ systemctl set-default multi-user.target
 cd /ci/
 ls -al
 
-if ls ./eggs-v10.0.60-*-linux-x64.tar.gz 1> /dev/null 2>&1; then
-    echo "penguins-eggs tarballs already present."
-else
-    echo "building penguins-eggs tarballs..."
-    source ./build-penguins-eggs-tarballs.sh
-fi
-
-# install tarball
-EGGS_HOME="/opt/penguins-eggs/"
-EGGS_PACKAGE=eggs-v10.0.60-*-linux-x64.tar.gz
-
-# Rimozione di /opt/penguins-eggs se esiste
-if [ -d "$EGGS_HOME" ]; then
-    rm -rf "$EGGS_HOME"
-fi
-
-# extract package
-tar -xf $EGGS_PACKAGE
-if [ $? -ne 0 ]; then
-    echo "Error: not possible extract $EGGS_PACKAGE."
-    exit 1
-fi
-
-mv eggs penguins-eggs
-$SUDO mv penguins-eggs /opt/
-
-# create link themes  grub/isolinux
-ln -sf "${EGGS_HOME}addons/eggs/theme/livecd/isolinux.main.full.cfg" "${EGGS_HOME}addons/eggs/theme/livecd/isolinux.main.cfg"
-ln -sf "${EGGS_HOME}addons/eggs/theme/livecd/grub.main.full.cfg" "${EGGS_HOME}addons/eggs/theme/livecd/grub.main.cfg"
-
-# Bash completions
-if [ -d "/usr/share/bash-completion/completions/" ]; then
-    rm -f /usr/share/bash-completion/completions/eggs.bash
-    ln -sf "${EGGS_HOME}scripts/eggs.bash" /usr/share/bash-completion/completions/eggs.bash
-fi
-
-# Zsh completions
-if [ -d "/usr/share/zsh/functions/Completion/Zsh/" ]; then
-    rm -f /usr/share/zsh/functions/Completion/Zsh/_eggs
-    ln -sf "${EGGS_HOME}scripts/_eggs" /usr/share/zsh/functions/Completion/Zsh/
-fi
-
-# Icons
-if [ -d "/usr/share/icons/" ]; then
-    rm -f /usr/share/icons/eggs.png
-    ln -sf "${EGGS_HOME}assets/eggs.png" /usr/share/icons/eggs.png
-fi
-
-# Manual
-if [ -d "/usr/share/man/man1" ]; then
-    rm -f /usr/share/man/man1/eggs.1.gz
-    ln -sf "${EGGS_HOME}manpages/doc/man/eggs.1.gz" /usr/share/man/man1/eggs.1.gz
-fi
-
-# Link binary
-rm -f /usr/bin/eggs
-ln -sf "${EGGS_HOME}bin/eggs" /usr/bin/eggs
-
-# eggs was installed!
-
+# installing eggs
+source ./penguins-eggs-tarballs-install.sh
 
 eggs dad -d
 egge tools clean -n
