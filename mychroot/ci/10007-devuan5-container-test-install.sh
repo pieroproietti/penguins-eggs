03c02321f04d6cd64345b5285b6682b8546ef54d
Piero Proietti <piero.proietti@gmail.com>
2025-03-28 09:02:02 +0100

    controllo

--- mychroot/ci/10007-devuan5-container-test-install.sh
+++ mychroot/ci/10007-devuan5-container-test-install.sh
@@ -15,7 +15,23 @@ pwd
 whoami
 
 ####################################################################################################################################
+# 2 hostname
+echo -e "$(hostname)\n" > /etc/hostname
+
+# 2.1 hosts
+echo -e "\
+127.0.0.1  localhost
+::1        localhost ip6-localhost ip6-loopback
+ff02::1    ip6-allnodes
+ff02::2    ip6-allrouters
+# This host address
+127.0.1.1   $(hostname)" > /etc/hosts
+
+####################################################################################################################################
+# penguins-eggs mininal requisites for debian bookworm
+
+cd $CMD_PATH
+
+####################################################################################################################################
 # run-on-devuan
 source ./run-on-devuan.sh
-
-
