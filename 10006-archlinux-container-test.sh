6968c54ed311dda07a685983a471972161e24c81
Piero Proietti <piero.proietti@gmail.com>
2025-03-28 09:52:19 +0100

    cleaning

--- 10006-archlinux-container-test.sh
+++ 10006-archlinux-container-test.sh
@@ -50,6 +50,6 @@ cd $CMD_PATH
 which podman 
 podman --version
 df -h
-podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev archlinux:latest /ci/10006-archlinux-container-test-install.sh
+podman run --hostname minimal-ci --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev archlinux:latest /ci/run-on-archlinux.sh
 df -h
 date
