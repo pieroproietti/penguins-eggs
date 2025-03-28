bc720166ffc3633ec976c68f7be7b4a3d199e84d
Piero Proietti <piero.proietti@gmail.com>
2025-03-28 08:53:13 +0100

    check

--- mychroot/ci/10007-devuan5-container-test-install.sh
+++ mychroot/ci/10007-devuan5-container-test-install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
-
 set -x
 export CMD_PATH=$(cd `dirname $0`; pwd)
 export PROJECT_NAME="${CMD_PATH##*/}"
@@ -16,4 +14,8 @@ env
 pwd
 whoami
 
+####################################################################################################################################
+# run-on-devuan
 source ./run-on-devuan.sh
+
+
