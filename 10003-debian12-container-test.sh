c7ac1588e2f730b8f360187c1067f038d0230345
Piero Proietti <piero.proietti@gmail.com>
2025-03-28 17:06:41 +0100

    semplificando...

--- 10003-debian12-container-test.sh
+++ 10003-debian12-container-test.sh
@@ -7,6 +7,16 @@ cd $CMD_PATH
 which podman 
 podman --version
 df -h
-podman run --hostname minimal --privileged --cap-add all --ulimit nofile=32000:32000 --pull=always -v $PWD/mychroot/ci:/ci -v /dev:/dev debian:12.9 /ci/run-on-debian.sh
+
+podman run --hostname minimal \
+    --privileged 
+    --cap-add all \
+    --ulimit nofile=32000:32000 \
+    --pull=always \
+    -v $PWD/mychroot/ci:/ci \
+    -v /dev:/dev \
+    debian:12.9 \
+    /ci/run-on-debian.sh
+
 df -h
 date
