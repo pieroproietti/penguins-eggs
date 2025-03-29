f59a836b6e6c289c910de4ab8cd2edbe8210f0a6
gnuhub <gnuhub@gmail.com>
2025-03-30 05:50:41 +0800

    20014-popos2204-container-test (#525)
    * add 20011-manjarolinux-container-test
    
    * just fix it
    
    * just fix it
    
    * add 20012-linuxmint-xfce-minimal-container-test
    
    * add 20013-ultramarine-linux-container-test
    
    * 20014-popos2204-container-test

---
+++ 20014-popos2204-container-test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+source ./10000-ubuntu-ci-server.sh
+
+cd $CMD_PATH
+which podman 
+podman --version
+df -h
+
+podman run \
+        --hostname minimal \
+        --privileged  \
+        --cap-add all \
+        --ulimit nofile=32000:32000 \
+        --pull=always \
+        -v $PWD/mychroot/ci:/ci \
+        -v /dev:/dev \
+        nycticoracs/pop_os:latest \
+        /ci/run-on-popos.sh
+
+df -h
+ls -al $PWD/mychroot/ci/iso/
+# upload $PWD/mychroot/ci/iso/ to server or github
+date
