9e29d74535799bfb28bf336e8f78c0b0470e8f14
Piero Proietti <piero.proietti@gmail.com>
2025-03-29 18:48:25 +0100

    preparing for build packages debs

---
+++ mychroot/ci/run-build-packages-debs.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
+
+####################################################################################################################################
+# 1 check
+cd $CMD_PATH
+env
+pwd
+whoami
+
+####################################################################################################################################
+# 2 hostname
+echo -e "$(hostname)\n" > /etc/hostname
+
+# 2.1 hosts
+echo -e "\
+127.0.0.1  localhost
+::1        localhost ip6-localhost ip6-loopback
+ff02::1    ip6-allnodes
+ff02::2    ip6-allrouters
+# This host address
+127.0.1.1   $(hostname)" > /etc/hosts
+
+####################################################################################################################################
+# penguins-eggs mininal requisites for debian bookworm
+
+cd $CMD_PATH
+apt update -y
+apt upgrade -y
+
+
+# save iso to the host server
+mv /home/eggs/.mnt/*.iso /ci/iso/
+ls -al /ci/iso/
---
+++ pod/build-packages-debs.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -x
+
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}" 
+echo $PROJECT_NAME
+cd $CMD_PATH
+
+source ci/penguins-eggs-tarballs-replace.sh
+
+# define YOLK if Debian
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [[ "$ID" == "debian" ]]; then
+        DEST="/var/local/yolk"
+        if [ ! -d $DEST ]; then
+            sudo mkdir -p $DEST
+        fi
+        YOLK="-v $DEST:$DEST"
+    fi
+fi
+
+podman run \
+    --hostname minimal \
+    --privileged \
+    --cap-add all \
+    --ulimit nofile=32000:32000 \
+    --pull=always \
+    --rm \
+    -it \
+    -v /dev:/dev \
+    -v ../mychroot/ci:/ci \
+    $YOLK \
+    debian \
+    bash
+
+# interactive session
+
+# This will be executed at end
+cd $CMD_PATH
+which podman 
+podman --version
+df -h
+date
+
