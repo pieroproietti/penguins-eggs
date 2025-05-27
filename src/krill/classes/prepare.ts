759be072ff94adc0dba3c9684c6a0fe7a77d7892
artisan <artisan@minimal>
2025-05-27 11:29:43 +0200

    ristrutturate krill/prepare e krill/sequence

--- src/krill/classes/prepare.ts
+++ src/krill/classes/prepare.ts
@@ -1,27 +1,30 @@
 /**
- * ./src/krill/prepare.tsx
+ * Krill Installer - Simplified Refactoring
+ * ./src/krill/prepare.ts
  * penguins-eggs v.10.0.0 / ecmascript 2020
  * author: Piero Proietti
  * email: piero.proietti@gmail.com
  * license: MIT
- * https://stackoverflow.com/questions/23876782/how-do-i-split-a-typescript-class-into-multiple-files
  */
 
 import os from 'os'
-import { IKrillConfig } from '../interfaces/i_krill_config.js'
-
-import axios from 'axios'
-import shx, { echo } from 'shelljs'
 import fs from 'fs'
+import shx from 'shelljs'
+import axios from 'axios'
+
+import { IKrillConfig } from '../interfaces/i_krill_config.js'
+import { INet } from '../../interfaces/index.js'
+import { IWelcome, ILocation, IKeyboard, IPartitions, IUsers } from '../interfaces/i_krill.js'
+import { SwapChoice, InstallationMode, FsType } from './krill_enums.js'
 
 import Keyboards from '../../classes/keyboards.js'
 import Locales from '../../classes/locales.js'
 import Systemctl from '../../classes/systemctl.js'
 import Utils from '../../classes/utils.js'
-
-// libraries
 import { exec } from '../../lib/utils.js'
+import Sequence from './sequence.js'
 
+// UI Components
 import { welcome } from './prepare.d/welcome.js'
 import { location } from './prepare.d/location.js'
 import { keyboard } from './prepare.d/keyboard.js'
@@ -30,17 +33,13 @@ import { users } from './prepare.d/users.js'
 import { network } from './prepare.d/network.js'
 import { summary } from './prepare.d/summary.js'
 
-import Sequence from './sequence.js'
-import { INet } from '../../interfaces/index.js'
-import { IWelcome, ILocation, IKeyboard, IPartitions, IUsers } from '../interfaces/i_krill.js'
-import { SwapChoice, InstallationMode, FsType } from './krill_enums.js'
-
 const config_file = '/etc/penguins-eggs.d/krill.yaml' as string
 
 /**
- *
+ * Main Krill installer class - Simplified refactoring
  */
 export default class Krill {
+  // UI Components
   welcome = welcome
   location = location
   keyboard = keyboard
@@ -49,25 +48,19 @@ export default class Krill {
   network = network
   summary = summary
 
+  // Configuration
   krillConfig = {} as IKrillConfig
-
   locales = new Locales()
-
   keyboards = new Keyboards()
 
+  // Installation flags
   unattended = false
-
   nointeractive = false
-
   chroot = false
-
   halt = false
 
   /**
-   * constructor
-   * @param unattended 
-   * @param nointeractive 
-   * @param halt 
+   * Constructor
    */
   constructor(unattended = false, nointeractive = false, halt = false, chroot = false) {
     this.unattended = unattended
@@ -77,144 +70,177 @@ export default class Krill {
   }
 
   /**
-   * 
-   * @param krillConfig 
-   * @param ip 
-   * @param random 
-   * @param domain 
-   * @param suspend 
-   * @param small 
-   * @param none 
-   * @param crypted 
-   * @param pve 
-   * @param btrfs 
-   * @param testing 
-   * @param verbose 
+   * Main prepare method - Simplified and cleaner
    */
-  async prepare(krillConfig = {} as IKrillConfig, ip = false, random = false, domain = '', suspend = false, small = false, none = false, crypted = false, pve = false, btrfs = false, testing = false, verbose = false) {
+  async prepare(
+    krillConfig = {} as IKrillConfig,
+    ip = false,
+    random = false,
+    domain = '',
+    suspend = false,
+    small = false,
+    none = false,
+    crypted = false,
+    pve = false,
+    btrfs = false,
+    testing = false,
+    verbose = false
+  ) {
+    try {
+      // System validation
+      await this.checkSystemRequirements()
+      
+      // Configuration setup
+      this.krillConfig = krillConfig
+      await this.setupConfiguration()
+      
+      // Stop services
+      await this.stopSystemServices(verbose, testing)
+      
+      // Build configurations
+      const configs = await this.buildConfigurations(ip, random, suspend, small, none)
+      
+      // Interactive or unattended mode
+      const finalConfigs = this.unattended 
+        ? this.applyUnattendedOptions(configs, crypted, pve, btrfs)
+        : await this.runInteractiveMode(crypted, pve, btrfs)
+      
+      // Install
+      await this.performInstallation(finalConfigs, domain, testing, verbose)
+      
+    } catch (error) {
+      await Utils.pressKeyToExit(`${(error as Error).message}\nkrill installer refuses to continue`)
+      process.exit()
+    }
+  }
 
+  /**
+   * Check system requirements (disks, LVM)
+   */
+  private async checkSystemRequirements(): Promise<void> {
     // Check disk presence
     const drives = shx.exec('lsblk |grep disk|cut -f 1 "-d "', { silent: true }).stdout.trim().split('\n')
     if (drives[0] === '') {
-      await Utils.pressKeyToExit(`No disk to install the system in this machine.\nkrill installer refuses to continue`)
-      process.exit()
+      throw new Error('No disk to install the system in this machine.')
     }
 
-    // Check Lvm2 presence
+    // Check LVM presence
     if (await this.pvExist()) {
-      // Create removeLvmPartitions
-      let scriptName = "removeLvmPartitions"
-
-      let cmds = "#!/bin/bash\n"
-      cmds += `# remove LV (Logical Volumes)\n`
-      cmds += `vg=$(vgs --noheadings -o vg_name| awk '{$1=$1};1')\n`
-      cmds += `lvs -o lv_name --noheadings | awk '{$1=$1};1' | while read -r lv; do\n`
-      cmds += ` lvremove -y /dev/mapper/$vg-$lv\n`
-      cmds += `done\n`
-      cmds += `\n`
-      cmds += `# remove VG (Volume groups)\n`
-      cmds += `vgremove --force $(vgs --noheadings -o vg_name $vg)\n`
-      cmds += `\n`
-      cmds += `# remove PV (Phisical Volumes) \n`
-      cmds += `pv=$(pvs --noheading -o pv_name | awk '{$1=$1};1')\n`
-      cmds += `pvremove --force --force $pv\n`
-      cmds += `# wipe PV (Phisical Volumes) \n`
-      cmds += `wipefs -a $pv\n`
-      cmds += `# clean device\n`
-      cmds += `sgdisk --zap-all $pv\n`
-      cmds += `dd if=/dev/zero of=$pv bs=1M count=10\n`
-
-      fs.writeFileSync(scriptName, cmds)
-      await exec(`chmod +x ${scriptName}`)
-
-      await Utils.pressKeyToExit(`There is a lvm2 volume in the system, remove it manually before installation.\nkrill installer refuses to continue`)
-      process.exit()
+      await this.createLvmRemovalScript()
+      throw new Error('There is a lvm2 volume in the system, remove it manually before installation.')
     }
+  }
 
-    /**
-    * stop udisks2.service
-    */
-    const systemdCtl = new Systemctl(verbose)
-    if (await systemdCtl.isActive('udisks2.service') && !testing) {
-      await systemdCtl.stop('udisks2.service')
-    }
-
-    let oWelcome = {} as IWelcome
-    let oLocation = {} as ILocation
-    let oKeyboard = {} as IKeyboard
-    let oPartitions = {} as IPartitions
-    let oUsers = {} as IUsers
-    let oNetwork = {} as INet
-
-    /**
-     * load default values
-     */
+  /**
+   * Setup configuration files and auto-configure timezone
+   */
+  private async setupConfiguration(): Promise<void> {
+    // Check config file
     if (!fs.existsSync(config_file)) {
-      console.log(`cannot find configuration file ${config_file},`)
-      process.exit(1)
+      throw new Error(`Cannot find configuration file ${config_file}`)
     }
 
-    this.krillConfig = krillConfig
-
-    /**
-     * test calamares/krill configuration presence
-     */
+    // Check calamares/krill configuration
     let configRoot = '/etc/penguins-eggs.d/krill/'
     if (fs.existsSync('/etc/calamares/settings.conf')) {
       configRoot = '/etc/calamares/'
     }
     if (!fs.existsSync(configRoot + 'settings.conf')) {
-      console.log(`Cannot find calamares/krill configuration file, please create it running:`)
-      console.log(`sudo eggs calamares`)
-      process.exit(1)
+      throw new Error('Cannot find calamares/krill configuration file, please create it running: sudo eggs calamares')
     }
 
+    // Auto-configure timezone
+    await this.autoConfigureTimezone()
+  }
 
-    oWelcome = { language: this.krillConfig.language }
-    // Try to auto-configure timezone by internet
-    const url = `https://geoip.kde.org/v1/calamares`
+  /**
+   * Auto-configure timezone from internet
+   */
+  private async autoConfigureTimezone(): Promise<void> {
     try {
-      const response = await axios.get(url)
+      const response = await axios.get('https://geoip.kde.org/v1/calamares')
       if (response.statusText === 'OK') {
-        const data = JSON.stringify(response.data)
-        const obj = JSON.parse(data)
-        this.krillConfig.region = obj.time_zone.substring(0, obj.time_zone.indexOf('/'))
-        this.krillConfig.zone = obj.time_zone.substring(obj.time_zone.indexOf('/') + 1)
+        const timeZone = response.data.time_zone
+        this.krillConfig.region = timeZone.substring(0, timeZone.indexOf('/'))
+        this.krillConfig.zone = timeZone.substring(timeZone.indexOf('/') + 1)
       }
     } catch (error) {
-      console.error('error: ' + error)
+      console.error('Error auto-configuring timezone:', error)
     }
+  }
 
-    oLocation = {
+  /**
+   * Stop system services
+   */
+  private async stopSystemServices(verbose: boolean, testing: boolean): Promise<void> {
+    const systemdCtl = new Systemctl(verbose)
+    if (await systemdCtl.isActive('udisks2.service') && !testing) {
+      await systemdCtl.stop('udisks2.service')
+    }
+  }
+
+  /**
+   * Build default configurations
+   */
+  private async buildConfigurations(ip: boolean, random: boolean, suspend: boolean, small: boolean, none: boolean) {
+    // Generate hostname
+    const hostname = this.generateHostname(ip, random)
+
+    // Build configuration objects
+    const oWelcome: IWelcome = { language: this.krillConfig.language }
+    
+    const oLocation: ILocation = {
       language: this.krillConfig.language,
       region: this.krillConfig.region,
       zone: this.krillConfig.zone
     }
 
-    oKeyboard = {
+    const oKeyboard: IKeyboard = {
       keyboardModel: this.krillConfig.keyboardModel,
       keyboardLayout: this.krillConfig.keyboardLayout,
       keyboardVariant: this.krillConfig.keyboardVariant,
       keyboardOption: this.krillConfig.keyboardOption
     }
 
+    let userSwapChoice = this.krillConfig.userSwapChoice
+    if (suspend) userSwapChoice = SwapChoice.Suspend
+    else if (small) userSwapChoice = SwapChoice.Small
+    else if (none) userSwapChoice = SwapChoice.None
 
-    oPartitions = {
+    const oPartitions: IPartitions = {
       installationDevice: this.krillConfig.installationDevice,
       installationMode: this.krillConfig.installationMode,
       filesystemType: this.krillConfig.filesystemType,
-      userSwapChoice: this.krillConfig.userSwapChoice,
+      userSwapChoice,
       replacedPartition: this.krillConfig.replacedPartition
     }
 
-    if (suspend) {
-      oPartitions.userSwapChoice = SwapChoice.Suspend
-    } else if (small) {
-      oPartitions.userSwapChoice = SwapChoice.Small
-    } else if (none) {
-      oPartitions.userSwapChoice = SwapChoice.None
+    const oUsers: IUsers = {
+      username: this.krillConfig.name,
+      fullname: this.krillConfig.fullname,
+      password: this.krillConfig.password,
+      rootPassword: this.krillConfig.rootPassword,
+      autologin: this.krillConfig.autologin,
+      hostname
     }
 
+    const oNetwork: INet = {
+      iface: await Utils.iface(),
+      addressType: this.krillConfig.addressType,
+      address: Utils.address(),
+      netmask: Utils.netmask(),
+      gateway: Utils.gateway(),
+      dns: Utils.getDns(),
+      domain: Utils.getDomain()
+    }
+
+    return { oWelcome, oLocation, oKeyboard, oPartitions, oUsers, oNetwork }
+  }
+
+  /**
+   * Generate hostname based on options
+   */
+  private generateHostname(ip: boolean, random: boolean): string {
     let hostname = this.krillConfig.hostname
     if (hostname === '') {
       hostname = shx.exec('cat /etc/hostname', { silent: true }).trim()
@@ -231,109 +257,56 @@ export default class Krill {
       hostname = `${os.hostname()}-${fl}${n}${sl}`
     }
 
-    oUsers = {
-      username: this.krillConfig.name,
-      fullname: this.krillConfig.fullname,
-      password: this.krillConfig.password,
-      rootPassword: this.krillConfig.rootPassword,
-      autologin: this.krillConfig.autologin,
-      hostname: hostname
-    }
+    return hostname
+  }
 
-    oNetwork =
-    {
-      iface: await Utils.iface(),
-      addressType: this.krillConfig.addressType,
-      address: Utils.address(),
-      netmask: Utils.netmask(),
-      gateway: Utils.gateway(),
-      dns: Utils.getDns(),
-      domain: Utils.getDomain()
-    }
+  /**
+   * Apply options for unattended installation
+   */
+  private applyUnattendedOptions(configs: any, crypted: boolean, pve: boolean, btrfs: boolean) {
+    const { oPartitions, oUsers } = configs
 
-    /**
-     * Defaults
-     */
+    // Set defaults for unattended
+    oPartitions.installationMode = InstallationMode.EraseDisk
 
-    /**
-     * random = false, 
-     * domain = '', 
-     * pve = false
-     * */
+    // Apply options
+    if (btrfs) oPartitions.filesystemType = FsType.btrfs
+    if (crypted || pve) oPartitions.installationMode = InstallationMode.Luks
 
-    if (this.unattended) {
-      oPartitions.installationMode = InstallationMode.EraseDisk
-    }
-
-    // fs
-    if (btrfs) {
-      oPartitions.filesystemType = FsType.btrfs
-    }
-
-    // LUKS
-    if (crypted) {
-      oPartitions.installationMode = InstallationMode.Luks
-    }
-
-    // swap
-    if (none) {
-      oPartitions.userSwapChoice = SwapChoice.None
-    }
-
-    if (small) {
-      oPartitions.userSwapChoice = SwapChoice.Small
-    }
-
-    if (suspend) {
-      oPartitions.userSwapChoice = SwapChoice.Suspend
-    }
-
-
-    if (pve) {
-      oPartitions.installationMode = InstallationMode.Luks
-    }
-
-    if (ip) {
-      oUsers.hostname = 'ip-' + Utils.address().replaceAll('.', '-')
-    }
-
-    /**
-     * interactive
-     */
-    if (!this.unattended) {
-      oWelcome = await this.welcome()
-      oLocation = await this.location(oWelcome.language)
-      oKeyboard = await this.keyboard()
-      oPartitions = await this.partitions(this.krillConfig.installationDevice, crypted, pve, btrfs)
-      oUsers = await this.users()
-      oNetwork = await this.network()
-    } else {
-      /**
-       * this variables ALWAYS need to be initializated
-       */
-
-      // oPartitions.installationDevice
-      if (oPartitions.installationDevice === '') {
-        // No RAID considerated
-        const drives = shx.exec('lsblk |grep disk|cut -f 1 "-d "', { silent: true }).stdout.trim().split('\n')
-        if (drives.length > 0) {
-          oPartitions.installationDevice = `/dev/` + drives[0]
-        } else {
-          console.log("Unable to find installation drive")
-          process.exit(1)
-        }
+    // Set default installation device if empty
+    if (oPartitions.installationDevice === '') {
+      const drives = shx.exec('lsblk |grep disk|cut -f 1 "-d "', { silent: true }).stdout.trim().split('\n')
+      if (drives.length > 0) {
+        oPartitions.installationDevice = `/dev/` + drives[0]
+      } else {
+        throw new Error("Unable to find installation drive")
       }
-
-
     }
+
+    return configs
+  }
+
+  /**
+   * Run interactive mode with UI
+   */
+  private async runInteractiveMode(crypted: boolean, pve: boolean, btrfs: boolean) {
+    const oWelcome = await this.welcome()
+    const oLocation = await this.location(oWelcome.language)
+    const oKeyboard = await this.keyboard()
+    const oPartitions = await this.partitions(this.krillConfig.installationDevice, crypted, pve, btrfs)
+    const oUsers = await this.users()
+    const oNetwork = await this.network()
+
+    return { oWelcome, oLocation, oKeyboard, oPartitions, oUsers, oNetwork }
+  }
+
+  /**
+   * Perform installation or testing
+   */
+  private async performInstallation(configs: any, domain: string, testing: boolean, verbose: boolean): Promise<void> {
+    const { oLocation, oKeyboard, oPartitions, oUsers, oNetwork } = configs
+
     await this.summary(oLocation, oKeyboard, oPartitions, oUsers)
-
-
-    /**
-     * INSTALL
-     */
-    const sequence = new Sequence(oLocation, oKeyboard, oPartitions, oUsers, oNetwork)
-
 
     if (testing) {
       console.log()
@@ -343,44 +316,60 @@ export default class Krill {
       console.log(oPartitions)
       process.exit()
     } else {
+      const sequence = new Sequence(oLocation, oKeyboard, oPartitions, oUsers, oNetwork)
       await sequence.start(domain, this.unattended, this.nointeractive, this.chroot, this.halt, verbose)
     }
   }
 
-
-
-
-
-
+  /**
+   * Create script to remove LVM partitions
+   */
+  private async createLvmRemovalScript(): Promise<void> {
+    const scriptName = "removeLvmPartitions"
+    let cmds = "#!/bin/bash\n"
+    cmds += `# remove LV (Logical Volumes)\n`
+    cmds += `vg=$(vgs --noheadings -o vg_name| awk '{$1=$1};1')\n`
+    cmds += `lvs -o lv_name --noheadings | awk '{$1=$1};1' | while read -r lv; do\n`
+    cmds += ` lvremove -y /dev/mapper/$vg-$lv\n`
+    cmds += `done\n`
+    cmds += `\n`
+    cmds += `# remove VG (Volume groups)\n`
+    cmds += `vgremove --force $(vgs --noheadings -o vg_name $vg)\n`
+    cmds += `\n`
+    cmds += `# remove PV (Physical Volumes) \n`
+    cmds += `pv=$(pvs --noheading -o pv_name | awk '{$1=$1};1')\n`
+    cmds += `pvremove --force --force $pv\n`
+    cmds += `# wipe PV (Physical Volumes) \n`
+    cmds += `wipefs -a $pv\n`
+    cmds += `# clean device\n`
+    cmds += `sgdisk --zap-all $pv\n`
+    cmds += `dd if=/dev/zero of=$pv bs=1M count=10\n`
+    
+    fs.writeFileSync(scriptName, cmds)
+    await exec(`chmod +x ${scriptName}`)
+  }
 
   /**
-   * return true if pv exist
+   * Check if Physical Volumes exist
    */
   private async pvExist(): Promise<boolean> {
-    let exist = false
     const check = `#!/bin/sh\npvdisplay |grep "PV Name" >/dev/null && echo 1|| echo 0`
-    if (shx.exec(check, { silent: true }).stdout.trim() === '1') {
-      exist = true
-    }
-    return exist
+    return shx.exec(check, { silent: true }).stdout.trim() === '1'
   }
 }
 
-
 /**
- *
- * @param mask
+ * Utility function for netmask conversion
  */
 function netmask2CIDR(mask: string) {
-  const countCharOccurences = (string: string, char: string) => string.split(char).length - 1;
-
-  const decimalToBinary = (dec: number) => (dec >>> 0).toString(2);
-  const getNetMaskParts = (nmask: string) => nmask.split('.').map(Number);
-  const netmask2CIDR = (netmask: string) =>
-    countCharOccurences(
-      getNetMaskParts(netmask)
-        .map(part => decimalToBinary(part))
-        .join(''),
-      '1'
-    );
+  const countCharOccurences = (string: string, char: string) => string.split(char).length - 1
+  const decimalToBinary = (dec: number) => (dec >>> 0).toString(2)
+  const getNetMaskParts = (nmask: string) => nmask.split('.').map(Number)
+  
+  return countCharOccurences(
+    getNetMaskParts(mask)
+      .map(part => decimalToBinary(part))
+      .join(''),
+    '1'
+  )
 }
