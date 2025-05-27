3e5b8ab1fe741ba30dd38036c59fef2789bf155a
artisan <artisan@minimal>
2025-05-27 21:08:47 +0200

    book

--- src/classes/ovary.d/make-dot-disk.ts
+++ src/classes/ovary.d/make-dot-disk.ts
@@ -30,6 +30,39 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname)
    */
 export function makeDotDisk(this: Ovary, info = '', mksquashfs = '', mkisofs = '') {
     if (this.verbose) {
+        console.log('Ovary: makeDotDisk');
+    }
+
+    const dotDisk = this.settings.iso_work + '.disk';
+    if (fs.existsSync(dotDisk)) {
+        shx.rm('-rf', dotDisk);
+    }
+
+    shx.mkdir('-p', dotDisk);
+    let text = `# Created at: ${Utils.formatDate(new Date())}\n`;
+    text += `# penguins_eggs v. ${Utils.getPackageVersion()}\n`;
+
+    // .disk/info
+    fs.writeFileSync(dotDisk + '/info', text, 'utf-8');
+
+    // Fix: Ensure this.uuid is valid
+    if (!this.uuid || this.uuid.trim() === '') {
+        this.uuid = 'default_id';
+    }
+
+    // Fix: Write to .disk/id as a FILE (not directory)
+    fs.writeFileSync(path.join(dotDisk, 'id'), this.uuid, 'utf-8');
+
+    // .disk/mksquashfs
+    fs.writeFileSync(dotDisk + '/mksquashfs', text + mksquashfs, 'utf-8');
+
+    // .disk/mkisofs
+    fs.writeFileSync(dotDisk + '/mkisofs', text + mkisofs, 'utf-8');
+}
+
+/*
+export function makeDotDisk(this: Ovary, info = '', mksquashfs = '', mkisofs = '') {
+    if (this.verbose) {
         console.log('Ovary: makeDotDisk')
     }
 
@@ -54,3 +87,4 @@ export function makeDotDisk(this: Ovary, info = '', mksquashfs = '', mkisofs = '
     // .disk/mkisofs
     fs.writeFileSync(dotDisk + '/mkisofs', text + mkisofs, 'utf-8')
 }
+*/
