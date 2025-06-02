3d156e74609599a56ea62fe6a5a4b55ed1a6efbb
Piero Proietti <piero.proietti@gmail.com>
2025-06-02 16:11:33 +0200

    correzioni, si spera, finali

--- src/classes/ovary.d/user-create-live.ts
+++ src/classes/ovary.d/user-create-live.ts
@@ -128,7 +128,7 @@ export async function userCreateLive(this: Ovary) {
             const groupExists = await exec(`chroot ${this.settings.work_dir.merged} getent group ${group}`, {ignore: true})
             if (groupExists.code == 0) {
                 cmds.push(await rexec(`chroot ${this.settings.work_dir.merged} usermod -aG ${group} ${this.settings.config.user_opt} ${this.toNull}`, this.verbose))
-                Utils.warning(`User ${this.settings.config.user_opt} added to group : ${group}`)
+                Utils.warning(`added ${this.settings.config.user_opt} to group ${group}`)
             }
         }
     } else {
