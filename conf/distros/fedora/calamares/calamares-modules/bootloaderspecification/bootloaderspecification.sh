30f51704b65857cc06c96663e54096e201bb0ecc
Piero Proietti <piero.proietti@gmail.com>
2025-07-15 12:05:41 +0200

    funzionanete su Fedora

--- conf/distros/fedora/calamares/calamares-modules/bootloaderspecification/bootloaderspecification.sh
+++ conf/distros/fedora/calamares/calamares-modules/bootloaderspecification/bootloaderspecification.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
 
+# Viene chiamato in CHROOT
 
-# Trova l'ultimo kernel installato (escludendo le voci di 'rescue')
-KERNEL_VERSION=$(ls $CHROOT/boot/vmlinuz-* | grep -v rescue | sed 's/.*vmlinuz-//' | sort -V | tail -n 1)
+# Solo l'ultimo kernel installato (escludendo le voci di 'rescue')
+KERNEL_VERSION=$(ls /boot/vmlinuz-* | grep -v rescue | sed 's/.*vmlinuz-//' | sort -V | tail -n 1)
 
 if [ -z "$KERNEL_VERSION" ]; then
     echo "ERRORE: Nessun kernel trovato in /boot"
@@ -14,13 +14,15 @@ fi
 # findmnt è più affidabile di blkid in un chroot
 ROOT_UUID=$(findmnt -n -o UUID --target /)
 
+# Assicuriamo che /boot/loader/entries esista
+mkdir -p /boot/loader/entries
+
+
 # Prendi l'ID della macchina per il nome del file BLS
-MACHINE_ID=$(cat $CHROOT/etc/machine-id)
+MACHINE_ID=$(cat /etc/machine-id)
 
 # Costruisci il percorso completo del file BLS
-BLS_FILE="$CHROOT/boot/loader/entries/${MACHINE_ID}-${KERNEL_VERSION}.conf"
-
-echo "Creazione del file BLS in: ${BLS_FILE}"
+BLS_FILE="/boot/loader/entries/${MACHINE_ID}-${KERNEL_VERSION}.conf"
 
 # Scrivi il contenuto nel file usando un "Here Document" (cat << EOF)
 cat > "${BLS_FILE}" << EOF
@@ -31,6 +33,5 @@ initrd /boot/initramfs-${KERNEL_VERSION}.img
 options root=UUID=${ROOT_UUID} ro
 EOF
 
-echo "File BLS creato con successo."
+exit 0
 
-# --- Fine Script ---
