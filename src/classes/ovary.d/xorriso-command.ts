be3e0cfece9476f2d81e33ee287c8f3c7cafa949
Piero Proietti <piero.proietti@gmail.com>
2025-08-13 13:40:02 +0200

    dovrebbe andare ora...

--- src/classes/ovary.d/xorriso-command.ts
+++ src/classes/ovary.d/xorriso-command.ts
@@ -26,69 +26,67 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname)
    * @returns cmd 4 mkiso
    */
 export async function xorrisoCommand(this: Ovary, clone = false, cryptedclone = false): Promise<string> {
-    if (this.verbose) {
-      console.log('Ovary: xorrisoCommand')
+  if (this.verbose) {
+    console.log('Ovary: xorrisoCommand')
+  }
+
+  const prefix = this.settings.config.snapshot_prefix
+
+  let typology = ''
+  // typology is applied only with standard egg-of
+  if (prefix.slice(0, 7) === 'egg-of_') {
+    if (clone) {
+      typology = '_clone'
+    } else if (cryptedclone) {
+      typology = '_crypted'
     }
 
-    const prefix = this.settings.config.snapshot_prefix
-
-    let typology = ''
-    // typology is applied only with standard egg-of
-    if (prefix.slice(0, 7) === 'egg-of_') {
-      if (clone) {
-        typology = '_clone'
-      } else if (cryptedclone) {
-        typology = '_crypted'
-      }
-
-      if (fs.existsSync('/usr/bin/eui-start.sh')) {
-        typology += '_EUI'
-      }
+    if (fs.existsSync('/usr/bin/eui-start.sh')) {
+      typology += '_EUI'
     }
+  }
 
-    const postfix = Utils.getPostfix()
-    this.settings.isoFilename = prefix + this.volid + '_' + Utils.uefiArch() + typology + postfix
-    //
-    const output = this.settings.config.snapshot_mnt + this.settings.isoFilename
+  const postfix = Utils.getPostfix()
+  this.settings.isoFilename = prefix + this.volid + '_' + Utils.uefiArch() + typology + postfix
+  //
+  const output = this.settings.config.snapshot_mnt + this.settings.isoFilename
 
-    let command = ''
-    // const appid = `-appid "${this.settings.distro.distroId}" `
-    // const publisher = `-publisher "${this.settings.distro.distroId}/${this.settings.distro.codenameId}" `
-    // const preparer = '-preparer "prepared by eggs <https://penguins-eggs.net>" '
+  let command = ''
+  // const appid = `-appid "${this.settings.distro.distroId}" `
+  // const publisher = `-publisher "${this.settings.distro.distroId}/${this.settings.distro.codenameId}" `
+  // const preparer = '-preparer "prepared by eggs <https://penguins-eggs.net>" '
 
-    let isoHybridMbr = ''
-    if (process.arch !== 'arm64') {
-      if (this.settings.config.make_isohybrid) {
-        const bootloaders = Diversions.bootloaders(this.familyId)
-        const isolinuxFile = path.resolve(bootloaders, `ISOLINUX/isolinux.bin`)
-        if (fs.existsSync(isolinuxFile)) {
-          isoHybridMbr = `-isohybrid-mbr ${isolinuxFile}`
-        } else {
-          Utils.warning(`Can't create isohybrid image. File: ${isolinuxFile} not found!`)
-          process.exit()
-        }
-      }
+  let isoHybridMbr = ''
+  if (this.settings.config.make_isohybrid) {
+    const bootloaders = Diversions.bootloaders(this.familyId)
+    const isolinuxFile = path.resolve(bootloaders, `ISOLINUX/isohdpfx.bin`)
+    if (fs.existsSync(isolinuxFile)) {
+      isoHybridMbr = `-isohybrid-mbr ${isolinuxFile}`
+    } else {
+      Utils.warning(`Can't create isohybrid image. File: ${isolinuxFile} not found!`)
+      process.exit()
     }
+  }
 
-    // su arm no isolinux
-    let isolinuxBin = ''
-    let isolinuxCat = ''
-    let noemulboot= ''
-    let bootloadsize = ''
-    let bootinfotable = ''
+  // su arm no isolinux
+  let isolinuxBin = ''
+  let isolinuxCat = ''
+  let noemulboot = ''
+  let bootloadsize = ''
+  let bootinfotable = ''
 
-    if (process.arch !== 'arm64') {
-      isolinuxBin = `-b isolinux/isolinux.bin`
-      isolinuxCat = `-c isolinux/boot.cat`
-      noemulboot = '-no-emul-boot'
-      bootloadsize = '-boot-load-size 4'
-      bootinfotable = '-boot-info-table'
-    } 
-    
-    if (Pacman.packageIsInstalled('genisoimage')) {
-      this.genisoimage = true
+  if (process.arch !== 'arm64') {
+    isolinuxBin = `-b isolinux/isolinux.bin`
+    isolinuxCat = `-c isolinux/boot.cat`
+    noemulboot = '-no-emul-boot'
+    bootloadsize = '-boot-load-size 4'
+    bootinfotable = '-boot-info-table'
+  }
 
-      command = `genisoimage \
+  if (Pacman.packageIsInstalled('genisoimage')) {
+    this.genisoimage = true
+
+    command = `genisoimage \
         -iso-level 3 \
         -allow-limited-size \
         -joliet-long \
@@ -106,34 +104,34 @@ export async function xorrisoCommand(this: Ovary, clone = false, cryptedclone = 
         -e boot/grub/efi.img \
         -o ${output} ${this.settings.iso_work}`
 
-      return command
-    }
-    
-    
-    /**
-     * xorriso
-     */
-    // uefi_opt="-uefi_elToritoAltBoot-alt-boot -e boot/grub/efi.img -isohybrid-gpt-basdat -no-emul-boot"
-    let uefi_elToritoAltBoot = ''
-    let uefi_e = ''
-    let uefi_isohybridGptBasdat = ''
-    let uefi_noEmulBoot = ''
-    if (this.settings.config.make_efi) {
-      uefi_elToritoAltBoot = '-eltorito-alt-boot'
-      uefi_e = '-e boot/grub/efi.img'
-      uefi_isohybridGptBasdat = '-isohybrid-gpt-basdat'
-      uefi_noEmulBoot = '-no-emul-boot'
-    }
-    /**
-     * L'immagine efi è efi.img ed è
-     * presente in boot/grub/efi.img
-     * per cui:
-     * -append_partition 2 0xef efi.img
-     * --efi-boot efi.img
-     * non sono necessari
-     */
+    return command
+  }
 
-    command = `xorriso -as mkisofs \
+
+  /**
+   * xorriso
+   */
+  // uefi_opt="-uefi_elToritoAltBoot-alt-boot -e boot/grub/efi.img -isohybrid-gpt-basdat -no-emul-boot"
+  let uefi_elToritoAltBoot = ''
+  let uefi_e = ''
+  let uefi_isohybridGptBasdat = ''
+  let uefi_noEmulBoot = ''
+  if (this.settings.config.make_efi) {
+    uefi_elToritoAltBoot = '-eltorito-alt-boot'
+    uefi_e = '-e boot/grub/efi.img'
+    uefi_isohybridGptBasdat = '-isohybrid-gpt-basdat'
+    uefi_noEmulBoot = '-no-emul-boot'
+  }
+  /**
+   * L'immagine efi è efi.img ed è
+   * presente in boot/grub/efi.img
+   * per cui:
+   * -append_partition 2 0xef efi.img
+   * --efi-boot efi.img
+   * non sono necessari
+   */
+
+  command = `xorriso -as mkisofs \
      -J \
      -joliet-long \
      -l \
@@ -152,6 +150,6 @@ export async function xorrisoCommand(this: Ovary, clone = false, cryptedclone = 
      ${uefi_noEmulBoot} \
      -o ${output} ${this.settings.iso_work}`
 
-    return command
+  return command
 
-  }
+}
