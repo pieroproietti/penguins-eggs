3bd4c81e6691aaecf374ad57fb4ce7268c987bb1
Piero Proietti <piero.proietti@gmail.com>
2025-09-07 16:31:51 +0200

    sistemata update speriamo per tutte

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -16,6 +16,7 @@ import Utils from '../classes/utils.js'
 import Diversions from '../classes/diversions.js'
 import { exec } from '../lib/utils.js'
 
+import fs from 'node:fs'
 import axios from 'axios'
 import https from 'node:https'
 const agent = new https.Agent({
@@ -63,26 +64,20 @@ export default class Update extends Command {
     const choose = await this.choosePkg()
     Utils.titles(`updating via ${choose}`)
     switch (choose) {
-      case 'package_manager': {
-        await this.getPkgFromPackageManager()
-
-        break
-      }
-
-      case 'lan': {
+      case 'LAN': {
         await this.getPkgFromLan()
 
         break
       }
 
-      case 'sourceforge': {
-        this.getPkgFromSourceforge()
+      case 'Package_Manager': {
+        await this.getPkgFromPackageManager()
 
         break
       }
 
-      case 'sources': {
-        this.getFromSources()
+      case 'Source': {
+        this.getFromSource()
 
         break
       }
@@ -94,19 +89,19 @@ export default class Update extends Command {
    *
    */
   async choosePkg(): Promise<string> {
-    const choices: string[] = ['abort']
-    choices.push('lan', 'package_manager', 'sources')
+    const choices: string[] = ['Abort']
+    choices.push('LAN', 'Source', 'Package_Manager')
 
     const questions: any = [
       {
         choices,
-        message: 'select update method',
+        message: 'Select update method',
         name: 'selected',
         type: 'list'
       }
     ]
     const answer = await inquirer.prompt(questions)
-    if (answer.selected === 'abort') {
+    if (answer.selected === 'Abort') {
       process.exit(0)
     }
 
@@ -119,20 +114,23 @@ export default class Update extends Command {
   async getPkgFromPackageManager() {
     let cmd = ""
     if (this.distro.familyId === 'alpine') {
-      cmd = `apk add penguins-egga`
+      cmd = `doas apk add penguins-egga`
     } else if (this.distro.familyId === 'archlinux') {
-      cmd = 'pacman -S penguins-eggs'
+      cmd = 'sudo pacman -S penguins-eggs'
     } else if (this.distro.familyId === "debian") {
-      cmd = 'apt install penguins-eggs'
+      cmd = 'sudo apt install penguins-eggs'
     } else if (this.distro.familyId === "fedora") {
-      cmd = 'dnf install penguins-eggs'
+      cmd = 'sudo dnf install penguins-eggs\nsudo dnf install penguins-eggs'
     } else if (this.distro.familyId === "openmamba") {
-      cmd = 'dnf install penguins-eggs'
+      cmd = 'sudo dnf install penguins-eggs\nsudo dnf install penguins-eggs'
     } else if (this.distro.familyId === "opensuse") {
-      cmd = 'dnf install penguins-eggs'
+      cmd = 'sudo zypper install penguins-eggs'
     }
+    Utils.titles(`update`)
+    Utils.warning(`To update/install penguins-eggs cut and copy follow commands`)
+    console.log()
     console.log(cmd)
-    await exec(cmd)
+    console.log()
   }
 
   /**
@@ -143,6 +141,7 @@ export default class Update extends Command {
     await Tu.loadSettings()
 
     Utils.warning('Update penguins-eggs from LAN')
+    let filter = ''
     let copy = ''
     let install = ''
 
@@ -151,7 +150,8 @@ export default class Update extends Command {
      */
     if (this.distro.familyId === 'alpine') {
       let repo = `alpine/x86_64`
-      let filter = `penguins-eggs-*[0-9][0-9].@([0-9]|[0-1][0-9]).@([0-9]|[0-3][0-9])-*.apk`
+
+      filter = `penguins-eggs-*-*.*.?-r?.apk`
       copy = `scp ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathPackages}/${repo}/${filter} /tmp`
       install = `apk add /tmp/${filter}`
     
@@ -161,10 +161,10 @@ export default class Update extends Command {
      */
     } else if (this.distro.familyId === 'archlinux') {
       let repo = "aur"
-      let filter = `penguins-eggs-[0-9][0-9].@([0-9]|[0-1][0-9]).@([0-9]|[0-3][0-9])-*-any.pkg.tar.zst`
+      filter = `penguins-eggs-??.*.*-?-any.pkg.tar.zst`
       if (Diversions.isManjaroBased(this.distro.distroId)) {
         repo = 'manjaro'
-        filter = `penguins-eggs-[0-9][0-9].@([0-9]|[0-1][0-9]).@([0-9]|[0-3][0-9])-*-any.pkg.tar.*`        
+        filter = `penguins-eggs-??.*.*-?-any.pkg.tar.*`        
       }
       copy = `scp ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathPackages}/${repo}/${filter} /tmp`
       install = `pacman -U /tmp/${filter}`
@@ -175,7 +175,7 @@ export default class Update extends Command {
      */
     } else if (this.distro.familyId === "debian") {
       let repo = 'debs'
-      let filter = `penguins-eggs_[0-9][0-9].@([0-9]|[0-1][0-9]).@([0-9]|[0-3][0-9])-*_${Utils.uefiArch()}.deb`
+      filter = `penguins-eggs_??.*.*-?_${Utils.uefiArch()}.deb`
       copy = `scp ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathPackages}/${repo}/${filter} /tmp`
       install = `dpkg -i /tmp/${filter}`
 
@@ -185,10 +185,12 @@ export default class Update extends Command {
      */
     } else if (this.distro.familyId === "fedora") {
       let repo = 'fedora'
+      let ftype = 'fc??'
       if (this.distro.distroId !=='Fedora') {
         repo = 'el9'
+        ftype = 'el?'
       }
-      let filter = `penguins-eggs-[0-9][0-9].[0-9]*.[0-9]*-*.fc??.x86_64.rpm`
+      filter = `penguins-eggs-??.*.*-?.${ftype}.x86_64.rpm`
       copy = `scp ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathPackages}/${repo}/${filter} /tmp`
       install = `dnf reinstall /tmp/${filter} || dnf install /tmp/${filter}`
  
@@ -198,7 +200,7 @@ export default class Update extends Command {
        */
     } else if (this.distro.familyId === "openmamba") {
       let repo = 'openmamba'
-      let filter = `penguins-eggs-[0-9][0-9].@([0-9]|[0-1][0-9]).@([0-9]|[0-3][0-9])-*mamba.*.rpm`
+      filter = `penguins-eggs-??.*.*-?mamba.x86_64.rpm`
       copy = `scp ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathPackages}/${repo}/${filter} /tmp`
       install = `dnf reinstall /tmp/${filter} || dnf install /tmp/${filter}`
 
@@ -207,7 +209,7 @@ export default class Update extends Command {
        */
     } else if (this.distro.familyId === "opensuse") {
       let repo = 'opensuse'
-      let filter = `penguins-eggs-[0-9][0-9].[0-9]*.[0-9]*-*.opensuse.x86_64.rpm`
+      filter = `penguins-eggs-*.*.*-?.opensuse.x86_64.rpm`
       copy = `scp ${Tu.config.remoteUser}@${Tu.config.remoteHost}:${Tu.config.remotePathPackages}/${repo}/${filter} /tmp`
       install = `zypper install /tmp/${filter}`
     }
@@ -224,57 +226,18 @@ export default class Update extends Command {
 
 
   /**
-   *
+   * 
    */
-  async getPkgFromSourceforge() {
-    let repo = ''
-    let cmd = ''
-    let url = 'https://sourceforge.net/projects/penguins-eggs/files/Packages'
-    let filter = `penguins-eggs`
-    if (this.distro.familyId === 'archlinux') {
-      repo = "aur"
-      filter = `penguins-eggs-10.?.*-?-any.pkg.tar.zst`
-      cmd = `sudo pacman -U ${filter}`
-      if (Diversions.isManjaroBased(this.distro.distroId)) {
-        repo = 'manjaro'
-      }
-      url = `${url}/${repo}`
-    } else if (this.distro.familyId === "alpine" ) {
-      let arch = 'x86_64'
-      if (process.arch === 'ia32') {
-        arch = 'i386'
-      }
-      repo="alpine"
-      url= `${url}/${repo}/$arch/${Utils.uefiArch()}`
-      const filter = `penguins-eggs-25.*.*-r*.apk`
-      cmd = `doas apk add ${filter}`
-
-    } else     if (this.distro.familyId === "debian") {
-      repo = "debs"
-      url = `${url}/${repo}`
-      filter = `penguins-eggs_25.*.*-*_${Utils.uefiArch()}.deb`
-      cmd = `sudo apt-get install ./${filter}`
-    }
-    let command = `- open your browser at ${url}\n`
-    command += `- select and download last package: ${filter}\n`
-    command += `- ${cmd}\n`
-    console.log(command)
-    this.show(url)
-  }
-
-  /**
- *
- * @param aptVersion
- */
-  getFromSources() {
-    console.log('You can get a fresh installation, cloning penguins-eggs:')
+  getFromSource() {
+    console.log('Use the following commands to use penguins-eggs from source:')
+    console.log('')
     console.log('cd ~')
     console.log('git clone https://github.com/pieroproietti/penguins-eggs')
+    console.log('cd penguins-eggs')
+    console.log('pnpm i')
+    console.log('./eggs')
     console.log('')
-    console.log('Then, open your browser to read detailed instructions:')
-    console.log('https://github.com/pieroproietti/penguins-eggs/blob/master/PREREQUISITES/README.md')
-    console.log('')
-    console.log('')
+    console.log('Note: requires nodejs>18 and pnpm installed')
   }
 
   /**
