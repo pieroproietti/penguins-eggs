6e0509b98421bc7d3284cdec2852ff40db08e43a
Piero Proietti <piero.proietti@gmail.com>
2025-09-30 09:30:45 +0200

    mamjaro-prerequisites

---
+++ manjaro-prerequisites.sh
@@ -0,0 +1,27 @@
+pacman -S \
+    arch-install-scripts \
+    dosfstools \
+    efibootmgr \
+    erofs-utils \
+    findutils \
+    git \
+    grub \
+    jq \
+    libarchive \
+    libisoburn \
+    lvm2 \
+    manjaro-tools-iso \
+    mkinitcpio-nfs-utils \
+    mtools \
+    nbd \
+    nodejs \
+    pacman-contrib \
+    parted \
+    procps-ng \
+    pv \
+    python \
+    rsync \
+    squashfs-tools \
+    sshfs \
+    get \
+    xdg-utils 
