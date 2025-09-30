23890d71ea7710b1eacb18743ff2973a8fe540fd
Piero Proietti <piero.proietti@gmail.com>
2025-09-30 18:59:39 +0200

    Devel30 (#636)
    * confronto manjaro con eggs
    
    * update-calamares.sh per manjaro
    
    * riordinato settings
    
    * mamjaro-prerequisites
    
    * ok
    
    * ok
    
    * vediamo se va
    
    * tol - test on line
    
    * vediamo
    
    * tol is starting
    
    * completion
    
    * setting
    
    * pnpm update version
    
    * aggiustamenti in manjaro
    
    * services
    
    * allineato completamente a manjaro
    
    * allineato a manjaro solo
    
    * proviamo su Arch

---
+++ tol/manjaro-prerequisites.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+pacman -S \
+    arch-install-scripts \
+    dosfstools \
+    efibootmgr \
+    erofs-utils \
+    findutils \
+    git \
+    grub \
+    jq \
+    libarchive \
+    libisoburn \
+    lvm2 \
+    manjaro-tools-iso \
+    mkinitcpio-nfs-utils \
+    mtools \
+    nbd \
+    nodejs \
+    pacman-contrib \
+    parted \
+    procps-ng \
+    pv \
+    python \
+    rsync \
+    squashfs-tools \
+    sshfs \
+    git \
+    xdg-utils 
