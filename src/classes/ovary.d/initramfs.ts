6cb12cf03c84bfa359934ec95f13940296da5a09
Piero Proietti <piero.proietti@gmail.com>
2025-10-11 21:41:04 +0200

    vediamo un po'

--- src/classes/ovary.d/initramfs.ts
+++ src/classes/ovary.d/initramfs.ts
@@ -23,10 +23,17 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname)
  * initramfsDebian
  */
 export async function initramfsDebianLuks(this: Ovary, verbose = true) {
-    // -c ${confdir}
     Utils.warning(`creating ${this.initrd} using mkinitramfs  on (ISO)/live`)
-    // await exec(`mkinitramfs -o ${this.settings.iso_work}live/${path.basename(this.initrd)} ${this.kernel} ${this.toNull}`, this.echo)
-    await exec(`mkinitramfs -o ${this.settings.iso_work}live/${path.basename(this.initrd)} ${this.kernel} -v`)
 
+    const src = path.join(__dirname, `../../../initramfs-crypto/initramfs-tools`)
+    const dest = `/etc/initramfs-tools/`
+    await exec(`cp -r ${src} ${dest}`)
+
+    const prefix = this.settings.config.snapshot_prefix
+    const log=`> ${this.settings.iso_work}${prefix}dracut.log.txt 2>&1`
+    const cmd=`mkinitramfs -o ${this.settings.iso_work}live/${path.basename(this.initrd)} ${this.kernel} -v ${log}`
+
+    console.log(cmd)
+    await exec(cmd, this.echo)
 }
 
