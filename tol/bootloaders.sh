d81c90dcec68cd3840a19f8128179bca17c39dcd
Piero Proietti <piero.proietti@gmail.com>
2025-11-19 10:33:31 +0100

    vediamo che succede

--- tol/bootloaders.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-_bootloadersver=25.9.8
-wget -P /tmp https://github.com/pieroproietti/penguins-bootloaders/releases/download/v$_bootloadersver/bootloaders.tar.gz
-sudo mkdir -p /usr/lib/penguins-eggs/
-sudo tar -xf /tmp/bootloaders.tar.gz -C /usr/lib/penguins-eggs
