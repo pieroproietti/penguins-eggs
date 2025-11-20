88149c6b34ae3910e07a5db29bb0de37293a4a8e
Piero Proietti <piero.proietti@gmail.com>
2025-11-20 20:12:34 +0100

    ritocchi

--- src/appimage/first-run-check.ts
+++ src/appimage/first-run-check.ts
@@ -10,6 +10,6 @@ import { Prerequisites } from './prerequisites.js';
 
 const prerequisites = new Prerequisites();
 if (!prerequisites.check()) {
-   console.log('WARNING: System needs setup for full functionality.');
+   console.log('WARNING: You need to setup penguins-eggs for full functionality.');
    console.log('Run: sudo eggs setup');
 }
