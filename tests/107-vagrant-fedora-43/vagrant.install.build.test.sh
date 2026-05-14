15b6f298a9c03b7bb3b1386e0d8e770d807b2474
gnuhub <gnuhub@gmail.com>
2026-05-14 16:14:50 +0800

    [ros-work-716-26040410]:[12828](chore:add more tests)[2026-05-14 16:14:50]

---
+++ tests/107-vagrant-fedora-43/vagrant.install.build.test.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+
+cd ../../
+pwd
+make
+rsync -avzP ./coa/coa $CMD_PATH/
+rsync -avzP ./oa/oa $CMD_PATH/
+
+cd $CMD_PATH
+echo "=== Install dependencies ==="
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    wget \
+    curl \
+    gnupg2 \
+    software-properties-common \
+    virtualbox
+
+echo "=== Install Vagrant ==="
+
+VAGRANT_VERSION="2.4.9"
+
+wget -q \
+  https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}-1_amd64.deb
+
+sudo dpkg -i vagrant_${VAGRANT_VERSION}-1_amd64.deb
+
+echo "=== Verify ==="
+
+vagrant --version
+VBoxManage --version
+cd $CMD_PATH
+ls -al
+sudo systemctl stop libvirtd
+sudo systemctl stop virtlogd
+sudo modprobe -r kvm_amd
+sudo modprobe -r kvm
+vagrant up
+sleep 10
+vagrant ssh -c "uname -a"
+vagrant ssh -c "/vagrant/oa --help"
+vagrant ssh -c "/vagrant/coa --help"
+vagrant ssh -c "sudo coa remaster --mode clone"
+
