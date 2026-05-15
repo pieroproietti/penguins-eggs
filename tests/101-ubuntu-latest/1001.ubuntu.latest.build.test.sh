ab4f9d0f4ec5e3629e86c96cc252ca25f8cae90b
Piero Proietti <piero.proietti@gmail.com>
2026-05-15 21:18:43 +0200

    ultimo tentativo con creazione ed installazione pacchetto oa-tools

--- tests/101-ubuntu-latest/1001.ubuntu.latest.build.test.sh
+++ tests/101-ubuntu-latest/1001.ubuntu.latest.build.test.sh
@@ -9,16 +9,20 @@ cd $CMD_PATH
 cd ../../
 pwd
 make
+./coa/coa tools build
+sudo dpkg -i oa-tools*.deb
+sudo apt-get install -i
 
-sudo rsync -avzP ./coa/coa /usr/bin/coa
-sudo rsync -avzP ./oa/oa /usr/bin/oa
-sudo apt update -y
-sudo apt install squashfs-tools xorriso live-boot live-boot-initramfs-tools dosfstools mtools rsync git sudo -y
-./oa/oa --help
-./coa/coa --help
-cd $CMD_PATH
-cd ../../
-#./coa/coa tools build
-ls -al
-./coa/coa coa tools clean
+#sudo rsync -avzP ./coa/coa /usr/bin/coa
+#sudo rsync -avzP ./oa/oa /usr/bin/oa
+
+# installazione pachetti
+#sudo apt update -y
+#sudo apt install squashfs-tools xorriso live-boot live-boot-initramfs-tools dosfstools mtools rsync git sudo -y
+#oa --help
+#coa --help
+#cd $CMD_PATH
+#cd ../../
+#ls -al
+sudo coa tools clean
 sudo coa remaster
