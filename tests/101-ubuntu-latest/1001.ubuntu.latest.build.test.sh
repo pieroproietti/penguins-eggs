904aafcc453e88c4f5b9d2dcbdb6d9fe191860fe
Piero Proietti <piero.proietti@gmail.com>
2026-05-16 04:28:50 +0200

    test di build

--- tests/101-ubuntu-latest/1001.ubuntu.latest.build.test.sh
+++ tests/101-ubuntu-latest/1001.ubuntu.latest.build.test.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env bash
 
-
+set -e # Fondamentale in CI: ferma lo script al primo errore!
 set -x
+
 export CMD_PATH=$(cd `dirname $0`; pwd)
 export PROJECT_NAME="${CMD_PATH##*/}"
-echo $PROJECT_NAME
-cd $CMD_PATH
+echo "Project name: $PROJECT_NAME"
+
+cd "$CMD_PATH"
 cd ../../
 pwd
+
+# 1. Compilazione del binario e generazione asset
 make
+
+# 2. Generazione del pacchetto .deb
 ./coa/coa tools build
-sudo dpkg -i oa-tools*.deb
-sudo apt-get install -i
+
+# 3. Installazione chirurgica del .deb
+# Usiamo find per beccarlo ovunque sia nato (nella root, in oa/ o in coa/) 
+# ed evitiamo che gli apici o l'asterisco facciano impazzire dpkg
+DEB_FILE=$(find . -maxdepth 3 -name "oa-tools*.deb" | head -n 1)
+
+if [ -z "$DEB_FILE" ]; then
+    echo "ERRORE: Il pacchetto .deb non è stato generato!"
+    exit 1
+fi
+
+sudo dpkg -i "$DEB_FILE"
+
+# 4. Risoluzione delle dipendenze (Corretto il refuso -i)
+sudo apt-get install -f -y
+
+echo "=== INSTALLAZIONE COMPLETATA CON SUCCESSO ==="
+exit 0
+
 
 #sudo rsync -avzP ./coa/coa /usr/bin/coa
 #sudo rsync -avzP ./oa/oa /usr/bin/oa
