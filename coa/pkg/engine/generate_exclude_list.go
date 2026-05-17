96332d7fa792662e7fcf88aa39c7721c9cad152c
Piero Proietti <piero.proietti@gmail.com>
2026-05-17 20:55:11 +0200

    excludendo var e filtrando usr

--- coa/pkg/engine/generate_exclude_list.go
+++ coa/pkg/engine/generate_exclude_list.go
@@ -79,7 +79,7 @@ func GenerateExcludeList(mode string, isGitHubAction bool) string {
 			"usr/local/share/powershell/*", // Ciao ciao PowerShell
 			"usr/share/swift/*",            // Swift toolchain
 			"var/lib/gems/*",               // Residui di Ruby
-			"usr/*",                        // il colpo di genio finare
+			"var/*",                        // il colpo di genio finare
 		)
 	}
 
