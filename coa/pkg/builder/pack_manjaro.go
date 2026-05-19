9e9afa4ac95dff891e3b7d45861ad6a98f038396
Piero Proietti <piero.proietti@gmail.com>
2026-05-19 14:39:35 +0200

    checked manjaro on manjaro

--- coa/pkg/builder/pack_manjaro.go
+++ coa/pkg/builder/pack_manjaro.go
@@ -5,30 +5,27 @@ import (
 	"os"
 	"path/filepath"
 
-	sysctx "coa/pkg/context" // Il nostro scudo contestuale
+	sysctx "coa/pkg/context"
 )
 
 // packManjaro genera il file PKGBUILD specifico per Manjaro Linux.
-// RISPETTA IL PATTO: Sfrutta il RuntimeContext per decidere se confinare in /tmp/oa-build o scrivere nella repo.
 func packManjaro(baseVer string, relNum string, ctx sysctx.RuntimeContext) {
-	// 1. Definiamo dove salvare il PKGBUILD tramite i campi del contesto
 	var outDir string
 
 	if ctx.EnvType == sysctx.EnvVagrant {
-		// In Vagrant va direttamente nella radice di /tmp/oa-build al sicuro da 9p
 		outDir = ctx.BaseBuildDir
 	} else {
-		// Local Mode: Scriviamo direttamente nella radice del progetto
 		outDir = ctx.ProjRoot
 	}
 
-	// 2. Definiamo il contenuto del PKGBUILD per Manjaro usando i campi della struct ctx
+	// IL PKGBUILD SEMPLICE: L'orchestratore ha già fatto tutto.
 	pkgbuildContent := fmt.Sprintf(`# Maintainer: Piero Proietti <piero.proietti@gmail.com>
 # coa is the mind and oa the arm
 pkgname=oa-tools-manjaro
 pkgver=%s
 pkgrel=%s
 _srcdir="%s"
+_coadir="%s"
 _build_dir="%s"
 pkgdesc="oa-tools universal Linux remastering (Manjaro edition)"
 arch=('x86_64')
@@ -36,17 +33,17 @@ license=('GPL3')
 # Optimized Manjaro dependencies for oa-tools
 depends=(
     'manjaro-tools-iso'      # Hook miso per initramfs (fondamentale su Manjaro)
-    'efibootmgr'              # EFI
-    'libisoburn'              # xorriso
-    'squashfs-tools'         # mksquashfs
-    'mtools'                 # manipolazione EFI img
-    'dosfstools'             # mkfs.vfat
-    'arch-install-scripts'   # arch-chroot
-    'grub'                    # bootloader
-    'rsync'                  # copia file
-    'sudo'                    # privilegi
-    'pv'                      # progress meter
-    'git'                    # gestione wardrobe
+    'efibootmgr'
+    'libisoburn'
+    'squashfs-tools'
+    'mtools'
+    'dosfstools'
+    'arch-install-scripts'
+    'grub'
+    'rsync'
+    'sudo'
+    'pv'
+    'git'
     'bash-completion'
 )
 
@@ -54,37 +51,18 @@ conflicts=('penguins-eggs' 'oa-tools')
 backup=('etc/oa-tools.d/oa-tools.yaml')
 options=(!debug)
 
-build() {
-    # Pulizia preventiva del workspace temporaneo in RAM
-    rm -rf "${_build_dir}/oa" "${_build_dir}/coa"
-    mkdir -p "${_build_dir}"
-
-    msg2 "Compilazione del motore C (oa) nello schema ${_build_dir}..."
-    cd "${_srcdir}/oa"
-    make clean BUILD_DIR="${_build_dir}"
-    make all BUILD_DIR="${_build_dir}"
-
-    msg2 "Compilazione del motore Go (coa) nello schema ${_build_dir}..."
-    cd "${_srcdir}/coa"
-    mkdir -p "${_build_dir}/coa"
-    go build -ldflags "-X 'coa/pkg/cmd.AppVersion=${pkgver}'" -o "${_build_dir}/coa/coa" main.go
-}
-
 package() {
-    # 1. Installazione binari dallo schema specchio in RAM
+    # 1. Installazione binari GIA' COMPILATI dall'orchestratore in RAM
     install -Dm755 "${_build_dir}/oa/oa" "${pkgdir}/usr/bin/oa"
     install -Dm755 "${_build_dir}/coa/coa" "${pkgdir}/usr/bin/coa"
     ln -s coa "${pkgdir}/usr/bin/eggs"
 
-    # 2. Configurazione di sistema (/etc/oa-tools.d)
+    # 2. Configurazione e logica agnostica
     install -d "${pkgdir}/etc/oa-tools.d/brain.d"
-
-    if [ -d "${_srcdir}/coa/brain.d" ]; then
-        msg2 "Installazione logica agnostica (brain.d)..."
-        cp -r "${_srcdir}/coa/brain.d/"* "${pkgdir}/etc/oa-tools.d/brain.d/"
+    if [ -d "${_coadir}/brain.d" ]; then
+        cp -r "${_coadir}/brain.d/"* "${pkgdir}/etc/oa-tools.d/brain.d/"
     fi
 
-    # Generazione del file principale oa-tools.yaml (Dialetto oa)
     cat <<EOF > "${pkgdir}/etc/oa-tools.d/oa-tools.yaml"
 ---
 # oa-tools configuration (Manjaro)
@@ -104,19 +82,23 @@ remaster:
 EOF
 
     if [ -d "${_srcdir}/conf" ]; then
-        msg2 "Installazione file di configurazione addizionali..."
         cp -r "${_srcdir}/conf/"* "${pkgdir}/etc/oa-tools.d/"
     fi
 
-    // Documentazione e Completamenti nativi
-    if [ -d "${_coadir}/docs/man" ]; then
-        cp "${_coadir}/docs/man/"*.1 "${pkgdir}/usr/share/man/man1/"
-        chmod 644 "${pkgdir}/usr/share/man/man1/"*.1
+    # 3. Documentazione GIA' GENERATA in RAM dall'orchestratore
+    if [ -d "${_build_dir}/docs/man" ]; then
+        install -d "${pkgdir}/usr/share/man/man1"
+        for manfile in "${_build_dir}/docs/man/"*.1; do
+            if [ -f "$manfile" ]; then
+                cp "$manfile" "${pkgdir}/usr/share/man/man1/"
+                chmod 644 "${pkgdir}/usr/share/man/man1/"$(basename "$manfile")
+            fi
+        done
     fi
 
-    install -Dm644 "${_coadir}/docs/completion/coa.bash" "${pkgdir}/usr/share/bash-completion/completions/coa"
-    install -Dm644 "${_coadir}/docs/completion/coa.zsh" "${pkgdir}/usr/share/zsh/vendor-completions/_coa"
-    install -Dm644 "${_coadir}/docs/completion/coa.fish" "${pkgdir}/usr/share/fish/vendor_completions.d/coa.fish"
+    install -Dm644 "${_build_dir}/docs/completion/coa.bash" "${pkgdir}/usr/share/bash-completion/completions/coa"
+    install -Dm644 "${_build_dir}/docs/completion/coa.zsh" "${pkgdir}/usr/share/zsh/vendor-completions/_coa"
+    install -Dm644 "${_build_dir}/docs/completion/coa.fish" "${pkgdir}/usr/share/fish/vendor_completions.d/coa.fish"
 
     ln -s coa "${pkgdir}/usr/share/bash-completion/completions/eggs"
     ln -s _coa "${pkgdir}/usr/share/zsh/vendor-completions/_eggs"
@@ -124,9 +106,9 @@ EOF
 
     echo "complete -o default -F __start_coa eggs" >> "${pkgdir}/usr/share/bash-completion/completions/coa"
 }
-`, baseVer, relNum, ctx.ProjRoot, ctx.BaseBuildDir)
+`, baseVer, relNum, ctx.ProjRoot, ctx.CoaDir, ctx.BaseBuildDir)
 
-	// Scrittura del file PKGBUILD direttamente nella cartella di output scelta
+	// Scrittura del file PKGBUILD direttamente nella cartella di output
 	pkgbuildPath := filepath.Join(outDir, "PKGBUILD")
 	err := os.WriteFile(pkgbuildPath, []byte(pkgbuildContent), 0644)
 	if err != nil {
@@ -137,6 +119,6 @@ EOF
 	if ctx.EnvType == sysctx.EnvVagrant {
 		fmt.Printf("[SUCCESS] [Vagrant Mode] PKGBUILD (Manjaro) protetto direttamente in: %s\n", outDir)
 	} else {
-		fmt.Printf("[SUCCESS] PKGBUILD (Manjaro) generato correttamente in: %s\n", outDir)
+		fmt.Printf("[SUCCESS] [Local Mode] PKGBUILD (Manjaro) generato correttamente in: %s\n", outDir)
 	}
 }
