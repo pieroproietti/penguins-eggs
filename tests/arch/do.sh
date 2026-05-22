d4b810e49bcddb46c290b314a7ee0eb3f9dcfa35
Piero Proietti <piero.proietti@gmail.com>
2026-05-22 10:34:16 +0200

    copy arch

--- tests/arch/do.sh
+++ tests/arch/do.sh
@@ -9,7 +9,7 @@ rm -f ./*.iso
 
 echo "=== Install dependencies (Ubuntu Host) ==="
 sudo apt-get update
-# Abbiamo aggiunto libvirt-dev, ruby-dev e build-essential per non far schiantare il plugin!
+# Dipendenze KVM e pacchetti di sviluppo per far compilare il plugin a Vagrant
 sudo apt-get install -y wget curl gnupg2 software-properties-common qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils libvirt-dev ruby-dev build-essential
 
 echo "=== Install Vagrant & libvirt plugin ==="
@@ -26,30 +26,25 @@ sudo usermod -a -G kvm $(whoami)
 # Scardiniamo i permessi del socket visto che siamo su una macchina usa-e-getta
 sudo chmod 666 /var/run/libvirt/libvirt-sock
 
-echo "=== Fix permessi directory per KVM/9p ==="
-# Diamo il permesso di attraversamento (execute) e lettura a tutti sulle cartelle padre
-sudo chmod a+rx /home/runner
-sudo chmod -R a+rwX /home/runner/work
-
-echo "=== Avvio Macchina Virtuale Arch ==="
-vagrant up --provider=libvirt
-
 echo "=== Avvio Macchina Virtuale Arch ==="
 vagrant up --provider=libvirt
 sleep 10
 
-echo "=== Compilazione nativa dentro Arch Linux ==="
-# Facciamo compilare a lei il codice in C e Go
-vagrant ssh -c "cd /vagrant && make"
+echo "=== Compilazione nativa (Metodo Copia Locale in Home) ==="
+# 1. Isoliamo la build copiando i sorgenti nella home della VM (Niente conflitti con 9p!)
+vagrant ssh -c "mkdir -p ~/build_oa && cp -r /vagrant/* ~/build_oa/"
+# 2. Compiliamo sul disco nativo di Arch in totale sicurezza
+vagrant ssh -c "cd ~/build_oa && make"
 
 echo "=== Installazione Binari e Cervello ==="
-vagrant ssh -c "sudo cp /vagrant/coa/coa /usr/bin/coa"
-vagrant ssh -c "sudo cp /vagrant/oa/oa /usr/bin/oa"
+# 3. Peschiamo i binari finiti dalla cartella temporanea
+vagrant ssh -c "sudo cp ~/build_oa/coa/coa /usr/bin/coa"
+vagrant ssh -c "sudo cp ~/build_oa/oa/oa /usr/bin/oa"
 vagrant ssh -c "sudo chmod +x /usr/bin/coa /usr/bin/oa"
 
 vagrant ssh -c "sudo mkdir -p /etc/oa-tools.d/"
-# (Sostituisci /vagrant/conf se la tua cartella profili ha un nome diverso!)
-vagrant ssh -c "sudo cp -r /vagrant/conf/* /etc/oa-tools.d/"
+# (Ricorda: se la tua cartella si chiama 'brain_profiles', cambia 'conf' qui sotto!)
+vagrant ssh -c "sudo cp -r ~/build_oa/conf/* /etc/oa-tools.d/"
 
 echo "=== Test Finale e Remastering ==="
 vagrant ssh -c "oa --help"
