1fc44fe9369c3ac6845fb02dfbcc291761e9285f
Piero Proietti <piero.proietti@gmail.com>
2026-05-25 18:17:12 +0200

    ¡Hemos hecho feliz a Jorge! Instalación en btrfs

--- coa/pkg/calamares/prepare-partition-conf.go
+++ coa/pkg/calamares/prepare-partition-conf.go
@@ -8,63 +8,93 @@ import (
 	"time"
 )
 
-// getPartitionTableType rileva se il sistema è avviato in modalità UEFI o BIOS
+// 1. Rileva se siamo su UEFI o BIOS (La tua funzione intatta)
 func getPartitionTableType() string {
-	// Verifichiamo se la directory EFI esiste per distinguere GPT da MSDOS
 	if _, err := os.Stat("/sys/firmware/efi"); err == nil {
 		return "gpt"
 	}
-	// Fallback per macchine BIOS/Legacy
 	return "msdos"
 }
 
-// GetAvailableFS scansiona il sistema alla ricerca dei tool mkfs.* effettivamente installati
+// 2. Rileva i filesystem realmente supportati dal sistema live (La tua funzione intatta)
 func GetAvailableFS() []string {
-	// I candidati supportati da Calamares/KPMCore
 	candidates := []string{"ext4", "btrfs", "xfs", "f2fs", "jfs", "reiser"}
 	var available []string
 
 	for _, fs := range candidates {
-		// Cerchiamo se esiste mkfs.nomefilesystem nel PATH di sistema
 		if _, err := exec.LookPath("mkfs." + fs); err == nil {
 			available = append(available, fs)
 		}
 	}
 
-	// Fail-safe: se non trova nulla, garantiamo almeno ext4
 	if len(available) == 0 {
 		available = append(available, "ext4")
 	}
 	return available
 }
 
-// PreparePartitionConf genera il file di configurazione dinamico per il modulo partition
+// 3. Genera il file di configurazione con il layout dinamico (Quella aggiornata)
 func PreparePartitionConf() error {
-	// 1. Rileviamo l'ambiente hardware e software
 	tableType := getPartitionTableType()
-	fsList := GetAvailableFS()
+	fsList := GetAvailableFS() // <-- Qui chiama la tua funzione perfetta!
 
-	// Usiamo il dialetto 'oa' per il nome del sistema
 	dialect := "oa"
 	currentDate := time.Now().Format("2006-01-02")
 	availableFSYaml := "[\"" + strings.Join(fsList, "\", \"") + "\"]"
 
-	// 2. Prepariamo il template YAML
-	// Nota: Il blocco 'partitionLayout' risolve il problema dello spazio grigio
-	// forzando la creazione di una partizione root al 100%
+	defaultFs := fsList[0]
+	btrfsSubvolumes := ""
+
+	for _, fs := range fsList {
+		if fs == "btrfs" {
+			defaultFs = "btrfs"
+			btrfsSubvolumes = `
+# Regole di subvolume essenziali per BTRFS
+btrfs:
+    - mountPoint: /
+      subvolume: /@
+    - mountPoint: /home
+      subvolume: /@home
+    - mountPoint: /var/cache
+      subvolume: /@cache
+    - mountPoint: /var/log
+      subvolume: /@log
+`
+			break
+		}
+	}
+
+	var layoutYaml string
+	if tableType == "gpt" {
+		layoutYaml = fmt.Sprintf(`partitionLayout:
+    - name: "efi"
+      filesystem: "fat32"
+      mountPoint: "/boot/efi"
+      size: 300MiB
+      attributes: [ "boot", "esp" ]
+    - name: "root"
+      filesystem: "%s"
+      mountPoint: "/"
+      size: 100%%`, defaultFs)
+	} else {
+		layoutYaml = fmt.Sprintf(`partitionLayout:
+    - name: "bios_grub"
+      filesystem: "unformatted"
+      size: 8MiB
+      attributes: [ "bios_grub" ]
+    - name: "root"
+      filesystem: "%s"
+      mountPoint: "/"
+      size: 100%%`, defaultFs)
+	}
+
 	config := fmt.Sprintf(`---
 # partition.conf - Generato dinamicamente il %s
 # Dialetto: %s | Filosofia: https://penguins-eggs.net/blog/eggs-bananas
 
-# Forza la tabella corretta rilevata dal sarto: %s
 defaultPartitionTableType: %s
 
-# Configurazione del layout automatico (Risolve il problema delle barre grigie)
-partitionLayout:
-    - name: "root"
-      filesystem: "%s"
-      mountPoint: "/"
-      size: 100%%
+%s
 
 efi:
   mountPoint: "/boot/efi"
@@ -72,10 +102,9 @@ efi:
   minimumSize: 32MiB
   label: "EFI"
 
-# Filesystem e opzioni di swap
 defaultFileSystemType:  "%s"
 availableFileSystemTypes: %s
-
+%s
 userSwapChoices: [none, small, suspend, file]
 luksGeneration: luks1
 drawNestedPartitions: false
@@ -85,15 +114,9 @@ initialSwapChoice: none
 
 lvm:
   enable: true
-`, currentDate, dialect, tableType, tableType, fsList[0], fsList[0], availableFSYaml)
+`, currentDate, dialect, tableType, layoutYaml, defaultFs, availableFSYaml, btrfsSubvolumes)
 
-	// 3. Scrittura del file nel percorso di Calamares
 	targetPath := oaInstallerRoot + "/modules/partition.conf"
-
-	// Assicuriamoci che la directory esista
-	if err := os.MkdirAll(oaInstallerRoot+"/modules", 0755); err != nil {
-		return err
-	}
-
+	os.MkdirAll(oaInstallerRoot+"/modules", 0755)
 	return os.WriteFile(targetPath, []byte(config), 0644)
 }
