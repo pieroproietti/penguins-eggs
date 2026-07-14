277bfb17ce42e3576c3fc082d1ad9f1d75e8689a
Piero Proietti <piero.proietti@gmail.com>
2026-07-14 18:54:39 +0200

    fix: replace openssl shell command with native Go sha512_crypt

--- coa/pkg/planner/hash-password.go
+++ coa/pkg/planner/hash-password.go
@@ -2,8 +2,9 @@ package planner
 
 import (
 	"coa/pkg/utils"
-	"os/exec"
 	"strings"
+
+	"github.com/tredoe/osutil/user/crypt/sha512_crypt"
 )
 
 func hashPassword(password string) string {
@@ -15,12 +16,12 @@ func hashPassword(password string) string {
 		return password
 	}
 
-	cmd := exec.Command("openssl", "passwd", "-6", password)
-	out, err := cmd.Output()
+	c := sha512_crypt.New()
+	hash, err := c.Generate([]byte(password), []byte{})
 	if err != nil {
-		utils.LogNormal("\n[ENGINE WARNING] Unable to hash password with openssl: %v", err)
+		utils.LogNormal("\n[ENGINE WARNING] Unable to hash password with sha512_crypt: %v", err)
 		return "$6$oa-tools$uTKAYeAVn.Y.Dy2To6HXsHt1Gt4HpMghmOV93a46jFY7hkAQ3tk7eRTKjcvSYDf5sOf3qnKzyyPYXurKp9ST3."
 	}
 
-	return strings.TrimSpace(string(out))
+	return string(hash)
 }
